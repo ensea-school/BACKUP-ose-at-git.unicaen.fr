0d866e6b7c42a8ae59ad6a1107f1ada57dd64c2b
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-02-26 13:15:58 +0000

    SUppression code commenté.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -8,20 +8,6 @@
  */
 
 return array(
-//    'doctrine' => array(
-//        'driver' => array(
-//            'zfcuser_entity' => array(
-//                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
-//                'paths' => __DIR__ . '/xml/zfcuser'
-//            ),
-//
-//            'orm_default' => array(
-//                'drivers' => array(
-//                    'ZfcUser\Entity'  => 'zfcuser_entity'
-//                )
-//            )
-//        )
-//    ),
     'doctrine' => array(
         'configuration' => array(
             'orm_default' => array(

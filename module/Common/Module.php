00dd09e56bd469fcec1ccc11e9b85a32dbd0c1be
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-03-19 09:29:11 +0000

    AJout traduction de messages d'erreur Oracle : à tester.

--- module/Common/Module.php
+++ module/Common/Module.php
@@ -37,6 +37,11 @@ class Module
             ),
         );
     }
+    
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
     /**
      * {@inheritDoc}

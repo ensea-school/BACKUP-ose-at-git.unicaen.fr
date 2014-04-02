f0e946b9aaa0792f769ec7b5f44ab43d50a211cd
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-04-02 14:34:11 +0000

    toString

--- module/Application/src/Application/Entity/Db/Discipline.php
+++ module/Application/src/Application/Entity/Db/Discipline.php
@@ -9,6 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Discipline
 {
+    public function __toString()
+    {
+        return $this->getLibelleCourt();
+    }
+    
     /**
      * @var \DateTime
      */

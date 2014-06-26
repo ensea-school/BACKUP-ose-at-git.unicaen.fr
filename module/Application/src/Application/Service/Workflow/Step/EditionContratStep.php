ec424a69eea13b8b57a108ee0eeaf31712689c6e
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-06-26 15:31:19 +0000

    Modif intitulé étape.

--- module/Application/src/Application/Service/Workflow/Step/EditionContratStep.php
+++ module/Application/src/Application/Service/Workflow/Step/EditionContratStep.php
@@ -16,9 +16,9 @@ class EditionContratStep extends Step
     public function __construct()
     {
         $labels = array(
-            IntervenantPermanentRole::ROLE_ID => "Aller à mon contrat",
-            IntervenantExterieurRole::ROLE_ID => "Aller à mon contrat",
-            ComposanteRole::ROLE_ID           => "Aller au contrat de l'intervenant %s",
+            IntervenantPermanentRole::ROLE_ID => "Je visualise mon contrat",
+            IntervenantExterieurRole::ROLE_ID => "Je visualise mon contrat",
+            ComposanteRole::ROLE_ID           => "Je visualise le contrat de l'intervenant %s",
         );
         $descriptions = array(
             IntervenantPermanentRole::ROLE_ID => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed sem libero. Nam urna magna, fringilla et blandit aliquam, condimentum a velit. Vivamus sollicitudin blandit augue ut dapibus. Vivamus faucibus quis massa id tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor suscipit mauris, in suscipit quam lacinia ut. Nam ac mollis enim, quis tincidunt sem.",

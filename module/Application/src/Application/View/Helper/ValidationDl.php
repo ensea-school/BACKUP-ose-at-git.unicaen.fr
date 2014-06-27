f9cc8f091a520848d66fd19823b8f056c98a52c1
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-06-27 08:57:14 +0000

    Validation enseignements (vision intervenant) : more user friendly!

--- module/Application/src/Application/View/Helper/ValidationDl.php
+++ module/Application/src/Application/View/Helper/ValidationDl.php
@@ -25,10 +25,12 @@ class ValidationDl extends AbstractDl
         $html    = '';
         $dtdds   = array();
         
-        $dtdds[] = sprintf($tplDtdd,
-            "Type de validation :", 
-            $entity->getTypeValidation()
-        );
+        if ($this->getIncludeTypeValidation()) {
+            $dtdds[] = sprintf($tplDtdd,
+                "Type de validation :", 
+                $entity->getTypeValidation()
+            );
+        }
         
         $dtdds[] = sprintf($tplDtdd,
             "Date et auteur :", 
@@ -54,5 +56,32 @@ class ValidationDl extends AbstractDl
         $html .= sprintf($this->getTemplateDl('validation validation-details'), implode(PHP_EOL, $dtdds)) . PHP_EOL;
  
         return $html;
+    }
+    
+    /**
+     * @var bool
+     */
+    private $includeTypeValidation = true;
+    
+    /**
+     * Inclusion du type de validation dans l'affichage ?
+     * 
+     * @return bool
+     */
+    public function getIncludeTypeValidation()
+    {
+        return $this->includeTypeValidation;
+    }
+
+    /**
+     * Inclure ou non le type de validation dans l'affichage.
+     * 
+     * @param bool $includeTypeValidation
+     * @return \Application\View\Helper\ValidationDl
+     */
+    public function setIncludeTypeValidation($includeTypeValidation)
+    {
+        $this->includeTypeValidation = $includeTypeValidation;
+        return $this;
     }
 }

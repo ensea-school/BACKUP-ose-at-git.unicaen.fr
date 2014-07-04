8e3a798223f5386980ea1116ec29d6a8ef88a1ee
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-07-04 09:36:00 +0000

    Workflow : Modif cosmétiques sur des intitulés d'étapes.

--- module/Application/src/Application/Service/Workflow/Step/ValidationDossierStep.php
+++ module/Application/src/Application/Service/Workflow/Step/ValidationDossierStep.php
@@ -16,9 +16,9 @@ class ValidationDossierStep extends Step
     public function __construct()
     {
         $labels = array(
-            IntervenantPermanentRole::ROLE_ID => "Aller à la validation de mes données personnelles par la composante",
-            IntervenantExterieurRole::ROLE_ID => "Aller à la validation de mes données personnelles par la composante",
-            ComposanteRole::ROLE_ID           => "Aller à la validation des données personnelles de l'intervenant %s",
+            IntervenantPermanentRole::ROLE_ID => "Je visualise la validation de mes données personnelles par la composante",
+            IntervenantExterieurRole::ROLE_ID => "Je visualise la validation de mes données personnelles par la composante",
+            ComposanteRole::ROLE_ID           => "Je visualise la validation des données personnelles de l'intervenant %s",
         );
         $descriptions = array(
             IntervenantPermanentRole::ROLE_ID => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed sem libero. Nam urna magna, fringilla et blandit aliquam, condimentum a velit. Vivamus sollicitudin blandit augue ut dapibus. Vivamus faucibus quis massa id tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor suscipit mauris, in suscipit quam lacinia ut. Nam ac mollis enim, quis tincidunt sem.",
--- module/Application/src/Application/Service/Workflow/Step/ValidationServiceStep.php
+++ module/Application/src/Application/Service/Workflow/Step/ValidationServiceStep.php
@@ -16,9 +16,9 @@ class ValidationServiceStep extends Step
     public function __construct()
     {
         $labels = array(
-            IntervenantPermanentRole::ROLE_ID => "Aller à la validation de mes services par la composante",
-            IntervenantExterieurRole::ROLE_ID => "Aller à la validation de mes enseignements par la composante",
-            ComposanteRole::ROLE_ID           => "Aller à la validation des services de l'intervenant %s",
+            IntervenantPermanentRole::ROLE_ID => "Je visualise la validation de mes services par la composante",
+            IntervenantExterieurRole::ROLE_ID => "Je visualise la validation de mes enseignements par la composante",
+            ComposanteRole::ROLE_ID           => "Je visualise la validation des services de l'intervenant %s",
         );
         $descriptions = array(
             IntervenantPermanentRole::ROLE_ID => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed sem libero. Nam urna magna, fringilla et blandit aliquam, condimentum a velit. Vivamus sollicitudin blandit augue ut dapibus. Vivamus faucibus quis massa id tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor suscipit mauris, in suscipit quam lacinia ut. Nam ac mollis enim, quis tincidunt sem.",

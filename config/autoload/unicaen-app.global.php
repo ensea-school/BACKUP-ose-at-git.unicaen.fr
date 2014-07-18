04a762f51a4fe1154625f6c0b048d4485f4c65e3
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-07-18 11:48:45 +0000

    Incrément date de version.

--- config/autoload/unicaen-app.global.php
+++ config/autoload/unicaen-app.global.php
@@ -13,7 +13,7 @@ $settings = array(
         'nom'     => "OSE",
         'desc'    => "Organisation des Services d'Enseignement",
         'version' => "1.0.0",
-        'date'    => "07/07/2014",
+        'date'    => "18/07/2014",
         'contact' => array('mail' => "Contactez votre composante.", /*'tel' => "01 02 03 04 05"*/),
         'mentionsLegales'        => "http://www.unicaen.fr/outils-portail-institutionnel/mentions-legales/",
         'informatiqueEtLibertes' => "http://www.unicaen.fr/outils-portail-institutionnel/informatique-et-libertes/",

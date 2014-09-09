e5d4f7fba758c97f33616de2c83c716661aca855
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-09-09 14:02:19 +0000

    Possibilité de désactiver/réactiver les filtres pour certaines entités données

--- module/Common/src/Common/ORM/Filter/HistoriqueFilter.php
+++ module/Common/src/Common/ORM/Filter/HistoriqueFilter.php
@@ -12,6 +12,8 @@ use Doctrine\ORM\Query\Filter\SQLFilter;
  */
 class HistoriqueFilter extends SQLFilter
 {
+    protected $disabledEntities = [];
+
     public function addFilterConstraint(ClassMetaData $targetEntity, $targetTableAlias)
     {
         // Check if the entity implements the required interface
@@ -19,6 +21,45 @@ class HistoriqueFilter extends SQLFilter
             return "";
         }
 
+        if (isset($this->disabledEntities[$targetEntity->name])){
+            return "";
+        }
+
         return "$targetTableAlias.HISTO_DESTRUCTION IS NULL AND $targetTableAlias.HISTO_DESTRUCTEUR_ID IS NULL";
+    }
+
+    /**
+     * Désactive le filtre pour une entité donnée
+     *
+     * @param string $entity
+     * @return self
+     */
+    public function disableForEntity( $entity )
+    {
+        $this->disabledEntities[$entity] = true;
+        return $this;
+    }
+
+    /**
+     * Réactive le filtre pour une entité donnée
+     *
+     * @param string $entity
+     * @return self
+     */
+    public function enableForEntity($entity)
+    {
+        unset($this->disabledEntities[$entity]);
+        return $this;
+    }
+
+    /**
+     * Réactive les filtres pour toutes les entités
+     *
+     * @return self
+     */
+    public function enableForAll()
+    {
+        $this->disabledEntities = [];
+        return $this;
     }
 }

3d264af726cf966648bb321ff45907e7c8800f45
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-09-19 08:21:27 +0000

    Rapatriement des derniers changements intervenus sur trunk,
    Mise à jour des fichiers de configuration

---
+++ module/Application/src/Application/Form/Agrement/Saisie.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Application\Form\Agrement;
+
+use Zend\Form\Element\Csrf;
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Stdlib\Hydrator\ClassMethods;
+use UnicaenApp\Hydrator\Strategy\DateStrategy;
+
+/**
+ * Formulaire de saisie d'un agrément.
+ *
+ * @author Bertrand GAUTHIER <bertrand.gauthier at unicaen.fr>
+ */
+class Saisie extends Form implements InputFilterProviderInterface
+{
+    use \Application\Traits\IntervenantAwareTrait;
+    use \Application\Traits\TypeAgrementAwareTrait;
+    
+    public function init()
+    {
+        $this->setHydrator(new ClassMethods(false));
+        
+        $this->setAttribute('method', 'POST');
+        
+        $this->add(array(
+            'name' => 'dateDecision',
+            'type'  => 'UnicaenApp\Form\Element\Date',
+            'options' => array(
+                'label' => "Date de la décision",
+            ),
+            'attributes' => array(
+                'id' => uniqid('dateDecision'),
+            ),
+        ));
+        $this->getHydrator()->addStrategy('dateDecision', new DateStrategy($this->get('dateDecision')));
+        
+        $this->add(new Csrf('security'));
+        
+        $this->add(array(
+            'name' => 'submit',
+            'type'  => 'Submit',
+            'attributes' => array(
+                'value' => "Enregistrer",
+            ),
+        ));
+
+        return $this;
+    }
+    
+    /**
+     * 
+     * @param \Application\Entity\Db\Intervenant[] $intervenants
+     * @return self
+     */
+    public function setIntervenants(array $intervenants)
+    {
+        if (!$this->has($name = "intervenants")) {
+            $mcb = new \Zend\Form\Element\MultiCheckbox($name);
+            $this->add($mcb);
+        }
+        
+        $options = [];
+        foreach ($intervenants as $intervenant) {
+            $options[$intervenant->getId()] = (string)$intervenant;
+        }
+        $this->get($name)->setValueOptions($options);
+        
+        return $this;
+    }
+    
+    /**
+     * Should return an array specification compatible with
+     * {@link Zend\InputFilter\Factory::createInputFilter()}.
+     *
+     * @return array
+     */
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'dateDecision' => [
+                'required'   => true,
+            ],
+            'intervenants' => [
+                'required'   => $this->has('intervenants'),
+            ],
+        );
+    }
+}

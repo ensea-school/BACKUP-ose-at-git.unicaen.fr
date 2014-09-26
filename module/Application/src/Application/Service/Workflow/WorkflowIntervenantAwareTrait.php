93fb2857af9d85e00d81948baca30a608da85a24
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-09-26 14:32:13 +0000

    Suppression code commenté.

--- module/Application/src/Application/Service/Workflow/WorkflowIntervenantAwareTrait.php
+++ module/Application/src/Application/Service/Workflow/WorkflowIntervenantAwareTrait.php
@@ -23,18 +23,6 @@ trait WorkflowIntervenantAwareTrait
      */
     public function getWorkflowIntervenant()
     {
-//        if (null === $this->workflowIntervenant) {
-//            if (null === $serviceLocator && method_exists($this, 'getServiceLocator')) {
-//                $serviceLocator = $this->getServiceLocator();
-//            }
-//            if (null === $serviceLocator || !$serviceLocator instanceof ServiceLocatorInterface) {
-//                throw new \Common\Exception\LogicException("Aucun service locator valide disponible pour obtenir le workflow.");
-//            }
-//            $this->workflowIntervenant = $serviceLocator->get("ApplicationWorkflowIntervenant");
-//        }
-//        
-//        $this->workflowIntervenant->setIntervenant($intervenant);
-//        
         return $this->workflowIntervenant;
     }
     

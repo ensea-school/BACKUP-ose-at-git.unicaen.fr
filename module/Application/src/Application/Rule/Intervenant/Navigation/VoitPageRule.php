b6b6475ddb7d2afc326d9fd4a9ae11a5587db528
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2014-09-26 14:54:06 +0000

    Règles métier : Documentation des entêtes des classes.

--- module/Application/src/Application/Rule/Intervenant/Navigation/VoitPageRule.php
+++ module/Application/src/Application/Rule/Intervenant/Navigation/VoitPageRule.php
@@ -7,7 +7,7 @@ use Application\Entity\Db\Intervenant;
 use Application\Service\Workflow\AbstractWorkflow;
 
 /**
- * Description of VoitPageRule
+ * Règle métier déterminant si une page de navigation est visible pour un intervenant.
  *
  * @author Bertrand GAUTHIER <bertrand.gauthier at unicaen.fr>
  */

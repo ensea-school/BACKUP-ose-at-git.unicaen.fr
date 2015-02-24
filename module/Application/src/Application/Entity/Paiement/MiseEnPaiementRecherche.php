6b1dc23bc727d4d05ba51d00c6c7c985769e1ac4
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-02-24 09:55:21 +0000

    Réintégration branche mise en paiement

---
+++ module/Application/src/Application/Entity/Paiement/MiseEnPaiementRecherche.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Application\Entity\Paiement;
+
+use Application\Interfaces\StructureAwareInterface; use Application\Traits\StructureAwareTrait;
+use Application\Interfaces\PeriodeAwareInterface; use Application\Traits\PeriodeAwareTrait;
+use Common\EntityCollection;
+use Application\Entity\Db\Intervenant;
+use Application\Entity\Db\MiseEnPaiement;
+
+
+/**
+ * Description of MiseEnPaiementRecherche
+ *
+ * @author Laurent LÉCLUSE <laurent.lecluse at unicaen.fr>
+ */
+class MiseEnPaiementRecherche implements StructureAwareInterface, PeriodeAwareInterface
+{
+
+    use StructureAwareTrait;
+    use PeriodeAwareTrait;
+
+    /**
+     * etat
+     *
+     * @var string
+     */
+    protected $etat;
+
+    /**
+     * Intervenants
+     *
+     * @var Intervenant[]
+     */
+    protected $intervenants;
+
+
+
+
+    /**
+     *
+     * @return string
+     */
+    function getEtat()
+    {
+        return $this->etat;
+    }
+
+    function setEtat($etat)
+    {
+        if ($etat === null || $etat === MiseEnPaiement::A_METTRE_EN_PAIEMENT || $etat === MiseEnPaiement::MIS_EN_PAIEMENT){
+            $this->etat = $etat;
+        }else{
+            throw new \Common\Exception\LogicException('L\'état de mise en paiement "'.$etat.'" est invalide.');
+        }
+        return $this;
+    }
+
+    /**
+     * 
+     * @return Intervenant[]
+     */
+    public function getIntervenants()
+    {
+        if (null === $this->intervenants){
+            $this->intervenants = new EntityCollection;
+            $this->intervenants->initEntityClass( 'Application\Entity\Db\Intervenant' );
+        }
+        return $this->intervenants;
+    }
+
+}

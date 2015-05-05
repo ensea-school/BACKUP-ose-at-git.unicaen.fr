e2cacb8d2b1b971146881f95380b0e1f556ea4c4
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-05-05 11:19:27 +0000

    Traductions des erreurs Oracle : ajout de "defined()" pour corriger l'erreur "Constant already defined...".

--- module/Common/language/fr_FR/Oracle_Errors.php
+++ module/Common/language/fr_FR/Oracle_Errors.php
@@ -10,44 +10,82 @@
   from user_constraints where constraint_type = 'U' and generated = 'USER NAME' and table_name not like 'BIN$%' order by constraint_name ;
  */
 
-define('CONSTRAINT_ADRESSE_INTERVENANT_SOURCE_UN', 'Adresse Intervenant Source Un');
-define('CONSTRAINT_ADRESSE_STRUCTURE_SOURCE_UN', 'Adresse Structure Source Un');
-define('CONSTRAINT_AFFECTATION_IS_UN', 'Affectation Is Un');
-define('CONSTRAINT_AFFECTATION_SRC_UN', 'Affectation Src Un');
-define('CONSTRAINT_CHEMIN_PEDAGOGIQUE__UN', 'Chemin Pedagogique  Un');
-define('CONSTRAINT_CHEMIN_PEDAGO_SRC_ID_UN', 'Chemin Pedago Src Id Un');
-define('CONSTRAINT_CIVILITE_LIBELLE_COURT_UN', 'Civilite Libelle Court Un');
-define('CONSTRAINT_CORPS_SOURCE_UN', 'Corps Source Un');
-define('CONSTRAINT_EMPLOI_UN', 'Emploi Un');
-define('CONSTRAINT_EP_CODE__UN', 'Ep Code  Un');
-define('CONSTRAINT_EPP_SOURCE_UN', 'Epp Source Un');
-define('CONSTRAINT_EPP_UN', 'Epp Un');
-define('CONSTRAINT_ETABLISSEMENT_SOURCE_ID_UN', 'Etablissement Source Id Un');
-define('CONSTRAINT_ETAPE_CODE__UN', 'Etape Code  Un');
-define('CONSTRAINT_GTYPE_FORMATION_SOURCE_UN', 'Gtype Formation Source Un');
-define('CONSTRAINT_IE_SOURCE_UN', 'Ie Source Un');
-define('CONSTRAINT_INTERVENANT_SOURCE__UN', 'Intervenant Source  Un');
-define('CONSTRAINT_IP_SOURCE_UN', 'Ip Source Un');
-define('CONSTRAINT_PERSONNEL_SOURCE__UN', 'Personnel Source  Un');
-define('CONSTRAINT_PE_SOURCE_UN', 'Pe Source Un');
-define('CONSTRAINT_ROLE_SOURCE_UN', 'Role Source Un');
-define('CONSTRAINT_ROLE_UN', 'Role Un');
-define('CONSTRAINT_SECTION_CNU_SOURCE_UN', 'Section Cnu Source Un');
-define('CONSTRAINT_SERVICE_DU_IA_UN', 'Service Du Ia Un');
-define('CONSTRAINT_SERVICE_REFERENTIEL_UN', "Il n'est pas possible d'attribuer plusieurs fois la même fonction référentielle");
-define('CONSTRAINT_SITUATION_FAMILIALE_CODE_UN', 'Situation Familiale Code Un');
-define('CONSTRAINT_SOURCE_CODE_UN', 'Source Code Un');
-define('CONSTRAINT_STRUCTURE_SOURCE_ID_UN', 'Structure Source Id Un');
-define('CONSTRAINT_TYPE_FORMATION__UN', 'Type Formation  Un');
-define('CONSTRAINT_TYPE_INTERVENANT_CODE_UN', 'Type Intervenant Code Un');
-define('CONSTRAINT_TYPE_ROLE_CODE_UN', 'Type Role Code Un');
-define('CONSTRAINT_TYPE_STRUCTURE_CODE_UN', 'Type Structure Code Un');
-define('CONSTRAINT_UTILISATEUR_INTERVENANT_UN', 'Utilisateur Intervenant Un');
-define('CONSTRAINT_UTILISATEUR_PERSONNEL_UN', 'Utilisateur Personnel Un');
-define('CONSTRAINT_UTILISATEUR_ROLE_ID_UN', 'Utilisateur Role Id Un');
-define('CONSTRAINT_UTILISATEUR_USERNAME_UN', 'Utilisateur Username Un');
-define('CONSTRAINT_VHE_SOURCE_UN', 'Vhe Source Un');
-define('CONSTRAINT_VOLUME_HORAIRE_EP__UN', 'Volume Horaire Ep  Un');
+defined('CONSTRAINT_ADRESSE_INTERVENANT_SOURCE_UN') ||
+ define('CONSTRAINT_ADRESSE_INTERVENANT_SOURCE_UN', 'Adresse Intervenant Source Un');
+defined('CONSTRAINT_ADRESSE_STRUCTURE_SOURCE_UN') || 
+ define('CONSTRAINT_ADRESSE_STRUCTURE_SOURCE_UN', 'Adresse Structure Source Un');
+defined('CONSTRAINT_AFFECTATION_IS_UN') || 
+ define('CONSTRAINT_AFFECTATION_IS_UN', 'Affectation Is Un');
+defined('CONSTRAINT_AFFECTATION_SRC_UN') || 
+ define('CONSTRAINT_AFFECTATION_SRC_UN', 'Affectation Src Un');
+defined('CONSTRAINT_CHEMIN_PEDAGOGIQUE__UN') || 
+ define('CONSTRAINT_CHEMIN_PEDAGOGIQUE__UN', 'Chemin Pedagogique  Un');
+defined('CONSTRAINT_CHEMIN_PEDAGO_SRC_ID_UN') || 
+ define('CONSTRAINT_CHEMIN_PEDAGO_SRC_ID_UN', 'Chemin Pedago Src Id Un');
+defined('CONSTRAINT_CIVILITE_LIBELLE_COURT_UN') || 
+ define('CONSTRAINT_CIVILITE_LIBELLE_COURT_UN', 'Civilite Libelle Court Un');
+defined('CONSTRAINT_CORPS_SOURCE_UN') || 
+ define('CONSTRAINT_CORPS_SOURCE_UN', 'Corps Source Un');
+defined('CONSTRAINT_EMPLOI_UN') || 
+ define('CONSTRAINT_EMPLOI_UN', 'Emploi Un');
+defined('CONSTRAINT_EP_CODE__UN') || 
+ define('CONSTRAINT_EP_CODE__UN', 'Ep Code  Un');
+defined('CONSTRAINT_EPP_SOURCE_UN') || 
+ define('CONSTRAINT_EPP_SOURCE_UN', 'Epp Source Un');
+defined('CONSTRAINT_EPP_UN') || 
+ define('CONSTRAINT_EPP_UN', 'Epp Un');
+defined('CONSTRAINT_ETABLISSEMENT_SOURCE_ID_UN') || 
+ define('CONSTRAINT_ETABLISSEMENT_SOURCE_ID_UN', 'Etablissement Source Id Un');
+defined('CONSTRAINT_ETAPE_CODE__UN') || 
+ define('CONSTRAINT_ETAPE_CODE__UN', 'Etape Code  Un');
+defined('CONSTRAINT_GTYPE_FORMATION_SOURCE_UN') || 
+ define('CONSTRAINT_GTYPE_FORMATION_SOURCE_UN', 'Gtype Formation Source Un');
+defined('CONSTRAINT_IE_SOURCE_UN') || 
+ define('CONSTRAINT_IE_SOURCE_UN', 'Ie Source Un');
+defined('CONSTRAINT_INTERVENANT_SOURCE__UN') || 
+ define('CONSTRAINT_INTERVENANT_SOURCE__UN', 'Intervenant Source  Un');
+defined('CONSTRAINT_IP_SOURCE_UN') || 
+ define('CONSTRAINT_IP_SOURCE_UN', 'Ip Source Un');
+defined('CONSTRAINT_PERSONNEL_SOURCE__UN') || 
+ define('CONSTRAINT_PERSONNEL_SOURCE__UN', 'Personnel Source  Un');
+defined('CONSTRAINT_PE_SOURCE_UN') || 
+ define('CONSTRAINT_PE_SOURCE_UN', 'Pe Source Un');
+defined('CONSTRAINT_ROLE_SOURCE_UN') || 
+ define('CONSTRAINT_ROLE_SOURCE_UN', 'Role Source Un');
+defined('CONSTRAINT_ROLE_UN') || 
+ define('CONSTRAINT_ROLE_UN', 'Role Un');
+defined('CONSTRAINT_SECTION_CNU_SOURCE_UN') || 
+ define('CONSTRAINT_SECTION_CNU_SOURCE_UN', 'Section Cnu Source Un');
+defined('CONSTRAINT_SERVICE_DU_IA_UN') || 
+ define('CONSTRAINT_SERVICE_DU_IA_UN', 'Service Du Ia Un');
+defined('CONSTRAINT_SERVICE_REFERENTIEL_UN') || 
+ define('CONSTRAINT_SERVICE_REFERENTIEL_UN', "Il n'est pas possible d'attribuer plusieurs fois la même fonction référentielle");
+defined('CONSTRAINT_SITUATION_FAMILIALE_CODE_UN') || 
+ define('CONSTRAINT_SITUATION_FAMILIALE_CODE_UN', 'Situation Familiale Code Un');
+defined('CONSTRAINT_SOURCE_CODE_UN') || 
+ define('CONSTRAINT_SOURCE_CODE_UN', 'Source Code Un');
+defined('CONSTRAINT_STRUCTURE_SOURCE_ID_UN') || 
+ define('CONSTRAINT_STRUCTURE_SOURCE_ID_UN', 'Structure Source Id Un');
+defined('CONSTRAINT_TYPE_FORMATION__UN') || 
+ define('CONSTRAINT_TYPE_FORMATION__UN', 'Type Formation  Un');
+defined('CONSTRAINT_TYPE_INTERVENANT_CODE_UN') || 
+ define('CONSTRAINT_TYPE_INTERVENANT_CODE_UN', 'Type Intervenant Code Un');
+defined('CONSTRAINT_TYPE_ROLE_CODE_UN') || 
+ define('CONSTRAINT_TYPE_ROLE_CODE_UN', 'Type Role Code Un');
+defined('CONSTRAINT_TYPE_STRUCTURE_CODE_UN') || 
+ define('CONSTRAINT_TYPE_STRUCTURE_CODE_UN', 'Type Structure Code Un');
+defined('CONSTRAINT_UTILISATEUR_INTERVENANT_UN') || 
+ define('CONSTRAINT_UTILISATEUR_INTERVENANT_UN', 'Utilisateur Intervenant Un');
+defined('CONSTRAINT_UTILISATEUR_PERSONNEL_UN') || 
+ define('CONSTRAINT_UTILISATEUR_PERSONNEL_UN', 'Utilisateur Personnel Un');
+defined('CONSTRAINT_UTILISATEUR_ROLE_ID_UN') || 
+ define('CONSTRAINT_UTILISATEUR_ROLE_ID_UN', 'Utilisateur Role Id Un');
+defined('CONSTRAINT_UTILISATEUR_USERNAME_UN') || 
+ define('CONSTRAINT_UTILISATEUR_USERNAME_UN', 'Utilisateur Username Un');
+defined('CONSTRAINT_VHE_SOURCE_UN') || 
+ define('CONSTRAINT_VHE_SOURCE_UN', 'Vhe Source Un');
+defined('CONSTRAINT_VOLUME_HORAIRE_EP__UN') || 
+ define('CONSTRAINT_VOLUME_HORAIRE_EP__UN', 'Volume Horaire Ep  Un');
 
 return [
     'ORA-00001: unique constraint (OSE.ADRESSE_INTERVENANT_SOURCE_UN) violated' => CONSTRAINT_ADRESSE_INTERVENANT_SOURCE_UN,

7d3161cb343dcc4c8e82801f98fa13df04481e77
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-05-27 13:56:29 +0000

    Script SQL utile.

--- data/Sql/Workflow.sql
+++ data/Sql/Workflow.sql
@@ -2,12 +2,13 @@
  * Consultation de la Feuille de route d'un intervenant.
  */
 
-select i.nom_usuel, e.code, e.libelle, ie.franchie, ie.courante, s.libelle_court
+select i.nom_usuel, e.ordre, e.code, e.libelle, ie.atteignable, ie.franchie, ie.courante, s.libelle_court
 from wf_intervenant_etape ie 
 join intervenant i on i.id = ie.intervenant_id
 join wf_etape e on e.id = ie.etape_id
 left join structure s on s.id = ie.structure_id
-where i.source_code = '1058';
+where i.source_code = '44003' --and ie.structure_id is  null
+order by e.ordre;
 
 
 /**

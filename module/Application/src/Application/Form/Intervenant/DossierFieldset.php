2fb4cc655c151e7ce935b1fe1467c22bfba2f791
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-05-29 12:59:59 +0000

    Formulaire données perso : ajout "title" sur le champ "email perso".

--- module/Application/src/Application/Form/Intervenant/DossierFieldset.php
+++ module/Application/src/Application/Form/Intervenant/DossierFieldset.php
@@ -247,6 +247,7 @@ class DossierFieldset extends Fieldset implements ServiceLocatorAwareInterface, 
                 'label' => 'Adresse mail personnelle (éventuelle)',
             ],
             'attributes' => [
+                'title' => "Si vous renseignez une adresse mail perso, celle-ci sera utitlisée pour vous contacter.",
                 'readonly' => false
             ],
             'type'       => 'Text',

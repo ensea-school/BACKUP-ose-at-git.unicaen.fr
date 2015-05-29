b674e83849086bd3749ac503acc14c0d4e0274aa
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-05-29 07:07:15 +0000

    WF : Modif intitulé de l'étape "clôture du réalisé".

--- module/Application/src/Application/Service/Workflow/Step/ClotureRealiseStep.php
+++ module/Application/src/Application/Service/Workflow/Step/ClotureRealiseStep.php
@@ -16,10 +16,10 @@ class ClotureRealiseStep extends Step
     public function __construct()
     {
         $labels = [
-            IntervenantPermanentRole::ROLE_ID => "Je visualise la clôture de la saisie de mes enseignements réalisés",
-            IntervenantExterieurRole::ROLE_ID => "Je visualise la clôture de la saisie de mes enseignements réalisés",
-            ComposanteRole::ROLE_ID           => "Je visualise la clôture de la saisie des enseignements réalisés de l'intervenant",
-            'default'                         => "Clôture de la saisie des enseignements réalisés",
+            IntervenantPermanentRole::ROLE_ID => "Je visualise la clôture de la saisie de mes services réalisés",
+            IntervenantExterieurRole::ROLE_ID => "Je visualise la clôture de la saisie de mes services réalisés",
+            ComposanteRole::ROLE_ID           => "Je visualise la clôture de la saisie des services réalisés de l'intervenant",
+            'default'                         => "Clôture de la saisie des services réalisés",
         ];
         $descriptions = [
             IntervenantPermanentRole::ROLE_ID => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed sem libero. Nam urna magna, fringilla et blandit aliquam, condimentum a velit. Vivamus sollicitudin blandit augue ut dapibus. Vivamus faucibus quis massa id tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor suscipit mauris, in suscipit quam lacinia ut. Nam ac mollis enim, quis tincidunt sem.",

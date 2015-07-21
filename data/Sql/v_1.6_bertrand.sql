b8a1efe262d8387e096e583523b4a1e7bffd2a6b
gauthierb <gauthierb@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-07-21 12:39:32 +0000

    Correction script SQL déploiement 1.6.

--- data/Sql/v_1.6_bertrand.sql
+++ data/Sql/v_1.6_bertrand.sql
@@ -18,37 +18,37 @@ select
 from indicateur i
 order by type, ordre;
 */
-update indicateur set id_interne = 110 where code = 'PermAffectAutreIntervMeme'; -- Affectation
-update indicateur set id_interne = 120 where code = 'PermAffectMemeIntervAutre'; -- Affectation
-update indicateur set id_interne = 130 where code = 'BiatssAffectMemeIntervAutre'; -- Affectation
-update indicateur set id_interne = 210 where code = 'AttenteAgrementCR'; -- Agrément
-update indicateur set id_interne = 220 where code = 'AttenteAgrementCA'; -- Agrément
-update indicateur set id_interne = 310 where code = 'AgrementCAMaisPasContrat'; -- Contrat / avenant
-update indicateur set id_interne = 320 where code = 'AttenteContrat'; -- Contrat / avenant
-update indicateur set id_interne = 330 where code = 'AttenteAvenant'; -- Contrat / avenant
-update indicateur set id_interne = 340 where code = 'SaisieServiceApresContratAvenant'; -- Contrat / avenant
-update indicateur set id_interne = 350 where code = 'ContratAvenantDeposes'; -- Contrat / avenant
-update indicateur set id_interne = 360 where code = 'AttenteRetourContrat'; -- Contrat / avenant
-update indicateur set id_interne = 410 where code = 'AttenteValidationDonneesPerso'; -- Données personnelles
-update indicateur set id_interne = 420 where code = 'DonneesPersoDiffImport'; -- Données personnelles
-update indicateur set id_interne = 430 where code = 'DonneesPersoModif'; -- Données personnelles
-update indicateur set id_interne = 510 where code = 'EnsHisto'; -- Enseignements et référentiel
-update indicateur set id_interne = 520 where code = 'PlafondHcPrevuHorsRemuFcDepasse'; -- Enseignements et référentiel
-update indicateur set id_interne = 530 where code = 'PlafondHcRealiseHorsRemuFcDepasse'; -- Enseignements et référentiel
-update indicateur set id_interne = 610 where code = 'AttenteValidationEnsPrevuPerm'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 620 where code = 'AttenteValidationRefPrevuPerm'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 630 where code = 'EnsRealisePermSaisieNonCloturee'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 640 where code = 'AttenteValidationEnsRealisePerm'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 650 where code = 'AttenteValidationEnsRealisePermAutreComp'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 660 where code = 'AttenteValidationRefRealisePerm'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 670 where code = 'AttenteValidationRefRealisePermAutreComp'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 680 where code = 'PlafondRefPrevuDepasse'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 690 where code = 'PlafondRefRealiseDepasse'; -- Enseignements et référentiel <em>Permanents</em>
-update indicateur set id_interne = 710 where code = 'AttenteValidationEnsPrevuVac'; -- Enseignements et référentiel <em>Vacataires</em>
-update indicateur set id_interne = 720 where code = 'AttenteValidationEnsRealiseVac'; -- Enseignements et référentiel <em>Vacataires</em>
-update indicateur set id_interne = 810 where code = 'AttenteDemandeMepPerm'; -- Mise en paiement <em>Permanents</em>
-update indicateur set id_interne = 820 where code = 'AttenteMepPerm'; -- Mise en paiement <em>Permanents</em>
-update indicateur set id_interne = 910 where code = 'AttenteDemandeMepVac'; -- Mise en paiement <em>Vacataires</em>
-update indicateur set id_interne = 920 where code = 'AttenteMepVac'; -- Mise en paiement <em>Vacataires</em>
-update indicateur set id_interne = 1010 where code = 'AttentePieceJustif'; -- Pièces justificatives
-update indicateur set id_interne = 1020 where code = 'AttenteValidationPieceJustif'; -- Pièces justificatives
+update indicateur set numero = 110  where code = 'PermAffectAutreIntervMeme'; -- Affectation
+update indicateur set numero = 120  where code = 'PermAffectMemeIntervAutre'; -- Affectation
+update indicateur set numero = 130  where code = 'BiatssAffectMemeIntervAutre'; -- Affectation
+update indicateur set numero = 210  where code = 'AttenteAgrementCR'; -- Agrément
+update indicateur set numero = 220  where code = 'AttenteAgrementCA'; -- Agrément
+update indicateur set numero = 310  where code = 'AgrementCAMaisPasContrat'; -- Contrat / avenant
+update indicateur set numero = 320  where code = 'AttenteContrat'; -- Contrat / avenant
+update indicateur set numero = 330  where code = 'AttenteAvenant'; -- Contrat / avenant
+update indicateur set numero = 340  where code = 'SaisieServiceApresContratAvenant'; -- Contrat / avenant
+update indicateur set numero = 350  where code = 'ContratAvenantDeposes'; -- Contrat / avenant
+update indicateur set numero = 360  where code = 'AttenteRetourContrat'; -- Contrat / avenant
+update indicateur set numero = 410  where code = 'AttenteValidationDonneesPerso'; -- Données personnelles
+update indicateur set numero = 420  where code = 'DonneesPersoDiffImport'; -- Données personnelles
+update indicateur set numero = 430  where code = 'DonneesPersoModif'; -- Données personnelles
+update indicateur set numero = 510  where code = 'EnsHisto'; -- Enseignements et référentiel
+update indicateur set numero = 520  where code = 'PlafondHcPrevuHorsRemuFcDepasse'; -- Enseignements et référentiel
+update indicateur set numero = 530  where code = 'PlafondHcRealiseHorsRemuFcDepasse'; -- Enseignements et référentiel
+update indicateur set numero = 610  where code = 'AttenteValidationEnsPrevuPerm'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 620  where code = 'AttenteValidationRefPrevuPerm'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 630  where code = 'EnsRealisePermSaisieNonCloturee'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 640  where code = 'AttenteValidationEnsRealisePerm'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 650  where code = 'AttenteValidationEnsRealisePermAutreComp'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 660  where code = 'AttenteValidationRefRealisePerm'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 670  where code = 'AttenteValidationRefRealisePermAutreComp'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 680  where code = 'PlafondRefPrevuDepasse'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 690  where code = 'PlafondRefRealiseDepasse'; -- Enseignements et référentiel <em>Permanents</em>
+update indicateur set numero = 710  where code = 'AttenteValidationEnsPrevuVac'; -- Enseignements et référentiel <em>Vacataires</em>
+update indicateur set numero = 720  where code = 'AttenteValidationEnsRealiseVac'; -- Enseignements et référentiel <em>Vacataires</em>
+update indicateur set numero = 810  where code = 'AttenteDemandeMepPerm'; -- Mise en paiement <em>Permanents</em>
+update indicateur set numero = 820  where code = 'AttenteMepPerm'; -- Mise en paiement <em>Permanents</em>
+update indicateur set numero = 910  where code = 'AttenteDemandeMepVac'; -- Mise en paiement <em>Vacataires</em>
+update indicateur set numero = 920  where code = 'AttenteMepVac'; -- Mise en paiement <em>Vacataires</em>
+update indicateur set numero = 1010 where code = 'AttentePieceJustif'; -- Pièces justificatives
+update indicateur set numero = 1020 where code = 'AttenteValidationPieceJustif'; -- Pièces justificatives

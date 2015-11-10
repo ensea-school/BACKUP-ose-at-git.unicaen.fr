4a5507a2f928bbcde41d5b819aa0183659f2769e
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-11-10 14:18:45 +0000

    Refactoring : colonnes devenues inutiles voire dangereuses car plus utilisées

--- module/Application/src/Application/Entity/Db/Utilisateur.php
+++ module/Application/src/Application/Entity/Db/Utilisateur.php
@@ -12,72 +12,6 @@ class Utilisateur extends AbstractUser
 {
     const APP_UTILISATEUR_ID = 1;
 
-    /**
-     * @var \Application\Entity\Db\Intervenant
-     */
-    protected $intervenant;
-
-    /**
-     * @var \Application\Entity\Db\Personnel
-     */
-    protected $personnel;
-
-
-
-    /**
-     * Set intervenant
-     *
-     * @param \Application\Entity\Db\Intervenant $intervenant
-     *
-     * @return Utilisateur
-     */
-    public function setIntervenant(\Application\Entity\Db\Intervenant $intervenant = null)
-    {
-        $this->intervenant = $intervenant;
-
-        return $this;
-    }
-
-
-
-    /**
-     * Get intervenant
-     *
-     * @return \Application\Entity\Db\Intervenant
-     */
-    public function getIntervenant()
-    {
-        return $this->intervenant;
-    }
-
-
-
-    /**
-     * Set personnel
-     *
-     * @param \Application\Entity\Db\Personnel $personnel
-     *
-     * @return Utilisateur
-     */
-    public function setPersonnel(\Application\Entity\Db\Personnel $personnel = null)
-    {
-        $this->personnel = $personnel;
-
-        return $this;
-    }
-
-
-
-    /**
-     * Get personnel
-     *
-     * @return \Application\Entity\Db\Personnel
-     */
-    public function getPersonnel()
-    {
-        return $this->personnel;
-    }
-
 
 
     /**
@@ -86,24 +20,6 @@ class Utilisateur extends AbstractUser
     public function getRoles()
     {
         return [];
-    }
-
-
-
-    /**
-     * @since PHP 5.6.0
-     * This method is called by var_dump() when dumping an object to get the properties that should be shown.
-     * If the method isn't defined on an object, then all public, protected and private properties will be shown.
-     *
-     * @return array
-     * @link  http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.debuginfo
-     */
-    function __debugInfo()
-    {
-        return [
-            'id'          => $this->id,
-            'displayName' => $this->displayName,
-        ];
     }
 
 }

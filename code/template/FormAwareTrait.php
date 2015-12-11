a61b4da25fef0168d93b35a193b2d9d80b36e9a8
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2015-12-11 11:02:36 +0000

    Refactoring : Eradication des chaines de caractères désignant des entités : remplacement par la syntaxe php5.5 ::class des entités

--- code/template/FormAwareTrait.php
+++ code/template/FormAwareTrait.php
@@ -35,25 +35,28 @@ trait <class>
 
 
     /**
+     * Retourne un nouveau formulaire ou fieldset systématiquement, sauf si ce dernier a été fourni manuellement.
+     *
      * @return <targetClass>
      * @throws RuntimeException
      */
     public function get<method>()
     {
-        if (empty($this-><variable>)){
-            $serviceLocator = Module::$serviceLocator;
-            if (! $serviceLocator) {
-                if (!method_exists($this, 'getServiceLocator')) {
-                    throw new RuntimeException('La classe ' . get_class($this) . ' n\'a pas accès au ServiceLocator.');
-                }
-
-                $serviceLocator = $this->getServiceLocator();
-                if (method_exists($serviceLocator, 'getServiceLocator')) {
-                    $serviceLocator = $serviceLocator->getServiceLocator();
-                }
-            }
-            $this-><variable> = $serviceLocator->get('FormElementManager')->get('<name>');
+        if (!empty($this-><variable>)){
+            return $this-><variable>;
         }
-        return $this-><variable>;
+
+        $serviceLocator = Module::$serviceLocator;
+        if (! $serviceLocator) {
+            if (!method_exists($this, 'getServiceLocator')) {
+                throw new RuntimeException('La classe ' . get_class($this) . ' n\'a pas accès au ServiceLocator.');
+            }
+
+            $serviceLocator = $this->getServiceLocator();
+            if (method_exists($serviceLocator, 'getServiceLocator')) {
+                $serviceLocator = $serviceLocator->getServiceLocator();
+            }
+        }
+        return $serviceLocator->get('FormElementManager')->get('<name>');
     }
 }
--- data/Sql/privileges.sql
+++ data/Sql/privileges.sql
@@ -1,13 +1,13 @@
   
-  /*
+  
 INSERT INTO CATEGORIE_PRIVILEGE (
   ID,
   CODE,
   LIBELLE
 ) VALUES (
   CATEGORIE_PRIVILEGE_ID_SEQ.nextval,
-  'discipline',
-  'Gestion des disciplines'
+  'contrat',
+  'Contrats de travail/Avenants'
 );
 
 INSERT INTO PRIVILEGE (
@@ -25,27 +25,51 @@ SELECT
   (SELECT count(*) FROM PRIVILEGE WHERE categorie_id = (SELECT id FROM CATEGORIE_PRIVILEGE WHERE code = t1.c )) + rownum ORDRE
 FROM (
 
---      SELECT 'droit' c, 'role-visualisation' p, 'Rôles - Visualisation' l FROM dual
---UNION SELECT 'droit' c, 'role-edition' p, 'Rôles - Édition' l FROM dual
-      SELECT 'discipline' c, 'gestion' p, 'Gestion' l FROM dual
-UNION SELECT 'discipline' c, 'visualisation' p, 'Visualisation' l FROM dual
-UNION SELECT 'discipline' c, 'edition' p, 'Édition' l FROM dual
+      SELECT 'contrat' c, 'association' p, 'Association' l FROM dual
+UNION SELECT 'contrat' c, 'visualisation' p, 'Visualisation' l FROM dual
+UNION SELECT 'contrat' c, 'creation' p, 'Création d''un projet' l FROM dual
+UNION SELECT 'contrat' c, 'suppression' p, 'Suppression d''un projet' l FROM dual
+UNION SELECT 'contrat' c, 'validation' p, 'Validation' l FROM dual
+UNION SELECT 'contrat' c, 'devalidation' p, 'Dévalidation' l FROM dual
+UNION SELECT 'contrat' c, 'depot-retour-signe' p, 'Dépôt de contrat signé' l FROM dual
+UNION SELECT 'contrat' c, 'saisie-date-retour-signe' p, 'Saisie de date retour' l FROM dual
+UNION SELECT 'enseignement' c, 'cloture' p, 'Clôture' l FROM dual
 
-) t1;*/
+) t1;
 
 select
-  /*/
-  p.id, cp.code categorie, p.code privilege, p.ordre ordre /*/
-  '    const '
-    || rpad( upper( replace( cp.code, '-', '_' ) || '_' || replace( p.code, '-', '_' ) ), MAX( length( cp.code ) + length( p.code ) ) OVER (PARTITION BY 1 )+1, ' ' )
-    || ' = ' || '''' || cp.code || '-' || p.code || '''' || ';' php_const /**/
+  
+  p.id, cp.code categorie, p.code privilege, p.ordre ordre
 from
   privilege p
   join categorie_privilege cp on cp.id = p.categorie_id
 order by
   cp.code, p.ordre;
   
+select * from categorie_privilege order by ordre;
 
+update categorie_privilege set ordre = 10 WHERE code = 'odf';
+update categorie_privilege set ordre = 20 WHERE code = 'discipline';
+
+update categorie_privilege set ordre = 30 WHERE code = 'intervenant';
+update categorie_privilege set ordre = 40 WHERE code = 'modif-service-du';
+update categorie_privilege set ordre = 50 WHERE code = 'dossier';
+update categorie_privilege set ordre = 60 WHERE code = 'piece-justificative';
+update categorie_privilege set ordre = 70 WHERE code = 'enseignement';
+update categorie_privilege set ordre = 80 WHERE code = 'motif-non-paiement';
+update categorie_privilege set ordre = 90 WHERE code = 'referentiel';
+update categorie_privilege set ordre = 100 WHERE code = 'agrement';
+update categorie_privilege set ordre = 110 WHERE code = 'contrat';
+update categorie_privilege set ordre = 120 WHERE code = 'mise-en-paiement';
+
+update categorie_privilege set ordre = 130 WHERE code = 'indicateur';
+update categorie_privilege set ordre = 140 WHERE code = 'droit';
+update categorie_privilege set ordre = 150 WHERE code = 'import';
+
+
+
+
+  
 
 
 

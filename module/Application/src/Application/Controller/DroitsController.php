7642bd5e1237f01c3d1c5c17abcbbc4190f100d3
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2016-01-13 15:03:40 +0000

    Système de filtrage du tableau des privilèges pour éviter le tableau géant inutilisable

--- module/Application/src/Application/Controller/DroitsController.php
+++ module/Application/src/Application/Controller/DroitsController.php
@@ -38,6 +38,8 @@ class DroitsController extends AbstractActionController
     use RoleFormAwareTrait;
     use AffectationFormAwareTrait;
 
+
+
     /**
      *
      * @return type
@@ -47,25 +49,29 @@ class DroitsController extends AbstractActionController
         return [];
     }
 
+
+
     public function rolesAction()
     {
-        $qb = $this->getServiceRole()->finderByHistorique();
-        $roles = $this->getServiceRole()->getList( $qb );
+        $qb    = $this->getServiceRole()->finderByHistorique();
+        $roles = $this->getServiceRole()->getList($qb);
 
         return compact('roles');
     }
 
+
+
     public function roleEditionAction()
     {
-        $role = $this->context()->roleFromRoute();
+        $role   = $this->context()->roleFromRoute();
         $errors = [];
 
         $form = $this->getFormDroitsRole();
-        if (empty($role)){
+        if (empty($role)) {
             $title = 'Création d\'un nouveau rôle';
-            $role = $this->getServiceRole()->newEntity();
+            $role  = $this->getServiceRole()->newEntity();
             $form->setObject($role);
-        }else{
+        } else {
             $title = 'Édition du rôle';
             $form->bind($role);
         }
@@ -77,8 +83,7 @@ class DroitsController extends AbstractActionController
                 try {
                     $this->getServiceRole()->save($role);
                     $form->get('id')->setValue($role->getId()); // transmet le nouvel ID
-                }
-                catch (\Exception $e) {
+                } catch (\Exception $e) {
                     $e        = DbException::translate($e);
                     $errors[] = $e->getMessage();
                 }
@@ -88,48 +93,78 @@ class DroitsController extends AbstractActionController
         return compact('form', 'title', 'errors');
     }
 
+
+
     public function roleSuppressionAction()
     {
         $role = $this->context()->mandatory()->roleFromRoute();
 
-        $title     = "Suppression du rôle";
-        $form      = new \Application\Form\Supprimer('suppr');
+        $title  = "Suppression du rôle";
+        $form   = new \Application\Form\Supprimer('suppr');
         $errors = [];
         $form->setAttribute('action', $this->url()->fromRoute(null, [], [], true));
 
         if ($this->getRequest()->isPost()) {
             try {
                 $this->getServiceRole()->delete($role);
-            }catch(\Exception $e){
-                $e = DbException::translate($e);
+            } catch (\Exception $e) {
+                $e        = DbException::translate($e);
                 $errors[] = $e->getMessage();
             }
         }
+
         return compact('role', 'title', 'form', 'errors');
     }
 
+
+
     public function privilegesAction()
     {
-        $ps = $this->getServicePrivilege()->getList();
-        $privileges = [];
-        foreach( $ps as $privilege ){
-            $categorie = $privilege->getCategorie();
-            if (! isset($privileges[$categorie->getCode()])){
-                $privileges[$categorie->getCode()] = [
-                    'categorie' => $categorie,
-                    'privileges' => [],
-                ];
-            }
-            $privileges[$categorie->getCode()]['privileges'][] = $privilege;
+        $filters = [];
+        if ($categorieFilter = $this->params()->fromQuery('cat')) {
+            $filters['cat'] = $categorieFilter;
+        }
+        if ($rsFilter = $this->params()->fromQuery('rs')){
+            $filters['rs'] = $rsFilter;
         }
 
-        $qb = $this->getServiceRole()->finderByHistorique();
-        $roles = $this->getServiceRole()->getList( $qb );
+        $ps         = $this->getServicePrivilege()->getList();
+        $privileges = [];
+        foreach ($ps as $privilege) {
+            $categorie = $privilege->getCategorie();
 
-        $qb = $this->getServiceStatutIntervenant()->finderByHistorique();
-        $statuts = $this->getServiceStatutIntervenant()->getList( $qb );
-        return compact('privileges', 'roles', 'statuts');
+            $ok = true;
+            if ($categorieFilter && $categorieFilter != $categorie->getCode()) $ok = false;
+
+            if ($ok) {
+                if (!isset($privileges[$categorie->getCode()])) {
+                    $privileges[$categorie->getCode()] = [
+                        'categorie'     => $categorie,
+                        'categorieLink' => $this->url()->fromRoute(null, [], ['query' => $filters+['cat'=>$categorie->getCode()]], true),
+                        'privileges'    => [],
+                    ];
+                }
+                $privileges[$categorie->getCode()]['privileges'][] = $privilege;
+            }
+        }
+
+        if ($rsFilter == 'r' || !$rsFilter) {
+            $qb    = $this->getServiceRole()->finderByHistorique();
+            $roles = $this->getServiceRole()->getList($qb);
+        }else{
+            $roles = [];
+        }
+
+        if ($rsFilter == 's' || !$rsFilter) {
+            $qb      = $this->getServiceStatutIntervenant()->finderByHistorique();
+            $statuts = $this->getServiceStatutIntervenant()->getList($qb);
+        }else{
+            $statuts = [];
+        }
+
+        return compact('privileges', 'roles', 'statuts', 'filters');
     }
+
 
 
     public function privilegesModifierAction()
@@ -139,47 +174,56 @@ class DroitsController extends AbstractActionController
         $privilege = $this->getServicePrivilege()->get($this->params()->fromPost('privilege'));
         $action    = $this->params()->fromPost('action');
 
-        switch($action){
-        case 'accorder':
-            if ($role  ) $this->roleAddPrivilege      ($role  , $privilege);
-            if ($statut) $this->statutAddPrivilege    ($statut, $privilege);
-        break;
-        case 'refuser':
-            if ($role  ) $this->roleRemovePrivilege   ($role  , $privilege);
-            if ($statut) $this->statutRemovePrivilege ($statut, $privilege);
-        break;
+        switch ($action) {
+            case 'accorder':
+                if ($role) $this->roleAddPrivilege($role, $privilege);
+                if ($statut) $this->statutAddPrivilege($statut, $privilege);
+                break;
+            case 'refuser':
+                if ($role) $this->roleRemovePrivilege($role, $privilege);
+                if ($statut) $this->statutRemovePrivilege($statut, $privilege);
+                break;
         }
+
         return compact('role', 'statut', 'privilege');
     }
 
+
+
     /* fonctions pour pallier un pb dans l'enregistrement via doctrine... (à revoir) */
-    private function roleAddPrivilege( Role $role, Privilege $privilege )
+    private function roleAddPrivilege(Role $role, Privilege $privilege)
     {
-        $sql = "INSERT INTO ROLE_PRIVILEGE (role_id, privilege_id) VALUES (".$role->getId().", ".$privilege->getId().")";
+        $sql = "INSERT INTO ROLE_PRIVILEGE (role_id, privilege_id) VALUES (" . $role->getId() . ", " . $privilege->getId() . ")";
         $this->em()->getConnection()->exec($sql);
         $this->em()->refresh($privilege);
         $this->em()->refresh($role);
     }
 
-    private function roleRemovePrivilege( Role $role, Privilege $privilege )
+
+
+    private function roleRemovePrivilege(Role $role, Privilege $privilege)
     {
-        $sql = "DELETE ROLE_PRIVILEGE WHERE role_id = ".$role->getId()." AND privilege_id = ".$privilege->getId();
+        $sql = "DELETE ROLE_PRIVILEGE WHERE role_id = " . $role->getId() . " AND privilege_id = " . $privilege->getId();
         $this->em()->getConnection()->exec($sql);
         $this->em()->refresh($privilege);
         $this->em()->refresh($role);
     }
 
-    private function statutAddPrivilege( StatutIntervenant $statut, Privilege $privilege )
+
+
+    private function statutAddPrivilege(StatutIntervenant $statut, Privilege $privilege)
     {
-        $sql = "INSERT INTO STATUT_PRIVILEGE (statut_id, privilege_id) VALUES (".$statut->getId().", ".$privilege->getId().")";
+        $sql = "INSERT INTO STATUT_PRIVILEGE (statut_id, privilege_id) VALUES (" . $statut->getId() . ", " . $privilege->getId() . ")";
         $this->em()->getConnection()->exec($sql);
         $this->em()->refresh($privilege);
         $this->em()->refresh($statut);
     }
 
-    private function statutRemovePrivilege( StatutIntervenant $statut, Privilege $privilege )
+
+
+    private function statutRemovePrivilege(StatutIntervenant $statut, Privilege $privilege)
     {
-        $sql = "DELETE STATUT_PRIVILEGE WHERE statut_id = ".$statut->getId()." AND privilege_id = ".$privilege->getId();
+        $sql = "DELETE STATUT_PRIVILEGE WHERE statut_id = " . $statut->getId() . " AND privilege_id = " . $privilege->getId();
         $this->em()->getConnection()->exec($sql);
         $this->em()->refresh($privilege);
         $this->em()->refresh($statut);
@@ -193,47 +237,49 @@ class DroitsController extends AbstractActionController
 
         $serviceAffectations = $this->getServiceAffectation();
 
-        list($qb,$alias) = $serviceAffectations->initQuery();
+        list($qb, $alias) = $serviceAffectations->initQuery();
 
-        $serviceAffectations->join( $this->getServiceRole(), $qb, 'role', true);
-        $serviceAffectations->join( $this->getServicePersonnel(), $qb, 'personnel', true);
-        $serviceAffectations->join( $this->getServiceSource(), $qb, 'source', true);
-        $serviceAffectations->leftJoin( $this->getServiceStructure(), $qb, 'structure', true);
+        $serviceAffectations->join($this->getServiceRole(), $qb, 'role', true);
+        $serviceAffectations->join($this->getServicePersonnel(), $qb, 'personnel', true);
+        $serviceAffectations->join($this->getServiceSource(), $qb, 'source', true);
+        $serviceAffectations->leftJoin($this->getServiceStructure(), $qb, 'structure', true);
         $serviceAffectations->finderByHistorique($qb);
 
         /* @var $qb \Doctrine\ORM\QueryBuilder */
 
-        switch($tri){
-        case 'structure':
-            $this->getServiceStructure()->orderBy($qb);
-            $this->getServiceRole()->orderBy($qb);
-            $this->getServicePersonnel()->orderBy($qb);
-            $this->getServiceSource()->orderBy($qb);
-        break;
-        case 'role':
-            $this->getServiceRole()->orderBy($qb);
-            $this->getServicePersonnel()->orderBy($qb);
-            $this->getServiceStructure()->orderBy($qb);
-            $this->getServiceSource()->orderBy($qb);
-        break;
-        case 'personnel':
-            $this->getServicePersonnel()->orderBy($qb);
-            $this->getServiceStructure()->orderBy($qb);
-            $this->getServiceRole()->orderBy($qb);
-            $this->getServiceSource()->orderBy($qb);
-        break;
-        case 'source':
-            $this->getServiceSource()->orderBy($qb);
-            $this->getServiceStructure()->orderBy($qb);
-            $this->getServiceRole()->orderBy($qb);
-            $this->getServicePersonnel()->orderBy($qb);
-        break;
+        switch ($tri) {
+            case 'structure':
+                $this->getServiceStructure()->orderBy($qb);
+                $this->getServiceRole()->orderBy($qb);
+                $this->getServicePersonnel()->orderBy($qb);
+                $this->getServiceSource()->orderBy($qb);
+                break;
+            case 'role':
+                $this->getServiceRole()->orderBy($qb);
+                $this->getServicePersonnel()->orderBy($qb);
+                $this->getServiceStructure()->orderBy($qb);
+                $this->getServiceSource()->orderBy($qb);
+                break;
+            case 'personnel':
+                $this->getServicePersonnel()->orderBy($qb);
+                $this->getServiceStructure()->orderBy($qb);
+                $this->getServiceRole()->orderBy($qb);
+                $this->getServiceSource()->orderBy($qb);
+                break;
+            case 'source':
+                $this->getServiceSource()->orderBy($qb);
+                $this->getServiceStructure()->orderBy($qb);
+                $this->getServiceRole()->orderBy($qb);
+                $this->getServicePersonnel()->orderBy($qb);
+                break;
         }
 
-        $affectations = $serviceAffectations->getList( $qb );
+        $affectations = $serviceAffectations->getList($qb);
 
         return compact('affectations', 'tri');
     }
+
+
 
     public function affectationEditionAction()
     {
@@ -242,11 +288,11 @@ class DroitsController extends AbstractActionController
         $errors = [];
 
         $form = $this->getFormDroitsAffectation();
-        if (empty($affectation)){
-            $title = 'Création d\'une nouvelle affectation';
+        if (empty($affectation)) {
+            $title       = 'Création d\'une nouvelle affectation';
             $affectation = $this->getServiceAffectation()->newEntity();
             $form->setObject($affectation);
-        }else{
+        } else {
             $title = 'Édition de l\'affectation';
             $form->bind($affectation);
         }
@@ -256,13 +302,12 @@ class DroitsController extends AbstractActionController
             $form->setData($request->getPost());
             if ($form->isValid()) {
                 try {
-                    if (! $affectation->getRole()->getPerimetre()->isComposante()){
+                    if (!$affectation->getRole()->getPerimetre()->isComposante()) {
                         $affectation->setStructure(null);
                     }
                     $this->getServiceAffectation()->save($affectation);
                     $form->get('id')->setValue($affectation->getId()); // transmet le nouvel ID
-                }
-                catch (\Exception $e) {
+                } catch (\Exception $e) {
                     $e        = DbException::translate($e);
                     $errors[] = $e->getMessage();
                 }
@@ -272,66 +317,72 @@ class DroitsController extends AbstractActionController
         return compact('form', 'title', 'errors');
     }
 
+
+
     public function affectationSuppressionAction()
     {
         $affectation = $this->context()->mandatory()->affectationFromRoute();
 
-        $title     = "Suppression de l'affectation";
-        $form      = new \Application\Form\Supprimer('suppr');
+        $title  = "Suppression de l'affectation";
+        $form   = new \Application\Form\Supprimer('suppr');
         $errors = [];
         $form->setAttribute('action', $this->url()->fromRoute(null, [], [], true));
 
         if ($this->getRequest()->isPost()) {
             try {
                 $this->getServiceAffectation()->delete($affectation);
-            }catch(\Exception $e){
-                $e = DbException::translate($e);
+            } catch (\Exception $e) {
+                $e        = DbException::translate($e);
                 $errors[] = $e->getMessage();
             }
         }
+
         return compact('affectation', 'title', 'form', 'errors');
     }
 
+
+
     /**
      * @param string $roleStatutCode
+     *
      * @return \Zend\Form\Form
      */
-    public function getFormDroitsSelection( $roleStatutCode )
+    public function getFormDroitsSelection($roleStatutCode)
     {
         $options = [];
-        if (empty($roleStatutCode)){
-            $options['null']= ['label' => 'Sélection du rôle'     , 'options' => ['' => 'Veuillez sélectionner un rôle...']];
+        if (empty($roleStatutCode)) {
+            $options['null'] = ['label' => 'Sélection du rôle', 'options' => ['' => 'Veuillez sélectionner un rôle...']];
         }
 
-        $options['roles']   = ['label' => 'Rôles (personnel)'     , 'options' => []];
+        $options['roles']   = ['label' => 'Rôles (personnel)', 'options' => []];
         $options['statuts'] = ['label' => 'Statuts (intervenants)', 'options' => []];
 
-        $qb = $this->getServiceRole()->finderByHistorique();
-        $roles = $this->getServiceRole()->getList( $qb );
-        foreach( $roles as $role ){
-            $options['roles']['options']['r-'.$role->getCode()] = (string)$role;
+        $qb    = $this->getServiceRole()->finderByHistorique();
+        $roles = $this->getServiceRole()->getList($qb);
+        foreach ($roles as $role) {
+            $options['roles']['options']['r-' . $role->getCode()] = (string)$role;
         }
 
-        $qb = $this->getServiceStatutIntervenant()->finderByHistorique();
-        $statuts = $this->getServiceStatutIntervenant()->getList( $qb );
-        foreach( $statuts as $statut ){
-            $options['statuts']['options']['s-'.$statut->getSourceCode()] = (string)$statut;
+        $qb      = $this->getServiceStatutIntervenant()->finderByHistorique();
+        $statuts = $this->getServiceStatutIntervenant()->getList($qb);
+        foreach ($statuts as $statut) {
+            $options['statuts']['options']['s-' . $statut->getSourceCode()] = (string)$statut;
         }
 
         $form = new \Zend\Form\Form;
         $form->add([
-            'name'          => 'role',
-            'type'          => 'Zend\Form\Element\Select',
-            'attributes'    => ['onchange'=>'document.location.href=$(this).parents("form").attr("action")+"/"+$(this).val();'],
-            'options'       => [
-                'label'=>'Choix du rôle ou du statut à paramétrer :',
-                'value_options'=> $options,
-                'empty_options'=>'Sélectionner un rôle...'
+            'name'       => 'role',
+            'type'       => 'Zend\Form\Element\Select',
+            'attributes' => ['onchange' => 'document.location.href=$(this).parents("form").attr("action")+"/"+$(this).val();'],
+            'options'    => [
+                'label'         => 'Choix du rôle ou du statut à paramétrer :',
+                'value_options' => $options,
+                'empty_options' => 'Sélectionner un rôle...',
             ],
         ]);
         $form->setAttribute('action', $this->url()->fromRoute(null, []));
 
-        $form->get('role')->setValue( $roleStatutCode ?: '' );
+        $form->get('role')->setValue($roleStatutCode ?: '');
 
         return $form;
     }

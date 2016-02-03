ab9fcc18e8ce3081cc53b6cb03b07bf6823f72d6
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2016-02-03 11:38:26 +0000

    Refonte de la page de maintenance pour qu'elle soit complètement indépendante. Suppression du répertoier DOC obsolète

--- public/maintenance.php
+++ public/maintenance.php
@@ -1,97 +1,75 @@
 <?php
 
-$remoteAddr= $_SERVER['REMOTE_ADDR'];
+$remoteAddr = $_SERVER['REMOTE_ADDR'];
 $forwarded = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
 
- $root = substr( $_SERVER['SCRIPT_NAME'], 0, -10 );
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
 
-?><!DOCTYPE html><html lang="fr">
-    <head>
-        <!-- Meta -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <!-- Title -->
-        <title>Maintenance - OSE</title>
-        <!-- Links -->
-        <link href="//www.unicaen.fr/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-<link href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" media="screen" rel="stylesheet" type="text/css">
-<link href="<?php echo $root ?>/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
-<link href="<?php echo $root ?>/css/bootstrap-theme.min.css" media="screen" rel="stylesheet" type="text/css">
-<link href="<?php echo $root ?>/css/unicaen.css" media="screen" rel="stylesheet" type="text/css">
-<link href="<?php echo $root ?>/css/app.css" media="screen" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
-    </head>
+    <title>Maintenance - OSE</title>
+</head>
 
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">Vous utilisez un navigateur internet hors d'âge. Veuillez le mettre à jour, svp.</p>
-        <![endif]-->
-        <div id="navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bs-navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand"><h1 class="title">OSE</h1></a>                </div>
-                <div class="navbar-collapse bs-navbar-collapse collapse">
-                    <ul class="nav&#x20;navbar-nav&#x20;menu-principal"></ul>
-                    <p class="navbar-text navbar-right"><span style="color:#555">REMOTE_ADDR=<?php echo $remoteAddr;?>, HTTP_X_FORWARDED_FOR=<?php echo $forwarded;?></span></p>
-                </div><!--/.nav-collapse -->
-            </div>
-        </div>
+<body>
+<div id="navbar">
+    <h1 class="title">OSE</h1>
+    <p class="info">
+        REMOTE_ADDR=<?php echo $remoteAddr; ?>, HTTP_X_FORWARDED_FOR=<?php echo $forwarded; ?>
+    </p>
+</div>
 
-        <div class="container ">
-            <ul class="breadcrumb"><li>Maintenance</li></ul>        </div>
+<div id="contenu">
 
-        <div id="contenu-principal" class="container">
+        <h1>OSE
+            <small>Organisation des Services d'Enseignement</small>
+        </h1>
+        <p class="lead"><?php echo $maintenanceText ?></p>
 
+</div>
+<style>
 
-<div class="jumbotron">
-    <h1>OSE <small>Organisation des Services d'Enseignement</small></h1>
-    <p class="lead"><?php echo $maintenanceText ?></p>
+    body{
+        margin: 0px;
+        padding:0px;
+    }
 
-    </div>
+    #navbar {
 
-                    </div>
+        font-size: 14px;
+        line-height: 1.42857;
+        color: #333;
+        background-image: linear-gradient(to bottom, #3C3C3C 0px, #222 100%);
+        background-repeat: repeat-x;
+        background-color: #222;
+        border: 1px #080808 solid;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    }
 
-        <hr />
+    #navbar h1 {
+        float: left;
+        color: #9D9D9D;
+        margin-top: 5px;
+        margin-left: 5px;
+    }
 
-        <div id="footer" class="container">
-            <ul class="navigation">
-    <li>
-        <a title="Page&#x20;d&#x27;accueil&#x20;du&#x20;site&#x20;de&#x20;l&#x27;Universit&#xE9;&#x20;de&#x20;Caen&#x20;-&#x20;Basse&#x20;Normandie" class="ucbn" href="http&#x3A;&#x2F;&#x2F;www.unicaen.fr&#x2F;">Université de Caen - Normandie</a>
-    </li>
-    <li>
-        <a title="&#xC0;&#x20;propos&#x20;de&#x20;cette&#x20;application" class="apropos" href="&#x2F;ose&#x2F;apropos">À propos</a>
-    </li>
-    <li>
-        <a title="Contact&#x20;concernant&#x20;l&#x27;application" class="contact" href="&#x2F;ose&#x2F;contact">Contact</a>
-    </li>
-    <li>
-        <a title="Page&#x20;de&#x20;navigation&#x20;au&#x20;sein&#x20;de&#x20;l&#x27;application" class="plan" href="&#x2F;ose&#x2F;plan">Plan de navigation</a>
-    </li>
-    <li>
-        <a title="Mentions&#x20;l&#xE9;gales" class="ml" href="http&#x3A;&#x2F;&#x2F;www.unicaen.fr&#x2F;outils-portail-institutionnel&#x2F;mentions-legales&#x2F;">Mentions légales</a>
-    </li>
-    <li>
-        <a title="Informatique&#x20;et&#x20;libert&#xE9;s" class="il" href="http&#x3A;&#x2F;&#x2F;www.unicaen.fr&#x2F;outils-portail-institutionnel&#x2F;informatique-et-libertes&#x2F;">Informatique et libertés</a>
-    </li>
-</ul>        </div>
+    #navbar .info {
+        color:#555;
+        text-align: right;
+        margin-right: 5px;
+    }
 
-        <a href="#" class="scrollup" title="Remonter en haut de cette page"><img alt="Remonter" src="/ose/css/icon-top.png"/></a>
+    #contenu {
+        margin: 2em;
+        padding:1em;
+        background-color: #f2dede;
+        border-radius: 6px;
+        border: 1px #d5c0c0 solid;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    }
 
-    </body>
+</style>
+</body>
 </html>
 <?php die();

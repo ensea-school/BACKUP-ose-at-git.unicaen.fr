3f4c84cb34f1f82f9253be6e56c998e433b5b229
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2016-02-15 13:55:11 +0000

    v3.0.5

--- config/autoload/unicaen-app.global.php
+++ config/autoload/unicaen-app.global.php
@@ -12,7 +12,7 @@ $settings = [
     'app_infos' => [
         'nom'     => "OSE",
         'desc'    => "Organisation des Services d'Enseignement",
-        'version' => "3.0.4",
+        'version' => "3.0.5",
         'date'    => "15/02/2016",
         'contact' => ['mail' => "Contactez votre composante.", /*'tel' => "01 02 03 04 05"*/],
         'mentionsLegales'        => "http://www.unicaen.fr/outils-portail-institutionnel/mentions-legales/",

972204eca795e0d5ba4f4e2473eb5aece990ab16
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2016-07-08 10:28:34 +0000

    Refonte des fichiers de config locaux/globaux pour + de simplicité

--- config/application.config.php
+++ config/application.config.php
@@ -12,27 +12,24 @@ if (!\Zend\Console\Console::isConsole()){
     array_unshift($modules, 'BjyAuthorize'); // ne charge BjyAuthorize QUE si on n'est pas en mode console
 }
 
-
-$moduleListenerOptions = [
-    'config_glob_paths'    => [
-        'config/autoload/{,*.}{global,local}.php',
-    ],
-    'module_paths' => [
-        './module',
-        './vendor',
-    ],
-];
-
 if ( 'development' == $env ) {
     $modules[] = 'ZendDeveloperTools';
     $modules[] = 'UnicaenCode';
-}else{
-    $moduleListenerOptions['config_cache_enabled']      = true;
-    $moduleListenerOptions['module_map_cache_enabled']  = true;
-    $moduleListenerOptions['cache_dir'] = 'data/cache/';
 }
 
 return [
+    'translator' => [
+        'locale' => 'fr_FR',
+    ],
     'modules' => $modules,
-    'module_listener_options' => $moduleListenerOptions,
+    'module_listener_options' => [
+        'config_glob_paths'    => [
+            'config/autoload/{,*.}{global,local}.php',
+        ],
+        'module_paths' => [
+            './module',
+            './vendor',
+        ],
+        'cache_dir' => 'data/cache/',
+    ],
 ];

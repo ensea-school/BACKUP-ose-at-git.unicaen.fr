4a3b9a10c2d6b47ce6839936cb8fe1768d90b60e
lecluse <lecluse@d57fa8bc-6af1-4de9-8b7d-78e900e231e7>
2016-07-13 08:16:05 +0000

    Version 2016-2017 des modèles de PJ, v4.1.2

--- config/autoload/unicaen-app.global.php
+++ config/autoload/unicaen-app.global.php
@@ -12,7 +12,7 @@ $settings = [
     'app_infos' => [
         'nom'     => "OSE",
         'desc'    => "Organisation des Services d'Enseignement",
-        'version' => "4.1.1",
+        'version' => "4.1.2",
         'date'    => "13/07/2016",
         'contact' => ['mail' => "Contactez votre composante.", /*'tel' => "01 02 03 04 05"*/],
         'mentionsLegales'        => "http://www.unicaen.fr/outils-portail-institutionnel/mentions-legales/",

547da6c5764b00985b319a4ffddf458a38acdf73
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-01-30 16:41:09 +0100

    Adaptation aux changements d'UnicaenImport

---
+++ module/Application/src/Application/Service/Factory/IntervenantServiceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Application\Service\Factory;
+
+use UnicaenImport\Processus\ImportProcessus;
+use Zend\ServiceManager\ServiceLocatorInterface as ContainerInterface;
+use Application\Service\IntervenantService;
+
+
+
+/**
+ * Description of IntervenantServiceFactory
+ *
+ * @author LECLUSE Laurent <laurent.lecluse at unicaen.fr>
+ */
+class IntervenantServiceFactory
+{
+
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array|null         $options
+     *
+     * @return IntervenantService
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, $options = null)
+    {
+        $service = new IntervenantService;
+        $service->setProcessusImport($container->get(ImportProcessus::class));
+
+        return $service;
+    }
+}

3d7c07a535550c3ebce4104a203ee44c46dc85f5
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-02-22 11:04:39 +0100

    Refonte requête de gestion des agréments

--- data/Sql/Agrements.sql
+++ data/Sql/Agrements.sql
@@ -11,4 +11,45 @@ FROM
   JOIN statut_intervenant si ON si.id = tas.statut_intervenant_id
 ORDER BY
   statut_intervenant,
-  type_agrement
+  type_agrement;
+
+SELECT source_code, libelle FROM statut_intervenant WHERE histo_destruction IS NULL;
+
+
+
+INSERT INTO TYPE_AGREMENT_STATUT (
+  ID,
+  TYPE_AGREMENT_ID,
+  STATUT_INTERVENANT_ID,
+  OBLIGATOIRE,
+  PREMIER_RECRUTEMENT,
+  HISTO_CREATION,HISTO_CREATEUR_ID,
+  HISTO_MODIFICATION,HISTO_MODIFICATEUR_ID
+) VALUES (
+  TYPE_AGREMENT_STATUT_ID_SEQ.NEXTVAL,
+  (SELECT id FROM type_agrement WHERE code = 'CONSEIL_RESTREINT'), -- CONSEIL_ACADEMIQUE | CONSEIL_RESTREINT
+  (SELECT id FROM statut_intervenant WHERE source_code = 'INTERMITTENT'),
+  1, -- OBLIGATOIRE
+  NULL, -- PREMIER_RECRUTEMENT
+  sysdate, (SELECT id FROM utilisateur WHERE username='lecluse'),
+  sysdate, (SELECT id FROM utilisateur WHERE username='lecluse')
+);
+
+
+INSERT INTO TYPE_AGREMENT_STATUT (
+  ID,
+  TYPE_AGREMENT_ID,
+  STATUT_INTERVENANT_ID,
+  OBLIGATOIRE,
+  PREMIER_RECRUTEMENT,
+  HISTO_CREATION,HISTO_CREATEUR_ID,
+  HISTO_MODIFICATION,HISTO_MODIFICATEUR_ID
+) VALUES (
+  TYPE_AGREMENT_STATUT_ID_SEQ.NEXTVAL,
+  (SELECT id FROM type_agrement WHERE code = 'CONSEIL_ACADEMIQUE'), -- CONSEIL_ACADEMIQUE | CONSEIL_RESTREINT
+  (SELECT id FROM statut_intervenant WHERE source_code = 'INTERMITTENT'),
+  1, -- OBLIGATOIRE
+  1, -- PREMIER_RECRUTEMENT
+  sysdate, (SELECT id FROM utilisateur WHERE username='lecluse'),
+  sysdate, (SELECT id FROM utilisateur WHERE username='lecluse')
+);

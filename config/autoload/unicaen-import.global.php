c3c8c997ec7651d0e7d766d1ef6ce0fc505b4bd8
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-04-09 17:25:17 +0200

    Adaptation à la refonte d'UnicaenImport

--- config/autoload/unicaen-import.global.php
+++ config/autoload/unicaen-import.global.php
@@ -27,32 +27,38 @@ return [
                                 'route'    => 'import',
                                 'resource' => PrivilegeController::getResourceId('Import\Controller\Import', 'index'),
                                 'pages'    => [
-                                    'differentiel'               => [
-                                        'label'       => "Écarts entre les données de l'application et ses sources",
-                                        'description' => "Affiche, table par table, la liste des données différentes entre l'application et ses sources de données",
-                                        'route'       => 'import',
-                                        'resource'    => PrivilegeController::getResourceId('Import\Controller\Import', 'differentiel'),
-                                        'params'      => [
-                                            'action' => 'index',
-                                        ],
-                                    ],
                                     'sources'                   => [
                                         'label'       => "Sources de données",
                                         'description' => "Liste des sources de données",
-                                        'route'       => 'import',
-                                        'resource'    => PrivilegeController::getResourceId('Import\Controller\Import', 'sources'),
-                                        'params'      => [
-                                            'action' => 'sources',
-                                        ],
+                                        'route'       => 'import/sources',
+                                        'resource'    => PrivilegeController::getResourceId('Import\Controller\Source', 'index'),
                                     ],
-                                    'tableau-bord'        => [
+                                    'tables'                    => [
+                                        'label'    => 'Tables',
+                                        'route'    => 'import/tables',
+                                        'resource' => PrivilegeController::getResourceId('Import\Controller\Table', 'index'),
+                                    ],
+                                    'tableau-bord'              => [
                                         'label'       => "Tableau de bord principal",
                                         'description' => "Liste, table par table, les colonnes dont les données sont importables ou non, leur caractéristiques et l'état de l'import à leur niveau.",
-                                        'route'       => 'import',
+                                        'route'       => 'import/tableau-bord',
                                         'resource'    => PrivilegeController::getResourceId('Import\Controller\Import', 'tableau-bord'),
-                                        'params'      => [
-                                            'action' => 'tableau-bord',
-                                        ],
+                                    ],
+                                    'differentiel'              => [
+                                        'label'       => "Écarts entre les données de l'application et ses sources",
+                                        'description' => "Affiche, table par table, la liste des données différentes entre l'application et ses sources de données",
+                                        'route'       => 'import/differentiel',
+                                        'resource'    => PrivilegeController::getResourceId('Import\Controller\Differentiel', 'index'),
+                                    ],
+                                    'maj-vues-fonctions' => [
+                                        'label'       => "Mise à jour des vues différentielles et des procédures de mise à jour",
+                                        'description' => "Réactualise les vues différentielles d'import. Ces dernières servent à déterminer quelles données ont changé,
+        sont apparues ou ont disparues des sources de données.
+        Met également à jour les procédures de mise à jour qui actualisent les données de l'application à partir des informations
+        fournies par les vues différentielles.
+        Cette réactualisation n'est utile que si les vues sources ont été modifiées.",
+                                        'route'       => 'import/maj-vues-fonctions',
+                                        'resource'    => PrivilegeController::getResourceId('Import\Controller\Import', 'update-views-and-packages'),
                                     ],
                                 ],
                             ],

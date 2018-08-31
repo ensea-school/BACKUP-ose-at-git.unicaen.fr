eddb8bfaa626641f7150fd6ac16b51c4b83af0df
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-08-31 15:50:06 +0200

    Tests divers

--- code/test6.php
+++ code/test6.php
@@ -6,35 +6,3 @@
  * @var $viewName   string
  * @var $sl         \Zend\ServiceManager\ServiceLocatorInterface
  */
-
-use Zend\Form\Form;
-
-var_dump($_POST);
-
-$horaire = isset($_POST['horaire']) ? $_POST['horaire'] : null;
-
-$hd = $horaire;
-//$hd = null;//'32/05/2017';
-
-$form = new Form();
-$form->add([
-    'type'    => 'DateTime',
-    'name'    => 'horaire',
-    'options' => [
-        'label'    => 'Horaire',
-        'format'   => 'd/m/Y à H:i',
-    ],
-]);
-$form->add([
-    'name'       => 'submit',
-    'type'       => 'Submit',
-    'attributes' => [
-        'value' => 'Enregistrer',
-        'title' => "Enregistrer",
-        'class' => 'btn btn-primary',
-    ],
-]);
-$form->get('horaire')->setValue($hd);
-
-echo $this->form($form);
-

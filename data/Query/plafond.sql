964dc0a54bec0596b8f916bd3cb88efd8150b2e6
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-11-06 11:35:58 +0100

    Correction bug plafond FC Majorées

--- data/Query/plafond.sql
+++ data/Query/plafond.sql
@@ -100,7 +100,7 @@ FROM
   JOIN formule_resultat          fr ON fr.intervenant_id = i.id AND fr.etat_volume_horaire_id = evh.id
   JOIN statut_intervenant        si ON si.id = i.statut_id
 WHERE
-  fr.total > si.maximum_hetd
+  fr.total - fr.heures_compl_fc_majorees > si.maximum_hetd
   /*i.id*/
 
 UNION

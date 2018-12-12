91a6aba4d10cebd5b30b39ce366eef48205938e1
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2018-12-12 12:06:11 +0100

    Modification de l'infrastructure de gestion de la formule de calcul

--- code/test1.php
+++ code/test1.php
@@ -7,124 +7,20 @@
  * @var $sl         \Zend\ServiceManager\ServiceLocatorInterface
  */
 
-use Application\Service\StructureService;
-use UnicaenApp\Util;
+use Application\Service\EtatVolumeHoraireService;
+use Application\Service\FormuleResultatService;
+use Application\Service\IntervenantService;
+use Application\Service\TypeVolumeHoraireService;
 
-$d = "'M','SCOUARNEC ','Jean-Max','EPS',04/04/1963,'France'
-'M','BERTHELEM ','Eric','EPS',26/10/1956,'France'
-'M','LEVASSEUR','Pierre-Yves','EPS',14/07/1985,'France'
-'MME','LEMEE ','Sabine','EPS',18/09/1977,'France'
-'M','BIDEL','Arnaud','EPS',21/11/1979,'France'
-'M','HENRY','Jean-Luc','EPS',27/06/1977,'France'
-'M','AUSSANT','Jean','EPS',06/12/1970,'France'
-'M','PHILIPPE','Ludovic','EPS',27/05/1977,'France'
-'M','ANDRE ','Vivien','EPS',17/04/1986,'France'
-'M','JEANNE ','Pierre','EPS',15/10/1974,'France'
-'MME','LETEINTURIER ','Caroline','Lettres',26/11/1976,'France'
-'MME','FERNANDES ','Florinda','LVE Anglais',23/01/1974,'France'
-'M','VAUDEVIRE ','Stéphane','LVE Anglais',05/02/1972,'France'
-'M','ROUQUIER ','Philippe','LVE Anglais',10/06/1976,'France'
-'MME','BOUHACEIN ','Caroline','LVE Espagnol',27/10/1973,'France'
-'M','PINSAULT ','Nicolas','LVE Espagnol',13/03/1970,'France'
-'M','BONINI ','Jean-François','LVE Italien',29/12/1956,'Maroc'
-'MME','CARTHY','Karthleen','LVE Italien',12/11/1974,'France'
-'M','LABBATE ','Ettore','LVE Italien',11/07/1973,'Italie'
-'MME','DESCAMPS ','Sarah','LVE Allemand',02/02/1966,'France'
-'MME','LANERY ','Hélène','Mathématiques',30/11/1972,'France'
-'M','LEFEUVRE ','Yann','Mathématiques',04/07/1972,'France'
-'MME','BOBLIN ','Emmanuele','Mathématiques',28/09/1959,'France'
-'MME','WEIBEL','Nathalie','Mathématiques',09/04/1973,'France'
-'MME','RODRIGUES DE OLIVEIR','Lisa','Philosophie',21/06/1973,'France'
-'MME','MACHEFERT ','Hélène','Philosophie',15/05/1972,'France'
-'MME','ARNOUX ','Frédérique','Physique chimie',09/03/1972,'France'
-'MME','CHARMARTY ','Sandrine','Physique chimie',07/11/1973,'France'
-'M','ROUX ','Giovanni','Physique chimie',10/04/1978,'France'
-'MME','SEIGLE ','Mélanie','Physique chimie',20/05/1971,'France'
-'M','ORLANDI','Daniel','SES',02/11/1970,'Italie'
-'M','ANDRE ','Cédric','SES',01/07/1972,'France'
-'M','MOLINA ','Stéphane','SES',05/04/1959,'France'
-'M','COUTABLE ','Grégory','STI',25/09/1972,'France'
-'M','JOUAUX ','François','STI',05/11/1984,'France'
-'M','CAILMAIL ','Philippe','STI',16/04/1972,'France'
-'M','CHARDON','Jean-Max','Champs pro',27/03/1963,'France'
-'MME','DATHEE-BEAUGE','Anne-Gaelle','Champs pro',15/03/1976,'France'
-'MME','LEFORT ','Marie','Champs pro',17/06/1961,'France'
-'MME','LEROY ','Christine','Champs pro',30/01/1962,'France'
-'MME','PICARD ','Dominique','Champs pro',21/10/1964,'France'
-'MME','SEPARI ','Guilaine','Champs pro',04/08/1966,'France'
-'M','GILLES ','Arnaud','SVT',27/11/1981,'France'
-'M','PITT ','Grégoire','SVT',23/09/1986,'France'
-'M','RIBOT ','Nicolas','SVT',08/07/1975,'France'
-'MME','DUVIVIER-FAUCHET','Delphine','SVT',03/06/1973,'France'
-'MME','HENRY ','Catherine','CPE',04/02/1958,'France'";
-
-$d = explode("\n", $d);
-
-$index = 1;
-echo '<pre>';
-foreach ($d as $l) {
-    $l             = explode(',', $l);
-    $civilite      = trim($l[0]) == "'M'" ? 2 : 1;
-    $nom           = trim(substr(trim($l[1]),1,-1));
-    $prenom        = trim(substr(trim($l[2]),1,-1));
-    $dateNaissance = $l[4];
-    $paysNaissance = substr(trim($l[5]),1,-1);
-    $code = '999999'.str_pad((string)($index++), 2, '0', STR_PAD_LEFT);
-
-    $critere = str_replace( '_', ' ', Util::reduce($nom.' '.$prenom));
+$intervenant = $sl->get(IntervenantService::class)->get(648);
+$typeVolumeHoraire = $sl->get(TypeVolumeHoraireService::class)->get(1);
+$etatVolumeHoraire = $sl->get(EtatVolumeHoraireService::class)->get(1);
+$frs = $sl->get(FormuleResultatService::class);
 
 
-    $paysCorresp = [
-        'France' => '456',
-        'Maroc' => '333',
-        'Italie' => '452',
-    ];
-    if (isset($paysCorresp[$paysNaissance])) $paysNaissance = $paysCorresp[$paysNaissance];
+$data = $frs->getData($intervenant, $typeVolumeHoraire, $etatVolumeHoraire);
 
-    $sql = "
-INSERT INTO intervenant (
-  id,
-  civilite_id,
-  nom_usuel,
-  prenom,
-  nom_patronymique,
-  date_naissance,
-  statut_id,
-  structure_id,
-  source_id,
-  source_code,
-  histo_creation,
-  histo_createur_id,
-  histo_modification,
-  histo_modificateur_id,
-  annee_id,
-  critere_recherche,
-  code,
-  supann_emp_id,
-  pays_naissance_id
-) VALUES (
-  INTERVENANT_ID_SEQ.NEXTVAL,-- id,
-  $civilite,-- civilite_id,
-  '$nom',-- nom_usuel,
-  '$prenom',-- prenom,
-  '$nom',-- nom_patronymique,
-  to_date('$dateNaissance', 'dd/mm/YYYY'),-- date_naissance,
-  31,-- statut_id,
-  102,-- structure_id,
-  2,-- source_id,
-  '$code',-- source_code,
-  sysdate,-- histo_creation,
-  4,-- histo_createur_id,
-  sysdate,-- histo_modification,
-  4,-- histo_modificateur_id,
-  2017,-- annee_id,
-  '$critere',-- critere_recherche,
-  '$code', -- code,
-  '$code', -- supann_emp_id,
-  $paysNaissance -- pays_naissance_id
-);
-    ";
+unset($data['structure-affectation']);
 
-    echo $sql;
-}
-echo '</pre>';
+
+var_dump($data );
--- module/Application/src/Application/Service/FormuleResultatService.php
+++ module/Application/src/Application/Service/FormuleResultatService.php
@@ -1,7 +1,17 @@
 <?php
 
 namespace Application\Service;
+
+use Application\Entity\Db\EtatVolumeHoraire;
+use Application\Entity\Db\Intervenant;
 use Application\Entity\Db\Structure;
+use Application\Entity\Db\TypeVolumeHoraire;
+use Application\Service\Traits\ServiceReferentielServiceAwareTrait;
+use Application\Service\Traits\ServiceServiceAwareTrait;
+use Application\Service\Traits\StructureServiceAwareTrait;
+use Application\Service\Traits\TypeInterventionServiceAwareTrait;
+use Application\Service\Traits\VolumeHoraireReferentielServiceAwareTrait;
+use Application\Service\Traits\VolumeHoraireServiceAwareTrait;
 
 /**
  * Description of FormuleResultat
@@ -10,6 +20,14 @@ use Application\Entity\Db\Structure;
  */
 class FormuleResultatService extends AbstractEntityService
 {
+    use ServiceServiceAwareTrait;
+    use ServiceReferentielServiceAwareTrait;
+    use VolumeHoraireServiceAwareTrait;
+    use VolumeHoraireReferentielServiceAwareTrait;
+    use StructureServiceAwareTrait;
+    use TypeInterventionServiceAwareTrait;
+
+
 
     /**
      * retourne la classe des entités
@@ -50,16 +68,16 @@ class FormuleResultatService extends AbstractEntityService
         if (!$structure) return $this->getTotalPrevisionnelValideWS(); // on ByPasse!!!
 
         $params = [
-             'structure' => (integer)$structure->getId(),
-             'annee' => (integer)$this->getServiceContext()->getAnnee()->getId(),
+            'structure' => (integer)$structure->getId(),
+            'annee'     => (integer)$this->getServiceContext()->getAnnee()->getId(),
         ];
 
-        $sql         = 'SELECT heures FROM V_HETD_PREV_VAL_STRUCT WHERE structure_id = :structure AND annee_id = :annee';
-        $sr = $this->getEntityManager()->getConnection()->executeQuery($sql, $params)->fetch();
+        $sql = 'SELECT heures FROM V_HETD_PREV_VAL_STRUCT WHERE structure_id = :structure AND annee_id = :annee';
+        $sr  = $this->getEntityManager()->getConnection()->executeQuery($sql, $params)->fetch();
 
-        if (isset($sr['HEURES'])){
+        if (isset($sr['HEURES'])) {
             return (float)$sr['HEURES'];
-        }else{
+        } else {
             return (float)0;
         }
     }
@@ -72,19 +90,161 @@ class FormuleResultatService extends AbstractEntityService
             'annee' => (integer)$this->getServiceContext()->getAnnee()->getId(),
         ];
 
-        $sql         = 'SELECT structure_id, heures FROM V_HETD_PREV_VAL_STRUCT WHERE annee_id = :annee';
+        $sql  = 'SELECT structure_id, heures FROM V_HETD_PREV_VAL_STRUCT WHERE annee_id = :annee';
         $stmt = $this->getEntityManager()->getConnection()->executeQuery($sql, $params);
 
         $res = ['total' => 0];
-        while( $d = $stmt->fetch()){
+        while ($d = $stmt->fetch()) {
             $structureId = (int)$d['STRUCTURE_ID'];
-            $heures = (float)$d['HEURES'];
+            $heures      = (float)$d['HEURES'];
 
             $res[$structureId] = $heures;
-            $res['total'] += $heures;
-            $res['total'] += $heures;
+            $res['total']      += $heures;
+            $res['total']      += $heures;
         }
 
         return $res;
+    }
+
+
+
+    /**
+     * @param Intervenant       $intervenant
+     * @param TypeVolumeHoraire $typeVolumeHoraire
+     * @param EtatVolumeHoraire $etatVolumeHoraire
+     *
+     * @return array
+     */
+    public function getData(Intervenant $intervenant, TypeVolumeHoraire $typeVolumeHoraire, EtatVolumeHoraire $etatVolumeHoraire): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $params = [
+            'intervenant'       => $intervenant->getId(),
+            'typeVolumeHoraire' => $typeVolumeHoraire->getId(),
+            'etatVolumeHoraire' => $etatVolumeHoraire->getId(),
+        ];
+
+        /* Données générales */
+        $data = [
+            'STRUCTURE'                      => $intervenant->getStructure(),
+            'typeVolumeHoraire'              => $typeVolumeHoraire,
+            'etatVolumeHoraire'              => $etatVolumeHoraire,
+            'HEURES_SERVICE_STATUTAIRE'      => $intervenant->getStatut()->getServiceStatutaire(),
+            'HEURES_SERVICE_MODIFIE'         => 0,
+            'HEURES_DECHARGE'                => 0,
+            'DEPASSEMENT_SERVICE_DU_SANS_HC' => false,
+            's'                              => [],
+            'r'                              => [],
+            'types-intervention'             => [],
+            'has-ponderation-service-compl'  => false,
+            'has-calcul'                     => false,
+        ];
+
+        $sql = "SELECT * FROM v_formule_intervenant WHERE intervenant_id = :intervenant";
+        $id  = $conn->fetchAssoc($sql, $params);
+        if ($id) {
+            $data['HEURES_SERVICE_STATUTAIRE']      = $id['HEURES_SERVICE_STATUTAIRE'];
+            $data['HEURES_SERVICE_MODIFIE']         = $id['HEURES_SERVICE_MODIFIE'];
+            $data['HEURES_DECHARGE']                = $id['HEURES_DECHARGE'];
+            $data['DEPASSEMENT_SERVICE_DU_SANS_HC'] = $id['DEPASSEMENT_SERVICE_DU_SANS_HC'] == '1';
+        }
+
+        /* Volumes horaires */
+        $sql  = "
+        SELECT fvh.* FROM v_formule_volume_horaire fvh WHERE 
+          fvh.intervenant_id = :intervenant
+          AND fvh.type_volume_horaire_id = :typeVolumeHoraire
+          AND fvh.etat_volume_horaire_id >= :etatVolumeHoraire
+        ";
+        $vhds = $conn->fetchAll($sql, $params);
+        foreach ($vhds as $vhd) {
+            if ($vhd['VOLUME_HORAIRE_ID']) {
+                $dsId = (int)$vhd['SERVICE_ID'];
+                $tiId = (int)$vhd['TYPE_INTERVENTION_ID'];
+
+                if (!isset($data['s'][$dsId])) {
+                    $service          = $this->getServiceService()->get($dsId);
+                    $data['s'][$dsId] = [
+                        'element-etablissement'     => $service->getElementPedagogique() ? $service->getElementPedagogique() : $service->getEtablissement(),
+                        'structure'                 => $this->getServiceStructure()->get($vhd['STRUCTURE_ID']),
+                        'TAUX_FI'                   => $vhd['TAUX_FI'],
+                        'TAUX_FA'                   => $vhd['TAUX_FA'],
+                        'TAUX_FC'                   => $vhd['TAUX_FC'],
+                        'PONDERATION_SERVICE_DU'    => (float)$vhd['PONDERATION_SERVICE_DU'],
+                        'PONDERATION_SERVICE_COMPL' => (float)$vhd['PONDERATION_SERVICE_COMPL'],
+                        'SERVICE_STATUTAIRE'        => $vhd['SERVICE_STATUTAIRE'] == '1',
+                        'heures'                    => [],
+                        'hetd'                      => [],
+                    ];
+                }
+                if (!isset($data['s'][$dsId]['heures'][$tiId])) {
+                    $data['s'][$dsId]['heures'][$tiId] = 0;
+                }
+                if (!isset($data['types-intervention'][$tiId])) {
+                    $data['types-intervention'][$tiId] = $this->getServiceTypeIntervention()->get($tiId);
+                }
+
+                $data['s'][$dsId]['heures'][$tiId] += (float)$vhd['HEURES'];
+            } else {
+                $drId = (int)$vhd['SERVICE_REFERENTIEL_ID'];
+                if (!isset($data['r'][$drId])) {
+                    $data['r'][$drId] = [
+                        'fonction'           => $this->getServiceServiceReferentiel()->get($drId)->getFonction(),
+                        'structure'          => $this->getServiceStructure()->get($vhd['STRUCTURE_ID']),
+                        'SERVICE_STATUTAIRE' => $vhd['SERVICE_STATUTAIRE'] == '1',
+                        'heures'             => 0,
+                        'hetd'               => [],
+                    ];
+                }
+                $data['r'][$drId]['heures'] += (float)$vhd['HEURES'];
+            }
+        }
+
+        /* Résultats de formule */
+        $sql = "SELECT * FROM formule_resultat WHERE intervenant_id = :intervenant
+            AND type_volume_horaire_id = :typeVolumeHoraire
+            AND etat_volume_horaire_id = :etatVolumeHoraire";
+        $fr  = $conn->fetchAssoc($sql, $params);
+        if ($fr) {
+            $data['has-calcul'] = true;
+            $frId               = $fr['ID'];
+            unset($fr['ID']);
+            foreach ($fr as $k => $v) {
+                $data[$k] = $v;
+            }
+            $sql  = "SELECT * FROM formule_resultat_service WHERE formule_resultat_id = :frId";
+            $frss = $conn->fetchAll($sql, compact('frId'));
+            foreach ($frss as $frs) {
+                $dsId = $frs['SERVICE_ID'];
+                if (isset($data['s'][$dsId])) {
+                    unset($frs['ID']);
+                    unset($frs['FORMULE_RESULTAT_ID']);
+                    unset($frs['SERVICE_ID']);
+                    unset($frs['TOTAL']);
+                    $data['s'][$dsId]['hetd'] = $frs;
+                }
+            }
+
+            $sql  = "SELECT * FROM formule_resultat_service_ref WHERE formule_resultat_id = :frId";
+            $frss = $conn->fetchAll($sql, compact('frId'));
+            foreach ($frss as $frs) {
+                $drId = $frs['SERVICE_REFERENTIEL_ID'];
+                if (isset($data['r'][$drId])) {
+                    unset($frs['ID']);
+                    unset($frs['FORMULE_RESULTAT_ID']);
+                    unset($frs['SERVICE_REFERENTIEL_ID']);
+                    unset($frs['TOTAL']);
+                    $data['r'][$drId]['hetd'] = $frs;
+                }
+            }
+        }
+
+        /* Tri final */
+        usort($data['types-intervention'], function ($ti1, $ti2) {
+            return $ti1->getOrdre() > $ti2->getOrdre();
+        });
+
+        return $data;
     }
 }

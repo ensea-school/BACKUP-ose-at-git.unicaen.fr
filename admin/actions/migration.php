f382949d3eac4bc392d5c1192af3f054255579d5
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-06-19 09:13:08 +0200

    Ajout des nouvelles colonnes dans le script de migration pour les autres établissements!!

--- admin/actions/migration.php
+++ admin/actions/migration.php
@@ -3,3 +3,6 @@
 // Script provisoire : il aura disparu à la prochaine version!!!
 $c->println('Mise à jour automatique de la base de données', $c::COLOR_LIGHT_CYAN);
 $oa->run('update-bdd');
+
+$oa->getBdd()->exec('alter table utilisateur add PASSWORD_RESET_TOKEN varchar2(256) default null');
+$oa->getBdd()->exec('create unique index USER_PASSWORD_RESET_TOKEN_UN on utilisateur (PASSWORD_RESET_TOKEN)');

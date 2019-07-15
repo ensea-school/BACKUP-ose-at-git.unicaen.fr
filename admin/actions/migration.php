a14be26ffd2aac54a42f9bfce59bace680a03d13
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-07-15 16:15:23 +0200

    Scritps de migration exécutés 2x!!

--- admin/actions/migration.php
+++ admin/actions/migration.php
@@ -6,9 +6,4 @@ $c->println('Mise à jour automatique de la base de données', $c::COLOR_LIGHT_C
 $oa->oldVersion = $oa->purgerVersion($c->getArg(2));
 $oa->version = $oa->purgerVersion($c->getArg(3));
 
-$c->println('ancienne version='.$oa->oldVersion);
-$c->println('nouvelle version='.$oa->version);
-
-$oa->migration('pre');
 $oa->run('update-bdd');
-$oa->migration('post');

9cb93faea12e58a78b7169812864d3604bfe311e
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-10-07 16:07:21 +0200

    Néttoyage du code de test d'admin

--- admin/actions/test.php
+++ admin/actions/test.php
@@ -3,21 +3,3 @@
 $bdd    = new \BddAdmin\Bdd(Config::get('bdds', 'dev-local'));
 $schema = new \BddAdmin\Schema($bdd);
 
-
-$osedir = $oa->getOseDir();
-
-$c->println("\nNettoyage des caches et mise à jour des proxies", $c::COLOR_LIGHT_CYAN);
-try {
-    $c->exec([
-        "cd $osedir",
-        "rm -Rf cache/*",
-        "php admin/actions/doctrine.php orm:generate-proxies",
-        "chmod -R 777 cache/DoctrineProxy",
-        "chmod -R 777 cache/Doctrine",
-    ], false);
-    $c->println('Cache nettoyé, proxies actualisés', $c::COLOR_GREEN);
-} catch (\Exception $e) {
-    $c->println($e->getMessage());
-    $c->println('Un problème est survenu : le cache de OSE n\'a pas été vidé. '
-        .'Merci de supprimer le contenu du répertoire /cache de OSE, pis de lancer la commande ./bin/ose clear-cache pour y remédier', $c::COLOR_RED);
-}

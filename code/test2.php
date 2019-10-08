b91800eb3b3f7caebfe3ba2775b5c1455cfaa523
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-10-08 09:18:06 +0200

    Début de test du bug des feuilles de route

--- code/test2.php
+++ code/test2.php
@@ -6,3 +6,38 @@
  * @var $viewName   string
  * @var $sl         \Zend\ServiceManager\ServiceLocatorInterface
  */
+
+$sql = "
+SELECT
+  w.annee_id,
+  w.intervenant_id,
+  si.source_code statut_intervenant,
+  s.libelle_court structure,
+  w.etape_code,
+  w.atteignable,
+  w.objectif,
+  w.realisation
+FROM
+  tbl_workflow w
+  JOIN wf_etape e ON e.id = w.etape_id
+  JOIN statut_intervenant si ON si.id = w.statut_intervenant_id
+  LEFT JOIN structure s ON s.id = w.structure_id
+ORDER BY
+  w.intervenant_id,
+  e.ordre
+";
+
+
+/** @var $em \Doctrine\ORM\EntityManager */
+$em = $container->get(\Application\Constants::BDD);
+
+$ids = $em->getConnection()->query($sql);
+$feuilles = [];
+
+foreach( $ids as $id ){
+    $intervenant = (int)$id['INTERVENANT_ID'];
+    unset($id['INTERVENANT_ID']);
+    $feuilles[$intervenant][] = $id;
+}
+
+var_dump($feuilles);

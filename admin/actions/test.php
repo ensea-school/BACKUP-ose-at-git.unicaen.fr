2a70b378b992e8fa025f4a75d12aeaaeeb8c0345
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-11-04 17:32:48 +0100

    Suppression de code de test

--- admin/actions/test.php
+++ admin/actions/test.php
@@ -1,19 +1,1 @@
 <?php
-
-$bdd    = new \BddAdmin\Bdd(Config::get('bdds', 'deploy-local'));
-$oa->setBdd($bdd);
-$bdd->debug = true;
-/* Insertion des données */
-$dataGen = new DataGen($oa);
-
-$table = null;
-$table = 'PRIVILEGE';
-
-//$bdd->getTable($table)->delete();
-
-$dataGen->install($table);
-
-if ($table) {
-    $r = $oa->getBdd()->getTable($table)->select();
-    var_dump(count($r));
-}

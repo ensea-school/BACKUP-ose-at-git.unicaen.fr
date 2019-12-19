b0a55b944ebacd4e24c975194c161f7701a52898
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2019-12-19 16:35:34 +0100

    Mise à jour des procédures d'installation, déplacement des mises à jour de liens publics Unicaen

--- admin/src/OseAdmin.php
+++ admin/src/OseAdmin.php
@@ -75,53 +75,6 @@ class OseAdmin
 
 
 
-    public function majUnicaenSymLinks(): bool
-    {
-        $oseDir = $this->getOseDir();
-
-        /** @todo à supprimer mi-2020 */
-        if (file_exists($oseDir . "public/vendor/unicaen/app/unicaen")) {
-            $this->console->exec("rm $oseDir" . "public/vendor/unicaen/app");
-        }
-
-        $oldLibs = [];
-        $od      = array_filter(glob($oseDir . 'public/vendor/unicaen/*'), 'is_dir');
-        foreach ($od as $dir) {
-            $oldLibs[] = basename($dir);
-        }
-
-        $newLibs = [];
-        $nd      = array_filter(glob($oseDir . 'vendor/unicaen/*'), 'is_dir');
-        foreach ($nd as $dir) {
-            if (is_dir($dir . '/public')) {
-                $newLibs[] = basename($dir);
-            }
-        }
-
-        $deleteLibs = array_diff($oldLibs, $newLibs);
-        $createLibs = array_diff($newLibs, $oldLibs);
-
-        foreach ($deleteLibs as $lib) {
-            $command = "rm $oseDir" . "public/vendor/unicaen/$lib";
-            $this->console->print($command);
-            $this->console->exec($command);
-        }
-
-        foreach ($createLibs as $lib) {
-            if (is_dir($oseDir . "/vendor/unicaen/$lib/public/unicaen/$lib")) {
-                $command = "cd $oseDir" . "public/vendor/unicaen;ln -sf ../../../vendor/unicaen/$lib/public/unicaen/$lib $lib";
-            } else {
-                $command = "cd $oseDir" . "public/vendor/unicaen;ln -sf ../../../vendor/unicaen/$lib/public $lib";
-            }
-            $this->console->print($command);
-            $this->console->exec($command);
-        }
-
-        return !(empty($deleteLibs) && empty($createLibs));
-    }
-
-
-
     public function gitlabIsReachable(): bool
     {
         $gitCheck = $this->console->exec("git ls-remote --heads " . self::OSE_ORIGIN, false);

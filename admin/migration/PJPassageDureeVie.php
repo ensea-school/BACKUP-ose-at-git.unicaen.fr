d249af20fb32d13441598a39ab656ebc4e6df326
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2020-03-10 11:06:56 +0100

    Ajout du calcul des TBLS dans le script de migration

--- admin/migration/PJPassageDureeVie.php
+++ admin/migration/PJPassageDureeVie.php
@@ -56,6 +56,17 @@ class PJPassageDureeVie extends AbstractMigration
         $bdd->exec($sql);
         $this->manager->supprimerSauvegarde('TPJS_AJOUT_DUREE_VIE');
 
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe_demande");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe_demande\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe_fournie");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe_fournie\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("");
+
         /* On fournie et on valide de force les PJ qui n'étaient pas tout le temps demandées avant */
         $sql   = '
         SELECT
@@ -110,6 +121,19 @@ class PJPassageDureeVie extends AbstractMigration
             $bdd->getTable('PIECE_JOINTE_FICHIER')->insert($pjf, $o);
             $this->manager->getOseAdmin()->getConsole()->print("Insertion des pièces jointes manquantes : $count / " . count($r) . "\r");
         }
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe_demande");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe_demande\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe_fournie");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe_fournie\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord piece_jointe");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'piece_jointe\'); END;');
+
+        $this->manager->getOseAdmin()->getConsole()->println("Calcul du tableau de bord workflow");
+        $bdd->exec('BEGIN unicaen_tbl.calculer(\'workflow\'); END;');
+
         $this->manager->getOseAdmin()->getConsole()->println("Terminé");
     }
 

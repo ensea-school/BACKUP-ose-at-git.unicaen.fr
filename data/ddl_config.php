ac55e540808568ee375393fdba3637048046a251
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2020-04-24 17:36:50 +0200

    MAJ config pour ne plus supprimer des objets DDL qui ont été supprimés <= v14

--- data/ddl_config.php
+++ data/ddl_config.php
@@ -3,49 +3,24 @@
 return [
     'explicit'          => true,
     'table'             => ['includes' => [
-        'TYPE_DOTATION',
-        'ELEMENT_TAUX_REGIMES_SAVE',
-        'TBL_DOSSIER2',
-        'FORMULE_TEST_STRUCTURE',
+        'ADRESSE_INTERVENANT',
+        'ADRESSE_STRUCTURE',
+
     ]],
     'materialized-view' => ['includes' => [
 
     ]],
     'view'              => ['includes' => [
-        'V_FORMULE_LOCAL_I_PARAMS',
-        'V_FORMULE_LOCAL_VH_PARAMS',
-        'V_TYPE_INTERVENTION_REGLE_EP',
-        'V_INTERVENANT_RECHERCHE',
-        'V_FORMULE_SERVICE',
-        'V_FORMULE_SERVICE_MODIFIE',
-        'V_FORMULE_SERVICE_REF',
-        'V_FORMULE_VOLUME_HORAIRE_REF',
-        'V_WORKFLOW_ETAPE_PERTINENTE',
-        'V_TBL_WORKFLOW',
+
     ]],
     'package'           => ['includes' => [
-        'OSE_IMPORT',
-        'UNICAEN_OSE_FORMULE',
+
     ]],
     'trigger'           => ['includes' => [
-        'TYPE_INTERVENTION_STRUCTURE_CK',
+
     ]],
     'sequence'          => ['includes' => [
-        'FORMULE_ID_SEQ',
-        'MESSAGE_ID_SEQ',
-        'PACKAGE_DEPS_ID_SEQ',
-        'PERSONNEL_ID_SEQ',
-        'TBL_CHARGENS_SEUILS_ID_SEQ',
-        'TBL_CONFIG_CLES_ID_SEQ',
-        'TBL_CONFIG_ID_SEQ',
-        'TBL_DEPENDANCES_ID_SEQ',
-        'TMP_CALCUL_ID_SEQ',
-        'TYPE_INTERVENTION_REGLE_ID_SEQ',
-        'TYPE_STRUCTURE_ID_SEQ',
-        'DROP SEQUENCE ANNEE_ID_SEQ',
-        'ETAT_VOLUME_HORAIRE_ID_SEQ',
-        'TYPE_VOLUME_HORAIRE_ID_SEQ',
-        'ANNEE_ID_SEQ',
+        
     ]],
     'materialized-view' => ['includes' => [
 

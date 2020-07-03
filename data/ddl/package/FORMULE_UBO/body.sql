cf230941f78414eb28a59fb27d529ad3a65cb167
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2020-07-03 10:38:02 +0200

    Quasi finalisation de l'optimisation du calcul des TBLs

--- data/ddl/package/FORMULE_UBO/body.sql
+++ data/ddl/package/FORMULE_UBO/body.sql
@@ -942,8 +942,8 @@ CREATE OR REPLACE PACKAGE BODY "FORMULE_UBO" AS
     RETURN '
     SELECT
       fi.*,
-      CASE WHEN si.source_code IN (''ENS_CH'',''ASS_MI_TPS'',''ENS_CH_LRU'',''DOCTOR'') THEN ''oui'' ELSE ''non'' END param_1,
-	    CASE WHEN si.source_code IN (''LECTEUR'',''ATER'') THEN ''oui'' ELSE ''non'' END param_2,
+      CASE WHEN si.code IN (''ENS_CH'',''ASS_MI_TPS'',''ENS_CH_LRU'',''DOCTOR'') THEN ''oui'' ELSE ''non'' END param_1,
+	    CASE WHEN si.code IN (''LECTEUR'',''ATER'') THEN ''oui'' ELSE ''non'' END param_2,
       NULL param_3,
       NULL param_4,
       NULL param_5

a7f8a0c671ce7f02afce767c3da5d5cba63f155e
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2021-03-02 16:20:19 +0100

    DBLINK sur SRC_HARPEGE_STRUCTURE_CODES.sql

--- doc/Connecteurs Import/Octopus/SRC_HARPEGE_STRUCTURE_CODES.sql
+++ doc/Connecteurs Import/Octopus/SRC_HARPEGE_STRUCTURE_CODES.sql
@@ -2,6 +2,6 @@ CREATE
 OR REPLACE VIEW SRC_HARPEGE_STRUCTURE_CODES AS
 SELECT vs.code  c_structure,
        vs2.code c_structuren2
-FROM v_structure@ vs
-         JOIN v_structure vs2 ON vs.niv2_id = vs2.id
-WHERE vs.racine_id = (SELECT id FROM v_structure WHERE code = 'UNIV')
+FROM octo.v_structure@octoprod vs
+         JOIN octo.v_structure@octoprod vs2 ON vs.niv2_id = vs2.id
+WHERE vs.racine_id = (SELECT id FROM octo.v_structure@octoprod WHERE code = 'UNIV')

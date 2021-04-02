4ccd92e0dc6a14d9ea13a12113a40e54de90d1d9
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-04-02 09:53:31 +0200

    Pouvoir récupérer les 2 nouveaux paramètres pour cusomiser les paiements

--- data/ddl/package/OSE_PARAMETRE/body.sql
+++ data/ddl/package/OSE_PARAMETRE/body.sql
@@ -2,6 +2,10 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PARAMETRE" AS
 
   cache_ose_user NUMERIC;
   cache_annee_id NUMERIC;
+  regle_paiement_annee_civile VARCHAR2(50);
+  pourc_s1_pour_annee_civile FLOAT;
+
+
 
   FUNCTION get_etablissement return Numeric AS
     etab_id numeric;
@@ -9,6 +13,8 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PARAMETRE" AS
     select to_number(valeur) into etab_id from parametre where nom = 'etablissement';
     RETURN etab_id;
   END get_etablissement;
+
+
 
   FUNCTION get_annee return Numeric AS
     annee_id numeric;
@@ -19,12 +25,16 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PARAMETRE" AS
     RETURN cache_annee_id;
   END get_annee;
 
+
+
   FUNCTION get_annee_import RETURN NUMERIC AS
     annee_id NUMERIC;
   BEGIN
     SELECT to_number(valeur) INTO annee_id FROM parametre WHERE nom = 'annee_import';
     RETURN annee_id;
   END get_annee_import;
+
+
 
   FUNCTION get_ose_user return NUMERIC AS
     ose_user_id numeric;
@@ -34,6 +44,8 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PARAMETRE" AS
     cache_ose_user := ose_user_id;
     RETURN cache_ose_user;
   END get_ose_user;
+
+
 
   FUNCTION get_formule RETURN formule%rowtype IS
     fdata formule%rowtype;
@@ -45,6 +57,28 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PARAMETRE" AS
       JOIN parametre p ON f.id = to_number(p.valeur)
     WHERE p.nom = 'formule';
     RETURN fdata;
+  END;
+
+
+
+  FUNCTION get_regle_paiement_annee_civ RETURN VARCHAR2 IS
+  BEGIN
+    IF regle_paiement_annee_civile IS NULL THEN
+      SELECT valeur INTO regle_paiement_annee_civile FROM parametre WHERE nom = 'regle_paiement_annee_civile';
+    END IF;
+
+    RETURN regle_paiement_annee_civile;
+  END;
+
+
+
+  FUNCTION get_pourc_s1_annee_civ RETURN VARCHAR2 IS
+  BEGIN
+    IF pourc_s1_pour_annee_civile IS NULL THEN
+      SELECT TO_NUMBER(valeur, '999999999D999999999', 'NLS_NUMERIC_CHARACTERS =.,') INTO pourc_s1_pour_annee_civile FROM parametre WHERE nom = 'pourc_s1_pour_annee_civile';
+    END IF;
+
+    RETURN pourc_s1_pour_annee_civile;
   END;
 
 END OSE_PARAMETRE;
--- data/ddl/package/OSE_PARAMETRE/definition.sql
+++ data/ddl/package/OSE_PARAMETRE/definition.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE PACKAGE "OSE_PARAMETRE" AS
 
-  function get_etablissement return Numeric;
-  function get_annee return Numeric;
-  function get_annee_import return Numeric;
-  function get_ose_user return Numeric;
-  function get_formule RETURN formule%rowtype;
+  FUNCTION get_etablissement RETURN Numeric;
+  FUNCTION get_annee RETURN Numeric;
+  FUNCTION get_annee_import RETURN Numeric;
+  FUNCTION get_ose_user RETURN Numeric;
+  FUNCTION get_formule RETURN formule%rowtype;
+  FUNCTION get_regle_paiement_annee_civ RETURN VARCHAR2;
+  FUNCTION get_pourc_s1_annee_civ RETURN VARCHAR2;
 
 END OSE_PARAMETRE;

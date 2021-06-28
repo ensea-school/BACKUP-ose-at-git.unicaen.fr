f80da3985edf3ef5d50ec3c05dc228f62d7019dc
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-06-28 11:28:50 +0200

    reformatage de la conig de DDL

--- data/ddl_config.php
+++ data/ddl_config.php
@@ -9,24 +9,37 @@ return [
             'INTERVENANT_SAISIE',
             'DOSSIER',
             'TBL_DEMS',
+        ],
+    ],
+    'materialized-view' => [
+        'includes' => [
 
         ],
-        'excludes' => ['ACT_%'],
+        'excludes' => [
+            'MV_EXT_SERVICE',
+        ],
     ],
-    'materialized-view' => ['includes' => [
+    'view'              => [
+        'includes' => [
+            'V_INDIC_DIFF_DOSSIER',
+            'V_MEP_INTERVENANT_STRUCTURE',
+        ],
+    ],
+    'package'           => [
+        'includes' => [
+            'FORMULE_ENSICAEN',
+        ],
+    ],
+    'trigger'           => [
+        'includes' => [
+            'F_CONTRAT',
+            'F_CONTRAT_S',
+            'INDIC_TRG_MODIF_DOSSIER',
+        ],
+    ],
+    'sequence'          => [
+        'includes' => [
 
-    ], 'excludes'                      => ['MV_EXT_SERVICE']],
-    'view'              => ['includes' => [
-        'V_INDIC_DIFF_DOSSIER',
-        'V_MEP_INTERVENANT_STRUCTURE',
-    ]],
-    'package'           => ['includes' => [
-
-    ], 'excludes'                      => 'OSE_ACTUL'],
-    'trigger'           => ['includes' => [
-        'F_CONTRAT', 'F_CONTRAT_S', 'INDIC_TRG_MODIF_DOSSIER',
-    ]],
-    'sequence'          => ['includes' => [
-
-    ]],
+        ],
+    ],
 ];

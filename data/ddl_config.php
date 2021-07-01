78e52e3e2d8f5aa1308a7db24319ddaa9cfcc294
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-07-01 12:25:02 +0200

    suite...

--- data/ddl_config.php
+++ data/ddl_config.php
@@ -12,18 +12,18 @@ return [
     ]],
     'materialized-view' => ['includes' => [
 
-    ],
-                            'view'     => ['includes' => [
-                                'V_INDIC_DIFF_DOSSIER',
-                                'V_MEP_INTERVENANT_STRUCTURE',
-                            ]],
-                            'package'  => ['includes' => [
+    ]],
+    'view'              => ['includes' => [
+        'V_INDIC_DIFF_DOSSIER',
+        'V_MEP_INTERVENANT_STRUCTURE',
+    ]],
+    'package'           => ['includes' => [
 
-                            ]],
-                            'trigger'  => ['includes' => [
-                                'F_CONTRAT', 'F_CONTRAT_S', 'INDIC_TRG_MODIF_DOSSIER',
-                            ]],
-                            'sequence' => ['includes' => [
+    ]],
+    'trigger'           => ['includes' => [
+        'F_CONTRAT', 'F_CONTRAT_S', 'INDIC_TRG_MODIF_DOSSIER',
+    ]],
+    'sequence'          => ['includes' => [
 
-                            ]],
-    ];
+    ]],
+];

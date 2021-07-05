0af1409aa03951845a9a402431bba561d43b343b
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-07-05 17:30:58 +0200

    Correction de plusieurs bugs au niveau du calcul des charges d'enseignement

--- data/ddl/view/V_CHARGENS_SEUILS_DED_DEF.sql
+++ data/ddl/view/V_CHARGENS_SEUILS_DED_DEF.sql
@@ -1,32 +1,41 @@
 CREATE OR REPLACE FORCE VIEW V_CHARGENS_SEUILS_DED_DEF AS
-WITH tisc AS (
-  SELECT DISTINCT
-    sc.type_intervention_id,
-    sc.scenario_id
+WITH seuils_perso AS (
+  SELECT
+    n.element_pedagogique_id,
+    n.etape_id,
+    sn.scenario_id,
+    sns.type_intervention_id,
+    sns.ouverture,
+    sns.dedoublement,
+    sns.assiduite
   FROM
-    seuil_charge sc
+    scenario_noeud_seuil sns
+    JOIN scenario_noeud sn ON sn.id = sns.scenario_noeud_id AND sn.histo_destruction IS NULL
+    JOIN noeud n ON n.id = sn.noeud_id
   WHERE
-    sc.histo_destruction IS NULL
+    sns.dedoublement IS NOT NULL
 )
 SELECT
-  n.noeud_id noeud_id,
-  tisc.scenario_id,
-  tisc.type_intervention_id,
-  COALESCE(snsetp.dedoublement, tcsd.dedoublement) dedoublement
+  n.noeud_id                                                   noeud_id,
+  s.id                                                         scenario_id,
+  ti.id                                                        type_intervention_id,
+  COALESCE(sep.ouverture, se.ouverture)                        ouverture,
+  COALESCE(sep.dedoublement, se.dedoublement, sd.dedoublement) dedoublement,
+  sep.assiduite                                                assiduite
 FROM
-            tbl_noeud                  n
-       JOIN                         tisc ON 1=1
+            tbl_noeud n
+       JOIN scenario                  s ON s.histo_destruction IS NULL
+       JOIN type_intervention        ti ON ti.histo_destruction IS NULL
+  LEFT JOIN seuils_perso            sep ON sep.element_pedagogique_id = n.element_pedagogique_id
+                                       AND sep.scenario_id = s.id
+                                       AND sep.type_intervention_id = ti.id
 
-  LEFT JOIN scenario_noeud         snetp ON snetp.noeud_id = n.noeud_etape_id
-                                        AND snetp.scenario_id = tisc.scenario_id
-                                        AND snetp.histo_destruction IS NULL
+  LEFT JOIN seuils_perso             se ON se.etape_id = n.element_pedagogique_etape_id
+                                       AND sep.scenario_id = s.id
+                                       AND sep.type_intervention_id = ti.id
 
-  LEFT JOIN scenario_noeud_seuil  snsetp ON snsetp.scenario_noeud_id = snetp.id
-                                        AND snsetp.type_intervention_id = tisc.type_intervention_id
-
-  LEFT JOIN tbl_chargens_seuils_def tcsd ON tcsd.annee_id = n.annee_id
-                                        AND tcsd.scenario_id = tisc.scenario_id
-                                        AND tcsd.groupe_type_formation_id = n.groupe_type_formation_id
-                                        AND tcsd.type_intervention_id = tisc.type_intervention_id
-WHERE
-  COALESCE(snsetp.dedoublement, tcsd.dedoublement)  IS NOT NULL
+  LEFT JOIN tbl_chargens_seuils_def  sd ON sd.annee_id = n.annee_id
+                                       AND sd.scenario_id = s.id
+                                       AND sd.structure_id = n.structure_etape_id
+                                       AND sd.groupe_type_formation_id = n.groupe_type_formation_id
+                                       AND sd.type_intervention_id = ti.id

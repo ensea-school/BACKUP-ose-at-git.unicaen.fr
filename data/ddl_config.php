6e8a73e1d6e06c7501fc142fe1bdafdf25d192e9
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-09-13 14:44:41 +0200

    MAJ Composer + DDL Pos

--- data/ddl_config.php
+++ data/ddl_config.php
@@ -17,10 +17,12 @@ return [
         ],
     ],
     'view'              => [
-        'includes' => [
+        'includes'    => [
             'V_INDIC_DIFF_DOSSIER',
             'V_MEP_INTERVENANT_STRUCTURE',
             'V_CHARGENS_SEUILS_DED_DEF',
+        ], 'excludes' => [
+            'V_TBL_PLAFOND_%', // Les vues plafonds sont générées et non créées à partir de la DDL
         ],
     ],
     'package'           => [

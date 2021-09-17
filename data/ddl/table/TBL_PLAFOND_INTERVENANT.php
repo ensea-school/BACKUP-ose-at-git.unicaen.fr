2842231cc8c3d9cfde77e167381b7821d97bda3a
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-09-17 11:48:55 +0200

    Revue des positionnements de colonnes

--- data/ddl/table/TBL_PLAFOND_INTERVENANT.php
+++ data/ddl/table/TBL_PLAFOND_INTERVENANT.php
@@ -18,7 +18,7 @@ return [
             'precision'   => NULL,
             'nullable'    => FALSE,
             'default'     => NULL,
-            'position'    => 3,
+            'position'    => 4,
             'commentaire' => NULL,
         ],
         'DEROGATION'             => [
@@ -30,7 +30,7 @@ return [
             'precision'   => 126,
             'nullable'    => FALSE,
             'default'     => '0',
-            'position'    => 8,
+            'position'    => 9,
             'commentaire' => 'Dérogation',
         ],
         'HEURES'                 => [
@@ -42,7 +42,7 @@ return [
             'precision'   => 126,
             'nullable'    => FALSE,
             'default'     => '0',
-            'position'    => 6,
+            'position'    => 7,
             'commentaire' => 'Nombre d\'heures pour l\'intervenant concerné',
         ],
         'ID'                     => [
@@ -66,7 +66,7 @@ return [
             'precision'   => NULL,
             'nullable'    => FALSE,
             'default'     => NULL,
-            'position'    => 5,
+            'position'    => 6,
             'commentaire' => NULL,
         ],
         'PLAFOND'                => [
@@ -78,7 +78,7 @@ return [
             'precision'   => 126,
             'nullable'    => FALSE,
             'default'     => '0',
-            'position'    => 7,
+            'position'    => 8,
             'commentaire' => 'Valeur du plafond',
         ],
         'PLAFOND_ETAT_ID' => [
@@ -114,7 +114,7 @@ return [
             'precision'   => NULL,
             'nullable'    => TRUE,
             'default'     => NULL,
-            'position'    => 4,
+            'position'    => 5,
             'commentaire' => NULL,
         ],
     ],

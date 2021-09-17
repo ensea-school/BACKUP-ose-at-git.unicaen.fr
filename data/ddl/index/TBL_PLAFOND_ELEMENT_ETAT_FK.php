5b092225e5ccfd488d5234d8063952f6082a08d3
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2021-09-17 11:54:42 +0200

    Ajout de l'état dans les vues et les tables (suite)

---
+++ data/ddl/index/TBL_PLAFOND_ELEMENT_ETAT_FK.php
@@ -0,0 +1,14 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'TBL_PLAFOND_ELEMENT_ETAT_FK',
+    'unique'  => FALSE,
+    'table'   => 'TBL_PLAFOND_ELEMENT',
+    'columns' => [
+        'PLAFOND_ETAT_ID',
+    ],
+];
+
+//@formatter:on
---
+++ data/ddl/ref-constraint/TBL_PLAFOND_ELEMENT_ETAT_FK.php
@@ -0,0 +1,16 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'        => 'TBL_PLAFOND_ELEMENT_ETAT_FK',
+    'table'       => 'TBL_PLAFOND_ELEMENT',
+    'rtable'      => 'PLAFOND_ETAT',
+    'delete_rule' => NULL,
+    'index'       => NULL,
+    'columns'     => [
+        'PLAFOND_ETAT_ID' => 'ID',
+    ],
+];
+
+//@formatter:on

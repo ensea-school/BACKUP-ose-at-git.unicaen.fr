3ac0da7e39a4166d1fade66e710d6e85a18770f2
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2021-10-14 11:37:55 +0200

    Affinage indicateur 380 en cas de non alimentation de affectation_fin via MV_INTERVENANT

--- data/ddl/view/V_INDICATEUR_380.sql
+++ data/ddl/view/V_INDICATEUR_380.sql
@@ -13,8 +13,9 @@ JOIN statut_intervenant si ON si.id = i.statut_id
 WHERE i.export_date IS NULL
 AND si.code != 'BIATSS'
 AND si.type_intervenant_id = 2
-AND i.affectation_fin < sysdate
+AND (COALESCE(i.affectation_fin, to_date('01/01/9999', 'dd/mm/YYYY')) < sysdate OR i.affectation_fin IS NULL )
 AND c.date_retour_signe IS NOT NULL
 AND i.annee_id = (SELECT valeur FROM parametre p WHERE nom = 'annee')
 GROUP BY i.id
 ) t
+;

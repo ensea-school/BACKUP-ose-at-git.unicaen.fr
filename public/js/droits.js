406838cd1c347c737bcbb9b20bd85a3a2fa0e79b
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-03-18 12:02:03 +0100

    Refonte de la page de gestion des privilèges

--- public/js/droits.js
+++ public/js/droits.js
@@ -12,7 +12,6 @@ $.widget("ose.droitsTbl", {
         td.html("<div class=\"loading\">&nbsp;</div>");
         td.load(Url('droits/privileges/modifier'), {
             role: td.data("role"),
-            statut: td.data("statut"),
             privilege: td.data("privilege"),
             action: action
         }, function ()
@@ -23,31 +22,26 @@ $.widget("ose.droitsTbl", {
     },
 
 
-
-    showOnlyCategorie: function (categorie)
+    showHideCategorie: function (categorie)
     {
+        this.element.find('tr.categorie[data-categorie="' + categorie + '"]').each(function () {
+
+            if ($(this).is(':visible')) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    },
+
+
+
+    showCategorie: function (categorie)
+    {
+        console.log(categorie);
         this.element.find('tr.categorie[data-categorie!="' + categorie + '"]').hide();
+        this.element.find('tr.categorie[data-categorie="' + categorie + '"]').show();
     },
-
-
-
-    showAll: function ()
-    {
-        this.element.find('tr.categorie').show();
-
-        this.element.find('th.role').show();
-        this.element.find('td.role').show();
-
-        this.element.find('th.statut').show();
-        this.element.find('td.statut').show();
-
-        this.element.find('th.roles').attr('colspan', this.element.find('th.role:visible').length).show();
-        this.element.find('th.statuts').attr('colspan', this.element.find('th.statut:visible').length).show();
-        this.showHideSeparator();
-
-        this.updateFirsts();
-    },
-
 
 
     hideRole: function (roleId)
@@ -62,67 +56,16 @@ $.widget("ose.droitsTbl", {
             this.element.find('th.roles').hide();
         }
 
-        this.showHideSeparator();
         this.updateFirsts();
     },
 
 
 
-    hideRoles: function ()
-    {
-        this.element.find('th.role').hide();
-        this.element.find('td.role').hide();
-        this.element.find('th.roles').hide();
-        this.showHideSeparator();
-    },
-
-
-
-    hideStatut: function (statutId)
-    {
-        this.element.find('th.statut[data-statut="' + statutId + '"]').hide();
-        this.element.find('td.statut[data-statut="' + statutId + '"]').hide();
-
-        var statutsCount = this.element.find('th.statut:visible').length;
-        if (statutsCount > 0) {
-            this.element.find('th.statuts').attr('colspan', statutsCount).show();
-        } else {
-            this.element.find('th.statuts').hide();
-        }
-        this.showHideSeparator();
-        this.updateFirsts();
-    },
-
-
-
-    hideStatuts: function ()
-    {
-        this.element.find('th.statut').hide();
-        this.element.find('td.statut').hide();
-        this.element.find('th.statuts').hide();
-        this.showHideSeparator();
-    },
-
-
-
-    showHideSeparator: function ()
-    {
-        if (this.element.find('th.roles:visible').length == 0 || this.element.find('th.statuts:visible').length == 0) {
-            this.element.find('.separator').hide();
-        } else {
-            this.element.find('.separator').show();
-        }
-    },
-
-
-
-    updateFirsts: function()
+    updateFirsts: function ()
     {
         this.element.find('th.role.first').removeClass('first');
-        this.element.find('th.statut.first').removeClass('first');
 
         this.element.find('th.role:visible:first').addClass('first');
-        this.element.find('th.statut:visible:first').addClass('first');
     },
 
 
@@ -153,7 +96,7 @@ $.widget("ose.droitsTbl", {
 
         this.element.find('a.categorie').click(function ()
         {
-            that.showOnlyCategorie($(this).data('categorie'));
+            that.showHideCategorie($(this).data('categorie'));
         });
 
         this.element.find('th.role a').click(function ()
@@ -162,31 +105,15 @@ $.widget("ose.droitsTbl", {
             that.hideRole(roleId);
         });
 
-        this.element.find('th.statut a').click(function ()
-        {
-            var statutId = $(this).parents('.statut').data('statut');
-            that.hideStatut(statutId);
-        });
-
-        this.element.find('a.roles').click(function ()
-        {
-            that.hideStatuts();
-        });
-
-        this.element.find('a.statuts').click(function ()
-        {
-            that.hideRoles();
+        this.element.find('select.categorie').change(function () {
+            that.showCategorie($(this).val());
         });
 
         this.element.find('.modifier').on('mouseover mouseout', function ()
         {
-            if ($(this).data('statut')){
-                var id = $(this).data('statut');
-                var selector = 'statut';
-            }else{
-                var id = $(this).data('role');
-                var selector = 'role';
-            }
+
+            var id = $(this).data('role');
+            var selector = 'role';
 
             that.element.find('.selected').removeClass('selected');
 
@@ -194,15 +121,12 @@ $.widget("ose.droitsTbl", {
                 .add($(this).parent().prevAll()
                     .children(':nth-child(' + ($(this).index() + 1) + '):not(.categorie)')).toggleClass('selected');
 
-            that.element.find('th.'+selector+'[data-'+selector+'="'+id+'"] div').toggleClass('selected');
+            that.element.find('th.' + selector + '[data-' + selector + '="' + id + '"] div').toggleClass('selected');
         });
-
-        this.element.find('a.show-all').click(function () { that.showAll();});
 
         this.updateFirsts();
     }
 });
-
 
 
 

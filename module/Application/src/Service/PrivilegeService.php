b4dde3b3403adfff260a71a384d6e44b906ffafd
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-03-21 10:50:12 +0100

    L'administrateur a tout le temps tous les privilèges, sans jamais pouvoir lui en retirer
    (cherry picked from commit ee1bf55443d7e83f608fdeea01377dfb422ea48e)

--- module/Application/src/Service/PrivilegeService.php
+++ module/Application/src/Service/PrivilegeService.php
@@ -3,6 +3,7 @@
 namespace Application\Service;
 
 use Application\Entity\Db\Annee;
+use Application\Entity\Db\Role;
 use Application\Service\Traits\ContextServiceAwareTrait;
 use Intervenant\Entity\Db\Statut;
 
@@ -46,6 +47,13 @@ class PrivilegeService extends \UnicaenAuth\Service\PrivilegeService
     {
         $privilegesRoles = [];
 
+        /* L'administrateur a tous les privilèges obligatoirement */
+        $rc         = new \ReflectionClass(\Application\Provider\Privilege\Privileges::class);
+        $privileges = array_values($rc->getConstants());
+        foreach ($privileges as $privilege) {
+            $privilegesRoles[$privilege] = [Role::ADMINISTRATEUR];
+        }
+
         $sql   = "
           SELECT
           cp.code || '-' || p.code privilege,
@@ -58,11 +66,8 @@ class PrivilegeService extends \UnicaenAuth\Service\PrivilegeService
         ";
         $query = $this->getEntityManager()->getConnection()->executeQuery($sql);
         while ($pr = $query->fetchAssociative()) {
-            $privilege = $pr['PRIVILEGE'];
-            $role      = $pr['ROLE'];
-            if (!array_key_exists($privilege, $privilegesRoles)) {
-                $privilegesRoles[$privilege] = [];
-            }
+            $privilege                     = $pr['PRIVILEGE'];
+            $role                          = $pr['ROLE'];
             $privilegesRoles[$privilege][] = $role;
         }
 
@@ -74,9 +79,6 @@ class PrivilegeService extends \UnicaenAuth\Service\PrivilegeService
             $sp = $statut->getPrivileges();
             foreach ($sp as $privilege => $has) {
                 if ($has) {
-                    if (!array_key_exists($privilege, $privilegesRoles)) {
-                        $privilegesRoles[$privilege] = [];
-                    }
                     $privilegesRoles[$privilege][] = $statut->getRoleId();
                 }
             }

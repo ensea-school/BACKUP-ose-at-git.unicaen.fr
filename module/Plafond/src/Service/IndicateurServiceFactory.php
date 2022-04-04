42438797b74e7c942861d9d99185f2a625d27b4c
joriot221 <florian.joriot@unicarn.fr>
2022-04-04 11:26:04 +0200

    plafond

---
+++ module/Plafond/src/Service/IndicateurServiceFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Plafond\Service;
+
+use Psr\Container\ContainerInterface;
+
+
+/**
+ * Description of IndicateurServiceFactory
+ *
+ * @author Laurent Lécluse <laurent.lecluse at unicaen.fr>
+ */
+class IndicateurServiceFactory
+{
+
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array|null         $options
+     *
+     * @return IndicateurService
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, $options = null): IndicateurService
+    {
+        $service = new IndicateurService;
+
+        /* Injectez vos dépendances ICI */
+
+        return $service;
+    }
+}

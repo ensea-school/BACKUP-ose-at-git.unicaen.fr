f394134c7de3e74125bb4c4ae92bc129b3dc9f14
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-05-09 13:52:20 +0200

    Réintroduction d'enciennes contraintes d'unicité obligatoires

---
+++ data/ddl/unique-constraint/INTERVENANT_SOURCE_UN.php
@@ -0,0 +1,17 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'INTERVENANT_SOURCE_UN',
+    'table'   => 'INTERVENANT',
+    'index'   => 'INTERVENANT_SOURCE_UN1',
+    'columns' => [
+        'SOURCE_CODE',
+        'ANNEE_ID',
+        'HISTO_DESTRUCTION',
+        'STATUT_ID',
+    ],
+];
+
+//@formatter:on

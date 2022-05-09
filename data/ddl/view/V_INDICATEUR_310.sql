20e4d5a4a6dfa0d386906111d581ed8105acd27d
joriot221 <florian.joriot@unicarn.fr>
2022-05-09 16:42:24 +0200

    modif indicateur 310 et 920

--- data/ddl/view/V_INDICATEUR_310.sql
+++ data/ddl/view/V_INDICATEUR_310.sql
@@ -12,14 +12,17 @@ WITH caok AS (
 SELECT
   w.intervenant_id,
   w.structure_id,
-  TO_CHAR(MAX( c.HISTO_MODIFICATION),'YYYY-MM-DD HH24:MI:SS') AS "Date modif"
+  TO_CHAR(MAX( a.HISTO_MODIFICATION),'YYYY-MM-DD HH24:MI:SS') AS "Date modif"
 FROM
   tbl_workflow w
+  JOIN TYPE_AGREMENT ta ON ta.code = 'CONSEIL_ACADEMIQUE'
   JOIN caok ON caok.intervenant_id = w.intervenant_id
-  LEFT JOIN contrat c ON c.intervenant_id = w.intervenant_id
+  LEFT JOIN AGREMENT a ON a.intervenant_id = w.intervenant_id
+
 WHERE
   w.atteignable = 1
   AND w.etape_code = 'CONTRAT'
   AND w.objectif > 0
   AND w.realisation < 1
+  AND a.TYPE_AGREMENT_ID = ta.id
 GROUP BY (  w.intervenant_id, w.structure_id)

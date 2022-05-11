7b4eb3b1dd34f50df56d91efd0e7cad2d4313d37
joriot221 <florian.joriot@unicarn.fr>
2022-05-11 15:19:46 +0200

    Maj date indicateur 810,820,910,920

--- data/ddl/view/V_INDICATEUR_810.sql
+++ data/ddl/view/V_INDICATEUR_810.sql
@@ -1,11 +1,21 @@
 CREATE OR REPLACE FORCE VIEW V_INDICATEUR_810 AS
-SELECT DISTINCT
+SELECT
   w.intervenant_id,
-  w.structure_id
+  w.structure_id,
+  Max(v.HISTO_CREATION) AS dateModif
 FROM
   tbl_workflow w
+  JOIN VALIDATION v ON v.INTERVENANT_ID = w.INTERVENANT_ID
+  JOIN type_volume_horaire tvh ON tvh.code = 'REALISE'
+  LEFT JOIN VALIDATION_VOL_HORAIRE vvh ON vvh.VALIDATION_ID = v.ID
+  LEFT JOIN VALIDATION_VOL_HORAIRE_REF vvhr ON vvhr.VALIDATION_ID = v.ID
+  LEFT JOIN VOLUME_HORAIRE vh ON vh.ID = vvh.VOLUME_HORAIRE_ID
+  LEFT JOIN VOLUME_HORAIRE_REF vhr ON vhr.ID = vvhr.VOLUME_HORAIRE_REF_ID
 WHERE
   w.etape_code = 'DEMANDE_MEP'
   AND w.type_intervenant_code = 'P'
   AND w.atteignable = 1
   AND w.objectif > w.realisation
+  AND (vhr.TYPE_VOLUME_HORAIRE_ID = tvh.id OR vh.TYPE_VOLUME_HORAIRE_ID = tvh.id)
+GROUP BY
+  w.intervenant_id, w.structure_id
--- data/ddl/view/V_INDICATEUR_920.sql
+++ data/ddl/view/V_INDICATEUR_920.sql
@@ -2,20 +2,22 @@ CREATE OR REPLACE FORCE VIEW V_INDICATEUR_920 AS
 SELECT
   w.intervenant_id,
   w.structure_id,
-  Max(v.HISTO_CREATION) AS dateModif
+  MAX(histo_modification) DATEMODIF
 FROM
-  tbl_workflow w
-  JOIN VALIDATION v ON v.INTERVENANT_ID = w.INTERVENANT_ID
-  JOIN type_volume_horaire tvh ON tvh.code = 'REALISE'
-  LEFT JOIN VALIDATION_VOL_HORAIRE vvh ON vvh.VALIDATION_ID = v.ID
-  LEFT JOIN VALIDATION_VOL_HORAIRE_REF vvhr ON vvhr.VALIDATION_ID = v.ID
-  LEFT JOIN VOLUME_HORAIRE vh ON vh.ID = vvh.VOLUME_HORAIRE_ID
-  LEFT JOIN VOLUME_HORAIRE_REF vhr ON vhr.ID = vvhr.VOLUME_HORAIRE_REF_ID
+  mise_en_paiement mep
+  LEFT JOIN formule_resultat_service frs ON frs.id = mep.formule_res_service_id
+  LEFT JOIN formule_resultat_service_ref frsr ON frsr.id = mep.formule_res_service_ref_id
+  JOIN formule_resultat fr ON fr.id = COALESCE(frs.formule_resultat_id,frsr.formule_resultat_id)
+  JOIN tbl_workflow w ON w.intervenant_id = fr.intervenant_id
 WHERE
-  w.etape_code = 'SAISIE_MEP'
+  mep.histo_destruction IS NULL
+  AND mep.periode_paiement_id IS NULL
+  AND w.etape_code = 'SAISIE_MEP'
   AND w.type_intervenant_code = 'E'
   AND w.atteignable = 1
   AND w.objectif > w.realisation
-  AND (vhr.TYPE_VOLUME_HORAIRE_ID = tvh.id OR vh.TYPE_VOLUME_HORAIRE_ID = tvh.id)
 GROUP BY
-  w.intervenant_id, w.structure_id
+  fr.intervenant_id,
+  w.annee_id,
+  w.intervenant_id,
+  w.structure_id

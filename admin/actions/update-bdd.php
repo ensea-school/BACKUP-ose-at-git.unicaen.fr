125aea0f4a792496bfb7ff6bb23e43e80323dbd9
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-05-12 14:24:58 +0200

    On ignore mv_ext_service en mode dev sous docker

--- admin/actions/update-bdd.php
+++ admin/actions/update-bdd.php
@@ -23,9 +23,9 @@ foreach ($ref as $ddlClass => $objects) {
     }
 }
 
-//if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'ose-dev.pc-local') {
-$filters['materialized-view']['excludes'][] = 'MV_EXT_SERVICE';
-//}
+if (($_SERVER['IGNORE_MV_EXT_SERVICE'] ?? "false") == "true") {
+    $filters['materialized-view']['excludes'][] = 'MV_EXT_SERVICE';
+}
 
 $tablesDep = [
     Ddl::INDEX,

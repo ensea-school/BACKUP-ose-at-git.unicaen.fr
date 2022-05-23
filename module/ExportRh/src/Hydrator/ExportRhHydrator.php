814022149f2a197318269f165d79937442e0b6fb
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2022-05-23 14:01:47 +0200

    Inversion code corresp 1 et 2 pour code siham

--- module/ExportRh/src/Hydrator/ExportRhHydrator.php
+++ module/ExportRh/src/Hydrator/ExportRhHydrator.php
@@ -23,7 +23,7 @@ class ExportRhHydrator implements HydratorInterface
     {
         //On mappe automatiquement le bon statut RH selon le statut OSE
         $statut = $object->getStatut();
-        $statutRh = $statut->getCodesCorresp1();
+        $statutRh = $statut->getCodesCorresp2();
         $typeEmploi = ($statut->getCodesCorresp3()) ? 'UCNVA' : 'UCNVCE';
 
         $data['connecteurForm'] = [

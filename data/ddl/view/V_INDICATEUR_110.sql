99a213709afa4287714163e5557e3c06c9ce5e88
joriot221 <florian.joriot@unicarn.fr>
2022-05-24 16:50:10 +0200

    Prise en compte de la refonte et du changement de numéro des indicateurs

--- data/ddl/view/V_INDICATEUR_110.sql
+++ data/ddl/view/V_INDICATEUR_110.sql
@@ -1,10 +1,12 @@
 CREATE OR REPLACE FORCE VIEW V_INDICATEUR_110 AS
 SELECT
   d.intervenant_id,
-  i.structure_id
+  i.structure_id,
+  TO_CHAR(MAX( ido.HISTO_MODIFICATION),'YYYY-MM-DD HH24:MI:SS') AS "Date modif"
 FROM
   tbl_dossier d
   JOIN intervenant i ON i.id = d.intervenant_id
+  JOIN INTERVENANT_DOSSIER ido ON ido.ID = d.DOSSIER_ID
 WHERE
   d.dossier_id IS NOT NULL
   /*Complétude des différents bloc dossier*/
@@ -19,3 +21,4 @@ WHERE
   AND d.completude_statut = 1
   AND d.validation_id IS NULL
   AND d.actif = 1
+GROUP BY (d.intervenant_id, i.STRUCTURE_ID)
--- data/ddl/view/V_INDICATEUR_120.sql
+++ data/ddl/view/V_INDICATEUR_120.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE FORCE VIEW V_INDICATEUR_120 AS
 SELECT DISTINCT
   i.id intervenant_id,
-  i.structure_id
+  i.structure_id,
+  TO_CHAR(MAX( d.HISTO_MODIFICATION),'YYYY-MM-DD HH24:MI:SS') AS "Date modif"
 FROM
   indic_modif_dossier d
   JOIN intervenant i ON i.id = d.intervenant_id
 WHERE
   d.histo_destruction IS NULL
+GROUP BY (i.id, i.STRUCTURE_ID)

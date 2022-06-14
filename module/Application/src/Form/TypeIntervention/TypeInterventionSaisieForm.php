3e76df3baf0366086cbbc52d31af5e2ea7aedb1c
joriot221 <florian.joriot@unicarn.fr>
2022-06-14 14:54:11 +0200

    Correction erreur : type of int|float attendu et string given

--- module/Application/src/Form/TypeIntervention/TypeInterventionSaisieForm.php
+++ module/Application/src/Form/TypeIntervention/TypeInterventionSaisieForm.php
@@ -166,7 +166,7 @@ class TypeInterventionSaisieForm extends AbstractForm
                     new \Laminas\Validator\Callback([
                         'messages' => [\Laminas\Validator\Callback::INVALID_VALUE => '%value% doit être >= 0'],
                         'callback' => function ($value) {
-                            return (StringFromFloat::run($value) >= 0.0 ? true : false);
+                            return (FloatFromString::run($value)) >= 0.0;
                         }]),
                 ],
             ],

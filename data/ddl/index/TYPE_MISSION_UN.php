365efb9bc4ec1e358df2a079511eda8ba61d22ce
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-12-01 11:03:54 +0100

    Contrainte d'unicité pour annualisation des types de missions

---
+++ data/ddl/index/TYPE_MISSION_UN.php
@@ -0,0 +1,16 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'TYPE_MISSION_UN',
+    'unique'  => TRUE,
+    'table'   => 'TYPE_MISSION',
+    'columns' => [
+        'CODE',
+        'ANNEE_ID',
+        'HISTO_DESTRUCTION',
+    ],
+];
+
+//@formatter:on

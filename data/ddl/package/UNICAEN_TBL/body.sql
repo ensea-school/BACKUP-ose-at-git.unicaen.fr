b25a246e730d5e29407b9e26c5aa79a5bbcef3a1
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2022-12-08 09:55:53 +0100

    MAJ DDL

--- data/ddl/package/UNICAEN_TBL/body.sql
+++ data/ddl/package/UNICAEN_TBL/body.sql
@@ -1241,17 +1241,15 @@ CREATE OR REPLACE PACKAGE BODY "UNICAEN_TBL" AS
           SUM(CASE WHEN vvhm.id IS NOT NULL OR vhm.auto_validation = 1 THEN vhm.heures ELSE 0 END) heures_validees
         FROM
                     intervenant                     i
-               JOIN statut                         si on si.id = i.statut_id
-               JOIN type_validation               tvm on tvm.code = ''MISSION''
-               JOIN type_validation              tvvh on tvvh.code = ''MISSION_REALISE''
-          LEFT JOIN mission                         m on m.intervenant_id = i.id AND m.histo_destruction IS NULL
-          LEFT JOIN validation_mission            vml on vml.mission_id = m.id
-          LEFT JOIN validation                     vm on vm.id = vml.validation_id AND vm.histo_destruction IS NULL
-
-          LEFT JOIN volume_horaire_mission        vhm on vhm.mission_id = m.id AND vhm.histo_destruction IS NULL
-          LEFT JOIN validation_vol_horaire_miss vvhml on vvhml.volume_horaire_mission_id = vhm.id
-          LEFT JOIN validation                   vvhm on vvhm.id = vvhml.validation_id AND vvhm.histo_destruction IS NULL
-
+               JOIN statut                         si ON si.id = i.statut_id
+               JOIN type_validation               tvm ON tvm.code = ''MISSION''
+               JOIN type_validation              tvvh ON tvvh.code = ''MISSION_REALISE''
+          LEFT JOIN mission                         m ON m.intervenant_id = i.id AND m.histo_destruction IS NULL
+          LEFT JOIN validation_mission            vml ON vml.mission_id = m.id
+          LEFT JOIN validation                     vm ON vm.id = vml.validation_id AND vm.histo_destruction IS NULL
+          LEFT JOIN volume_horaire_mission        vhm ON vhm.mission_id = m.id AND vhm.histo_destruction IS NULL
+          LEFT JOIN validation_vol_horaire_miss vvhml ON vvhml.volume_horaire_mission_id = vhm.id
+          LEFT JOIN validation                   vvhm ON vvhm.id = vvhml.validation_id AND vvhm.histo_destruction IS NULL
         WHERE
           i.histo_destruction IS NULL
           AND si.mission = 1

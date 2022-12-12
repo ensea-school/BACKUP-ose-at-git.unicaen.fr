7609a87a84066d64ba0b6e5a4c718aa34ec364cc
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2022-12-12 11:10:29 +0100

    Premier travail administration taux de mission

---
+++ module/Mission/src/Controller/TauxMissionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mission\Controller;
+
+use Application\Controller\AbstractController;
+
+/**
+ * Description of TauxMissionController
+ *
+ * @author Florian Joriot <florian.joriot at unicaen.fr>
+ */
+class TauxMissionController extends AbstractController
+{
+    public function indexAction()
+    {
+        return [];
+    }
+}
+

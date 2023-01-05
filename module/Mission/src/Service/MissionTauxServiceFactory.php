58f9ba115b2a12cce9375ae40ede833d20474fe8
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2023-01-05 14:33:35 +0100

    Création d'un MissionTauxService.php

---
+++ module/Mission/src/Service/MissionTauxServiceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mission\Service;
+
+use Psr\Container\ContainerInterface;
+
+
+/**
+ * Description of MissionTauxServiceFactory
+ *
+ * @author Florian Joriot <florian.joriot at unicaen.fr>
+ */
+class MissionTauxServiceFactory
+{
+
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array|null         $options
+     *
+     * @return MissionTauxService
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, $options = null): MissionTauxService
+    {
+        $service = new MissionTauxService;
+
+        /* Injectez vos dépendances ICI */
+
+        return $service;
+    }
+}
+

b89e76e54d89227a89b2bb6642d8d825e5e53f80
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2023-02-06 13:21:37 +0100

    Ajout d'une contrainte d'unicité sur le code type intervention

---
+++ data/ddl/unique-constraint/TYPE_INTERVENTION_CODE_UN.php
@@ -0,0 +1,15 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'TYPE_INTERVENTION_CODE_UN',
+    'table'   => 'TYPE_INTERVENTION',
+    'index'   => 'TYPE_INTERVENTION_CODE_UN',
+    'columns' => [
+        'CODE',
+        'HISTO_DESTRUCTION',
+    ],
+];
+
+//@formatter:on

b408cb7efd3098b6298ec25a8f7a718e6bc423ab
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-03-01 11:57:04 +0100

    Suivi de mission (WIP)

--- front/Mission/Suivi.vue
+++ front/Mission/Suivi.vue
@@ -1,16 +1,56 @@
 <template>
-    <u-calendar date="2023-02-01"></u-calendar>
+    <u-calendar :date="date" @changeDate="changeDate" :events="realise" />
     <button @click="test()">Test</button>
 </template>
 
 <script>
 
+import SuiviEvent from './SuiviEvent.vue';
+
 export default {
     name: 'Suivi',
+    props: {
+        intervenant: {type: Number, required: true},
+    },
+    data(){
+        return {
+            date: new Date(),
+            realise: [
+                {
+                    component: SuiviEvent,
+                    date: new Date(2023,1,5),
+                    texte:'Coucou, le 50 février 2023',
+                },
+                {
+                    component: SuiviEvent,
+                    date: new Date(2023,1,5),
+                    texte:'Coucou, le 505 février 2023',
+
+                },
+                {
+                    component: SuiviEvent,
+                    date: new Date(2023,1,6),
+                    texte:'Coucou, le 60 février 2023'
+                },
+                {
+                    component: SuiviEvent,
+                    date: new Date(2023,2,6),
+                    texte:'Coucou, le 6 mars 2023',
+
+                },
+            ]
+        };
+    },
     methods: {
         test()
         {
             console.log(this.lignes());
+        },
+
+        changeDate(dateObj)
+        {
+            console.log(dateObj);
+            this.date = dateObj;
         }
     }
 }
---
+++ front/Mission/SuiviEvent.vue
@@ -0,0 +1,16 @@
+<template>
+    {{ event.texte }}
+</template>
+
+<script>
+export default {
+    name: "SuiviEvent",
+    props: {
+        event: {type: Object, required: true},
+    },
+}
+</script>
+
+<style scoped>
+
+</style>

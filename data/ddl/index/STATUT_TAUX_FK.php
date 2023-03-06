eec81ceca31a097af7c53d9f8edd677643cad0f3
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-03-06 16:59:37 +0100

    Corrections BDD

---
+++ data/ddl/index/STATUT_TAUX_FK.php
@@ -0,0 +1,14 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'STATUT_TAUX_FK',
+    'unique'  => FALSE,
+    'table'   => 'STATUT',
+    'columns' => [
+        'TAUX_REMU_ID',
+    ],
+];
+
+//@formatter:on

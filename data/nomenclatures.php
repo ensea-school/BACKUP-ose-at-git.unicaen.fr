f06314fc9e9817409ff1268caa062f172d1ae2a4
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-03-13 13:09:10 +0100

    Dataupdater pour les taux

--- data/nomenclatures.php
+++ data/nomenclatures.php
@@ -899,41 +899,6 @@ return [
         ],
     ],
 
-    'TAUX_REMU' => [
-        [
-            'ID'      => 1,
-            'CODE'    => 'TLD',
-            'LIBELLE' => 'Taux légal par défaut',
-        ],
-    ],
-
-    'TAUX_REMU_VALEUR' => [
-        [
-            'ID'           => 1,
-            'TAUX_REMu_ID' => 1,
-            'DATE_EFFET'   => '27/07/2009',
-            'VALEUR'       => 40.58,
-        ],
-        [
-            'ID'           => 2,
-            'TAUX_REMu_ID' => 1,
-            'DATE_EFFET'   => '25/06/2014',
-            'VALEUR'       => 40.91,
-        ],
-        [
-            'ID'           => 3,
-            'TAUX_REMu_ID' => 1,
-            'DATE_EFFET'   => '01/02/2017',
-            'VALEUR'       => 41.41,
-        ],
-        [
-            'ID'           => 4,
-            'TAUX_REMu_ID' => 1,
-            'DATE_EFFET'   => '01/07/2022',
-            'VALEUR'       => 42.86,
-        ],
-    ],
-
     'UTILISATEUR' => [
         [
             'USERNAME'             => 'oseappli',

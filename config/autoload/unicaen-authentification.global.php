d67819ea707aebeef203ca7a85c955e45ac561d0
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-04-03 15:59:31 +0200

    #47682

--- config/autoload/unicaen-authentification.global.php
+++ config/autoload/unicaen-authentification.global.php
@@ -77,6 +77,11 @@ $settings = [
         'enabled'     => AppConfig::get('cas', 'actif', false),
 
         /**
+         * Permet de sauter le formulaire d'authentification "/auth/connexion" si CAS est la seule source d'authentification
+         */
+        'form_skip' => true,
+
+        /**
          * Description facultative de ce mode d'authentification qui apparaîtra sur la page de connexion.
          */
         'description' => "Cliquez sur le bouton ci-dessous pour vous connecter à l'aide de l'authentification centralisée (CAS).",
@@ -91,6 +96,11 @@ $settings = [
          * NB: en réalité cela permet aussi l'authentification avec un compte local.
          */
         'enable'     => false,
+
+        /**
+         * Permet de sauter le formulaire d'authentification "/auth/connexion" si CAS est la seule source d'authentification
+         */
+        'form_skip' => true,
 
         /**
          * URL de déconnexion.

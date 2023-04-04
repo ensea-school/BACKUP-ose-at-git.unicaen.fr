97b1fa8126f8541b91b2b2870c8a23f2e7efc68e
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-04-04 14:49:21 +0200

    Ajout de PIECE_JOINTE_ID dans le TBL_PIECE_JOINTE

---
+++ data/ddl/ref-constraint/TBL_PJ_PIECE_JOINTE_FK.php
@@ -0,0 +1,16 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'        => 'TBL_PJ_PIECE_JOINTE_FK',
+    'table'       => 'TBL_PIECE_JOINTE',
+    'rtable'      => 'PIECE_JOINTE',
+    'delete_rule' => 'CASCADE',
+    'index'       => NULL,
+    'columns'     => [
+        'PIECE_JOINTE_ID' => 'ID',
+    ],
+];
+
+//@formatter:on

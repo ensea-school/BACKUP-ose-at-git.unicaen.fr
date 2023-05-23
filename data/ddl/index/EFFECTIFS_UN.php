0e5a88a00afc2232d206000fb7f68f9c953b0ae5
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-05-23 11:10:37 +0200

    Formules de Rennes 2 et Paris 1

---
+++ data/ddl/index/EFFECTIFS_UN.php
@@ -0,0 +1,15 @@
+<?php
+
+//@formatter:off
+
+return [
+    'name'    => 'EFFECTIFS_UN',
+    'unique'  => TRUE,
+    'table'   => 'EFFECTIFS',
+    'columns' => [
+        'ELEMENT_PEDAGOGIQUE_ID',
+        'HISTO_DESTRUCTION',
+    ],
+];
+
+//@formatter:on

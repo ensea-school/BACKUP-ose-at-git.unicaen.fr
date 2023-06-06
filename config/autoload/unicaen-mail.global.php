3dcd872aa4f529943406495f98101a36601edd49
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2023-06-06 14:57:33 +0200

    fusion de l'ensemble des config de unicaen/mail

--- config/autoload/unicaen-mail.global.php
+++ config/autoload/unicaen-mail.global.php
@@ -1,9 +1,50 @@
 <?php
 
 use UnicaenMail\Controller\MailController;
+use UnicaenMail\Entity\Db\Mail;
 use UnicaenPrivilege\Guard\PrivilegeController;
 
 return [
+    'unicaen-mail' => [
+        /**
+         * Classe de entité
+         **/
+        'mail_entity_class' => Mail::class,
+
+        /**
+         * Options concernant l'envoi de mail par l'application
+         */
+        'transport_options' => [
+            'host' => AppConfig::get('mail', 'smtpHost'),
+            'port' => AppConfig::get('mail', 'smtpPort'),
+        ],
+
+        'redirect_to' => AppConfig::get('mail', 'redirection'),
+        'do_not_send' => AppConfig::get('mail', 'envoiDesactive'),
+
+        'subject_prefix' => 'OSE',
+        'from_name'      => 'Application',
+        'from_email'     => AppConfig::get('mail', 'from'),
+
+        /**
+         * Adresses des redirections si do_not_send est à true
+         */
+
+        'module' => [
+            'default' => [
+                'redirect_to'    => AppConfig::get('mail', 'redirection'),
+                'do_not_send'    => true,
+                'subject_prefix' => 'DEV',
+                'from_name'      => 'OSE | Application',
+                'from_email'     => AppConfig::get('mail', 'from'),
+
+            ],
+
+        ],
+    ],
+
+    'server_url' => AppConfig::get('mail', 'scheme') . '://' . AppConfig::get('mail', 'domain'),
+
     'navigation' => [
         'default' => [
             'home' => [

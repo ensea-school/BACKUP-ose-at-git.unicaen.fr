40538d14dbd98944a3df92bb2640bb08f05bfbb3
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-06-13 10:28:24 +0200

    Meilleure protection avec les triggers

--- module/Mission/src/Controller/SuiviController.php
+++ module/Mission/src/Controller/SuiviController.php
@@ -89,10 +89,10 @@ class SuiviController extends AbstractController
 
         $triggers = [
             '/' => function (VolumeHoraireMission $original, array $extracted) {
-                $extracted['canEdit'] = $this->isAllowed($original, Privileges::MISSION_EDITION_REALISE);
-                $extracted['canValider'] = $this->isAllowed($original, Privileges::MISSION_VALIDATION_REALISE);
-                $extracted['canDevalider'] = $this->isAllowed($original, Privileges::MISSION_DEVALIDATION_REALISE);
-                $extracted['canSupprimer'] = $this->isAllowed($original, Privileges::MISSION_EDITION_REALISE);
+                $extracted['canEdit'] &= $this->isAllowed($original, Privileges::MISSION_EDITION_REALISE);
+                $extracted['canValider'] &= $this->isAllowed($original, Privileges::MISSION_VALIDATION_REALISE);
+                $extracted['canDevalider'] &= $this->isAllowed($original, Privileges::MISSION_DEVALIDATION_REALISE);
+                $extracted['canSupprimer'] &= $this->isAllowed($original, Privileges::MISSION_EDITION_REALISE);
 
                 return $extracted;
             },

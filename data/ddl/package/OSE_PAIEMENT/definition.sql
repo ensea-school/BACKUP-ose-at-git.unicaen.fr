3b7860a368736bbdf06cdc28e7937ce43b1e74b2
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-06-16 12:01:43 +0200

    MAJ DDL

--- data/ddl/package/OSE_PAIEMENT/definition.sql
+++ data/ddl/package/OSE_PAIEMENT/definition.sql
@@ -12,8 +12,8 @@ CREATE OR REPLACE PACKAGE "OSE_PAIEMENT" AS
 
   FUNCTION get_format_mois_du RETURN VARCHAR2;
 
-  FUNCTION get_taux_horaire(id_in IN NUMBER, date_val IN DATE) return float;
+  FUNCTION get_taux_horaire(id_in IN NUMBER, date_val IN DATE) RETURN FLOAT;
 
-  FUNCTION get_taux_horaire_date(id_in IN NUMBER, date_val IN DATE) return DATE;
+  FUNCTION get_taux_horaire_date(id_in IN NUMBER, date_val IN DATE) RETURN DATE;
 
 END ose_paiement;

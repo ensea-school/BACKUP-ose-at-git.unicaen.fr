c5c9dce83aa77f26b066050762634063f64d1aeb
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-06-19 15:45:22 +0200

    Bug plafond avec des ens ext

--- module/Plafond/src/Service/PlafondService.php
+++ module/Plafond/src/Service/PlafondService.php
@@ -321,8 +321,10 @@ class PlafondService extends AbstractEntityService
             $libVal = 'entity.libelle';
             $filters['pd.ELEMENT_PEDAGOGIQUE_ID'] = (int)$pqr->entity->getId();
 
-        } elseif ($pqr->entity instanceof Service && $pqr->entity->getElementPedagogique()) {
-            return $this->makeQuery($pqr->sub($pqr->entity->getElementPedagogique()));
+        } elseif ($pqr->entity instanceof Service) {
+            if ($pqr->entity->getElementPedagogique()) {
+                return $this->makeQuery($pqr->sub($pqr->entity->getElementPedagogique()));
+            }
 
         } elseif ($pqr->entity instanceof VolumeHoraire) {
             $perimetre = PlafondPerimetre::VOLUME_HORAIRE;

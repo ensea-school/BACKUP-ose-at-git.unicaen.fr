4e02811eaa0a7abcaed1988f9d03cea2b79ab1bd
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-07-05 16:53:42 +0200

    Pb suite modularisation ODF

--- admin/migration/v21Nomenclatures.php
+++ admin/migration/v21Nomenclatures.php
@@ -24,12 +24,17 @@ class v21Nomenclatures extends AbstractMigration
     public function after()
     {
         $c   = $this->manager->getOseAdmin()->getConsole();
-        $du = $this->manager->getBdd()->dataUpdater();
+        $bdd = $this->manager->getBdd();
+
+        $du = $bdd->dataUpdater();
 
         $du->run('install', 'TAUX_REMU');
         $du->run('install', 'TAUX_REMU_VALEUR');
         $du->run('install', 'JOUR_FERIE');
         $du->run('install', 'TYPE_SERVICE');
         $du->run('install', 'TYPE_MISSION');
+
+        $bdd->exec("update etat_sortie set PDF_TRAITEMENT = replace(PDF_TRAITEMENT, 'Application\Entity\Db\TypeIntervention', 'OffreFormation\Entity\Db\TypeIntervention') where PDF_TRAITEMENT like '%Application\Entity\Db\TypeIntervention%'");
+        $bdd->exec("update etat_sortie set csv_traitement = replace(csv_traitement, 'Application\Entity\Db\TypeIntervention', 'OffreFormation\Entity\Db\TypeIntervention') where csv_traitement like '%Application\Entity\Db\TypeIntervention%'");
     }
 }

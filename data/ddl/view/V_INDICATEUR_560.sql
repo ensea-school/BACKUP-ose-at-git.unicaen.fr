f2050911a7d17430a2c1d64d52be0255bc718c0a
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2023-10-12 15:23:35 +0200

    Correction #50952

--- data/ddl/view/V_INDICATEUR_560.sql
+++ data/ddl/view/V_INDICATEUR_560.sql
@@ -6,23 +6,25 @@ SELECT
 FROM
   tbl_workflow w
   JOIN TYPE_VOLUME_HORAIRE tvh ON tvh.CODE = 'REALISE'
-  JOIN tbl_workflow wc ON wc.intervenant_id = w.intervenant_id
-  JOIN intervenant i ON i.id = wc.intervenant_id
+  LEFT JOIN tbl_workflow wc ON wc.intervenant_id = w.intervenant_id AND wc.etape_code = 'CLOTURE_REALISE'
+  JOIN intervenant i ON i.id = w.intervenant_id
   JOIN structure s ON s.id = w.structure_id
-  JOIN statut s ON s.id = i.statut_id
+  JOIN statut s ON s.id = i.statut_id AND s.type_intervenant_id = (SELECT id FROM type_intervenant ti WHERE code = 'P')
   LEFT JOIN HISTO_INTERVENANT_SERVICE his ON his.INTERVENANT_ID = w.intervenant_id
 WHERE
   w.etape_code = 'REFERENTIEL_VALIDATION_REALISE'
   AND w.objectif > w.realisation
   AND w.atteignable = 1
-
-  AND (wc.etape_code = 'CLOTURE_REALISE' AND s.cloture = 1)
-  AND wc.objectif = wc.realisation
+  AND CASE WHEN s.cloture = 1 AND wc.objectif = wc.realisation THEN 1
+  		   WHEN s.cloture = 0 THEN 1
+  		   ELSE 0 END  = 1
   AND w.structure_id = i.structure_id
-
   AND his.TYPE_VOLUME_HORAIRE_ID = tvh.ID
   AND his.REFERENTIEL = 1
 GROUP BY
   w.annee_id,
   w.intervenant_id,
   i.structure_id
+
+
+

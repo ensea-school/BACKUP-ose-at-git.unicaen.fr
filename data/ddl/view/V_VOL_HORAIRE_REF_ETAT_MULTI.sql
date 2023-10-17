b6fc0d2d25165e4541edc745224915184ada0382
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-10-17 16:54:08 +0200

    Erreur commit : retour en arrière

--- data/ddl/view/V_VOL_HORAIRE_REF_ETAT_MULTI.sql
+++ data/ddl/view/V_VOL_HORAIRE_REF_ETAT_MULTI.sql
@@ -5,7 +5,7 @@ select vh.id VOLUME_HORAIRE_REF_ID, evh.id ETAT_VOLUME_HORAIRE_ID
   join etat_volume_horaire evh on evh.code = 'saisi'
   where vh.histo_destruction IS NULL
 union all
-  select vh.id, evh.id
+  select vh.id VOLUME_HORAIRE_REF_ID, evh.id ETAT_VOLUME_HORAIRE_ID
   from volume_horaire_ref vh
   join service_referentiel s on s.id = vh.service_referentiel_id and s.histo_destruction IS NULL
   join etat_volume_horaire evh on evh.code = 'valide'
@@ -14,3 +14,17 @@ union all
     SELECT * FROM validation v JOIN validation_vol_horaire_ref vvh ON vvh.validation_id = v.id
     WHERE vvh.volume_horaire_ref_id = vh.id AND v.histo_destruction IS NULL
   )
+union all
+  select vh.id VOLUME_HORAIRE_REF_ID, evh.id ETAT_VOLUME_HORAIRE_ID
+  from volume_horaire_ref vh
+  join service_referentiel s on s.id = vh.service_referentiel_id and s.histo_destruction IS NULL
+  join contrat c on vh.contrat_id = c.id and c.histo_destruction IS NULL
+  join etat_volume_horaire evh on evh.code = 'contrat-edite'
+  where vh.histo_destruction IS NULL
+union all
+  select vh.id VOLUME_HORAIRE_REF_ID, evh.id ETAT_VOLUME_HORAIRE_ID
+  from volume_horaire_ref vh
+  join service_referentiel s on s.id = vh.service_referentiel_id and s.histo_destruction IS NULL
+  join contrat c on vh.contrat_id = c.id and c.histo_destruction IS NULL and c.date_retour_signe is not null
+  join etat_volume_horaire evh on evh.code = 'contrat-signe'
+  where vh.histo_destruction IS NULL

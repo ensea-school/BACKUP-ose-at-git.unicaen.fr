351e99a3ee049dd37e4fe444de31f73e74733660
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-10-17 16:52:17 +0200

    Formule Avignon

--- data/ddl/view/V_VOLUME_HORAIRE_REF_ETAT.sql
+++ data/ddl/view/V_VOLUME_HORAIRE_REF_ETAT.sql
@@ -1,17 +1,9 @@
 CREATE OR REPLACE FORCE VIEW V_VOLUME_HORAIRE_REF_ETAT AS
-SELECT
-  vh.id volume_horaire_ref_id,
+SELECT DISTINCT
+  vhr.id volume_horaire_ref_id,
   evh.id etat_volume_horaire_id
 FROM
-  volume_horaire_ref vh
-  LEFT JOIN contrat c ON c.id = vh.contrat_id AND c.histo_destruction IS NULL
-  LEFT JOIN validation cv ON cv.id = c.validation_id AND cv.histo_destruction IS NULL
-  JOIN etat_volume_horaire evh ON evh.code = CASE
-    WHEN c.date_retour_signe IS NOT NULL THEN 'contrat-signe'
-    WHEN cv.id IS NOT NULL THEN 'contrat-edite'
-    WHEN vh.auto_validation = 1 OR EXISTS(
-      SELECT * FROM validation v JOIN validation_vol_horaire_ref vvh ON vvh.validation_id = v.id
-      WHERE vvh.volume_horaire_ref_id = vh.id AND v.histo_destruction IS NULL
-    ) THEN 'valide'
-    ELSE 'saisi'
-    END
+  volume_horaire_ref vhr
+  LEFT JOIN validation_vol_horaire_ref vvhr ON vvhr.volume_horaire_ref_id = vhr.id
+  LEFT JOIN validation v ON v.id = vvhr.validation_id AND v.histo_destruction IS NULL
+  JOIN etat_volume_horaire evh ON evh.code = CASE WHEN vhr.auto_validation = 1 OR v.id IS NOT NULL THEN 'valide' ELSE 'saisi' END

7af1f6c4f05c7ce9a209ba8ab1af846a46eac1af
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2023-10-27 11:36:40 +0200

    Correction indicateur 530 qui remonte des intervenants qui n'ont pas accès au service réalisé (erreur 530 en cliquant sur eux)

--- data/ddl/view/V_INDICATEUR_530.sql
+++ data/ddl/view/V_INDICATEUR_530.sql
@@ -1,12 +1,14 @@
-CREATE OR REPLACE FORCE VIEW V_INDICATEUR_530 AS
 SELECT DISTINCT
   w.intervenant_id,
   i.structure_id
 FROM
   tbl_workflow w
   JOIN intervenant i ON i.id = w.intervenant_id
+  JOIN tbl_workflow w2 ON w2.intervenant_id = w.intervenant_id
 WHERE
   w.etape_code = 'CLOTURE_REALISE'
+  AND w2.etape_code = 'SERVICE_SAISIE_REALISE'
   AND w.type_intervenant_code = 'P'
   AND w.atteignable = 1
+  AND w2.atteignable = 1
   AND w.objectif > w.realisation

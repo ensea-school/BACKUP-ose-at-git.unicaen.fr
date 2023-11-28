e56419b33482573e0db64b8dc5844b93796339a4
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-11-28 11:28:19 +0100

    Nouvelle commande calcul-feuille-de-route
    (cherry picked from commit 9873f89896b080cb330cc86ad0728c257ca0ad4e)

---
+++ admin/actions/calcul-feuille-de-route.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @var \Application\Service\WorkflowService $ws */
+$ws = $oa->getContainer()->get(\Application\Service\WorkflowService::class);
+
+$id = (int)$c->getArg(2);
+
+if (0 === $id){
+    $c->printDie('L\'ID valide d\'un intervenant doit être transmis en argument');
+}
+
+$intervenant = $ws->getEntityManager()->find(\Application\Entity\Db\Intervenant::class, $id);
+
+if (!$intervenant){
+    $c->printDie('L\'intervenant dont l\'ID est '.$id.' n\'existe pas');
+}
+
+$c->printMainTitle('Feuille de route de '.$intervenant);
+
+$c->println('Année universitaire '.$intervenant->getAnnee());
+$c->println('Statut '.lcfirst($intervenant->getStatut()));
+
+
+$c->print('Calcul en cours ...');
+$ws->calculerTableauxBord([], $intervenant);
+$c->print("\r");
+
+if (empty($errors)) {
+    $c->println('Feuille de route actualisée', $c::COLOR_GREEN );
+} else {
+    foreach ($errors as $error) {
+        $c->error($error);
+    }
+}

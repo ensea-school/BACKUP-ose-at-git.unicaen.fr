06de1a9de182649a2f8169d2a4462c63abdfab00
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2023-11-30 09:32:40 +0100

    Finalisation du script de migration (a tester)

--- admin/migration/v23FonctionReferentiel.php
+++ admin/migration/v23FonctionReferentiel.php
@@ -25,46 +25,86 @@ class v23FonctionReferentiel extends AbstractMigration
         $bdd = $this->manager->getBdd();
         $c   = $this->manager->getOseAdmin()->getConsole();
         $c->msg('Mise en etat de la base de données pour pouvoir migrer');
+        $this->traitementContrainteTriggerIndex($bdd, $c);
 
+        $this->traitementFonctionReferentielInsertion($c, $bdd);
+
+        $this->traitementParentIdFonctionReferentiel($c, $bdd);
+
+        $this->traitementServiceReferentiel($c, $bdd);
+        $this->traitementPlafondReferentiel($c, $bdd);
+
+        //supprimer les anciennes fonction qui n'ont pas d'année
+        $c->msg('Suppression des anciennes fonctions');
+
+        $bdd->exec('DELETE FROM TBL_REFERENTIEL');
+        $bdd->exec('DELETE FROM TBL_PLAFOND_REFERENTIEL');
+        $bdd->exec('DELETE FROM FONCTION_REFERENTIEL WHERE annee_id IS NULL');
+        $c->msg('Fin de la migration des fonctions referentiel');
+    }
+
+
+
+    /**
+     * @param Bdd     $bdd
+     * @param Console $c
+     *
+     * @return void
+     */
+    public function traitementContrainteTriggerIndex(Bdd $bdd, Console $c): void
+    {
 
         //Mise en etat de la base de données pour pouvoir migrer
         try {
             $bdd->exec("ALTER TABLE FONCTION_REFERENTIEL ADD(ANNEE_ID NUMBER)");
         } catch (\Exception $e) {
-            $c->msg('error alter ANNEE_ID');
+            $c->printDie('impossible d\'ajouter la colonne ANNEE_ID');
         }
 
         try {
-            $bdd->exec("alter table FONCTION_REFERENTIEL modify HISTO_MODIFICATION null");
-        } catch (\Exception $e) {
-            $c->msg('error alter HISTO_MODIFICATION');
-        }
-        try {
             $bdd->exec("alter table FONCTION_REFERENTIEL modify HISTO_MODIFICATEUR_ID null");
         } catch (\Exception $e) {
-            $c->msg('error alter HISTO_MODIFICATEUR_ID');
         }
         try {
             $bdd->exec("ALTER TABLE FONCTION_REFERENTIEL DROP CONSTRAINT FONCTION_REFERENTIEL_UN");
         } catch (\Exception $e) {
-            $c->msg('error drop constraint FONCTION_REFERENTIEL_UN');
         }
         try {
             $bdd->exec("ALTER TABLE FONCTION_REFERENTIEL DROP CONSTRAINT FONCTION_REFERENTIEL_CODE_UN");
         } catch (\Exception $e) {
-            $c->msg('error drop constraint FONCTION_REFERENTIEL_CODE_UN');
         }
         try {
             $bdd->exec("DROP INDEX FONCTION_REFERENTIEL_CODE_UN");
         } catch (\Exception $e) {
-            $c->msg('error drop index FONCTION_REFERENTIEL_CODE_UN');
         }
         try {
             $bdd->exec("DROP INDEX FONCTION_REFERENTIEL_UN");
         } catch (\Exception $e) {
-            $c->msg('error drop index FONCTION_REFERENTIEL_UN');
+        }
+        try {
+            $bdd->exec("ALTER TABLE SERVICE_REFERENTIEL DROP CONSTRAINT SERVICE_REFERENTIEL_HISTO_CK");
+        } catch (\Exception $e) {
         }
 
+        try {
+            $bdd->exec("DROP TRIGGER SERVICE_REFERENTIEL_HISTO_CK");
+        } catch (\Exception $e) {
+        }
+    }
+
+
+
+    /**
+     * @param Console $c
+     * @param Bdd     $bdd
+     *
+     * @return mixed
+     * @throws \BddAdmin\Exception\BddCompileException
+     * @throws \BddAdmin\Exception\BddException
+     * @throws \BddAdmin\Exception\BddIndexExistsException
+     */
+    public function traitementFonctionReferentielInsertion(Console $c, Bdd $bdd): void
+    {
         $c->msg('Récuperations des fonctions référentiels');
         $resFonctions = $bdd->select("SELECT *
             FROM FONCTION_REFERENTIEL fr");
@@ -75,38 +115,46 @@ class v23FonctionReferentiel extends AbstractMigration
         $countFonction       = count($resFonctions);
         $nombreFonctionFaite = 1;
         $c->msg('Annualisation des ' . $countFonction . ' fonctions référentiels');
+        $bdd->beginTransaction();
         foreach ($resFonctions as $fonction) {
-            $fonction['HISTO_CREATION']    = new DateTime();
-            $fonction['HISTO_CREATEUR_ID'] = $oa->getOseAppliId();
-            for ($a = 2010; $a < 2100; $a++) {
-                if ($fonction['HISTO_MODIFICATION'] != null) {
-                    $fonction['HISTO_MODIFICATION']    = null;
-                    $fonction['HISTO_MODIFICATEUR_ID'] = null;
-                }
+            $fonction['HISTO_CREATION']        = new DateTime();
+            $fonction['HISTO_CREATEUR_ID']     = $oa->getOseAppliId();
+            $fonction['HISTO_MODIFICATEUR_ID'] = null;
 
-                if ($a == $this->anneeCourante()) {
-                    $fonction['HISTO_MODIFICATION']    = new DateTime();
-                    $fonction['HISTO_MODIFICATEUR_ID'] = $oa->getOseAppliId();
-                }
+            for ($a = 2010; $a < 2100; $a++) {
+
+
+//                if ($a == $this->anneeCourante()) {
+//                    $fonction['HISTO_MODIFICATION']    = new DateTime();
+//                    $fonction['HISTO_MODIFICATEUR_ID'] = $oa->getOseAppliId();
+//                }
+
                 $fonction['ID']       = null;
                 $fonction['ANNEE_ID'] = $a;
                 $bdd->getTable('FONCTION_REFERENTIEL')->insert($fonction);
             }
-            $c->msg('Fonction annualisé : ' . $nombreFonctionFaite . ' sur ' . $countFonction);
+            if ($nombreFonctionFaite % 10 == 0) {
+                $c->msg('Fonction annualisé : ' . $nombreFonctionFaite . ' sur ' . $countFonction);
+            }
             $nombreFonctionFaite++;
         }
+        $bdd->commitTransaction();
+    }
 
-        try {
-            $bdd->exec("ALTER TABLE SERVICE_REFERENTIEL DROP CONSTRAINT SERVICE_REFERENTIEL_HISTO_CK");
-        } catch (\Exception $e) {
-        }
 
-        try {
-            $bdd->exec("DROP TRIGGER SERVICE_REFERENTIEL_HISTO_CK");
-        } catch (\Exception $e) {
-        }
 
-        $c->msg('Traitement des fonctions réferentiels inseré');
+    /**
+     * @param Console $c
+     * @param Bdd     $bdd
+     *
+     * @return void
+     * @throws \BddAdmin\Exception\BddCompileException
+     * @throws \BddAdmin\Exception\BddException
+     * @throws \BddAdmin\Exception\BddIndexExistsException
+     */
+    public function traitementParentIdFonctionReferentiel(Console $c, Bdd $bdd): void
+    {
+        $c->begin('Traitement des fonctions réferentiels inseré');
         $lignefonctionTraite = 0;
 
         $sql = 'select fr2.code, fr2.histo_destruction, fr.annee_id, fr.id
@@ -117,20 +165,50 @@ class v23FonctionReferentiel extends AbstractMigration
         $fonctionsRef = $bdd->select($sql, [], ['fetch' => Bdd::FETCH_EACH]);
 
         while ($fonctionRef = $fonctionsRef->next()) {
-            if(!$fonctionRef['ANNEE_ID']){
-                continue;
-            }
             $newFonction = $this->calculNouvelleFonctionId($bdd, $fonctionRef);
             $bdd->exec('UPDATE fonction_referentiel fr
                             SET fr.parent_id =' . $newFonction . '
                             WHERE fr.id =' . $fonctionRef['ID']);
             $lignefonctionTraite++;
             if ($lignefonctionTraite % 1000 == 0) {
-                $c->msg($lignefonctionTraite.' fonctions traités');
+                $c->msg($lignefonctionTraite . ' fonctions traités');
             }
         }
-        $c->msg('Toutes les fonction referentiel ont été migré');
+        $c->end('Toutes les fonction referentiel ont été migré');
+    }
 
+
+
+    private function calculNouvelleFonctionId(Bdd $bdd, array $itemRef): mixed
+    {
+        $sql = 'SELECT fr.id 
+                FROM FONCTION_REFERENTIEL fr
+                WHERE fr.code = \'' . $itemRef['CODE'] . '\'
+                AND fr.annee_id = ' . $itemRef['ANNEE_ID'];
+
+        if ($itemRef['HISTO_DESTRUCTION']) {
+            $sql .= ' AND fr.histo_destruction = to_timestamp(\'' . $itemRef['HISTO_DESTRUCTION'] . '\', \'yyyy-mm-dd hh24:mi:ss\')';
+        } else {
+            $sql .= ' AND fr.histo_destruction IS NULL';
+        }
+        $res = $bdd->select($sql);
+
+        return $res[0]['ID'];
+    }
+
+
+
+    /**
+     * @param Console $c
+     * @param Bdd     $bdd
+     *
+     * @return void
+     * @throws \BddAdmin\Exception\BddCompileException
+     * @throws \BddAdmin\Exception\BddException
+     * @throws \BddAdmin\Exception\BddIndexExistsException
+     */
+    public function traitementServiceReferentiel(Console $c, Bdd $bdd): void
+    {
         $c->msg('Migration des données du service referentiel vers les nouvelles fonctions');
         $ligneServiceTraite = 0;
 
@@ -149,10 +227,25 @@ class v23FonctionReferentiel extends AbstractMigration
                             WHERE sr.id =' . $serviceRef['ID']);
             $ligneServiceTraite++;
             if ($ligneServiceTraite % 1000 == 0) {
-                $c->msg($ligneServiceTraite.' lignes de service traité');
+                $c->msg($ligneServiceTraite . ' lignes de service traité');
             }
         }
         $c->msg('Toutes les lignes de service referentiel ont été migré');
+    }
+
+
+
+    /**
+     * @param Console $c
+     * @param Bdd     $bdd
+     *
+     * @return void
+     * @throws \BddAdmin\Exception\BddCompileException
+     * @throws \BddAdmin\Exception\BddException
+     * @throws \BddAdmin\Exception\BddIndexExistsException
+     */
+    public function traitementPlafondReferentiel(Console $c, Bdd $bdd): void
+    {
         $c->msg('Migration des données de plafond referentiel vers les nouvelles fonctions');
         $ligneplafondTraite = 0;
 
@@ -171,20 +264,10 @@ class v23FonctionReferentiel extends AbstractMigration
 
             $ligneplafondTraite++;
             if ($ligneplafondTraite % 1000 == 0) {
-                $c->msg($ligneplafondTraite.' plafonds traités');
+                $c->msg($ligneplafondTraite . ' plafonds traités');
             }
         }
         $c->msg('Tous les plafond referentiel ont été migré');
-
-        //supprimer les anciennes fonction qui n'ont pas d'année
-        $c->msg('Suppression des anciennes fonctions');
-
-        $bdd->exec('DELETE FROM TBL_REFERENTIEL');
-        $bdd->exec('DELETE FROM TBL_PLAFOND_REFERENTIEL');
-        $bdd->exec('DELETE FROM FONCTION_REFERENTIEL WHERE annee_id IS NULL;');
-        $c->msg('Fin de la migration des fonctions referentiel');
-
-
     }
 
 
@@ -198,22 +281,5 @@ class v23FonctionReferentiel extends AbstractMigration
         } else {
             return $annee - 1;
         }
-    }
-
-
-
-    private function calculNouvelleFonctionId(Bdd $bdd, array $itemRef): mixed
-    {
-        $sql = 'SELECT fr.id 
-                FROM FONCTION_REFERENTIEL fr
-                WHERE fr.code = \'' . $itemRef['CODE'] . '\'
-                AND fr.annee_id = ' . $itemRef['ANNEE_ID'];
-
-        if ($itemRef['HISTO_DESTRUCTION']) {
-            $sql .= ' AND fr.histo_destruction = to_timestamp(\'' . $itemRef['HISTO_DESTRUCTION'] . '\', \'yyyy-mm-dd hh24:mi:ss\')';
-        }
-        $res = $bdd->select($sql);
-
-        return $res[0]['ID'];
     }
 }

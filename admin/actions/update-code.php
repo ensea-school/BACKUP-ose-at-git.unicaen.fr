e15f9b699dfd1faabc8c9292fae70ad2bc1c5444
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2023-12-19 09:43:05 +0100

    Plus de problème pour Composer

--- admin/actions/update-code.php
+++ admin/actions/update-code.php
@@ -50,6 +50,6 @@ $c->println("\nMise à jour des dépendances à l'aide de Composer", $c::COLOR_L
 $c->passthru([
     "export COMPOSER_ALLOW_SUPERUSER=1",
     "cd $osedir",
-    "php composer.phar self-update --2.2",
+    "php composer.phar self-update",
     "php composer.phar install --optimize-autoloader",
 ]);

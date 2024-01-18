671b0c1fc5430d9a2112c8a6e13c67a9798b01fb
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2024-01-18 17:19:14 +0100

    Branchement des indicateurs 930 940 et 960 sur le nouveau tbl_paiement

--- data/ddl/view/V_INDICATEUR_930.sql
+++ data/ddl/view/V_INDICATEUR_930.sql
@@ -4,14 +4,14 @@ SELECT
   w.structure_id,
   MAX(histo_modification) AS "Date de modification"
 FROM
-  mise_en_paiement mep
+  tbl_paiement tp
+  LEFT JOIN mise_en_paiement mep ON mep.id = tp.mise_en_paiement_id
   LEFT JOIN formule_resultat_service frs ON frs.id = mep.formule_res_service_id
   LEFT JOIN formule_resultat_service_ref frsr ON frsr.id = mep.formule_res_service_ref_id
   JOIN formule_resultat fr ON fr.id = COALESCE(frs.formule_resultat_id,frsr.formule_resultat_id)
   JOIN tbl_workflow w ON w.intervenant_id = fr.intervenant_id
 WHERE
-  mep.histo_destruction IS NULL
-  AND mep.periode_paiement_id IS NULL
+  tp.periode_paiement_id IS NULL
   AND w.etape_code = 'SAISIE_MEP'
   AND w.type_intervenant_code = 'E'
   AND w.atteignable = 1

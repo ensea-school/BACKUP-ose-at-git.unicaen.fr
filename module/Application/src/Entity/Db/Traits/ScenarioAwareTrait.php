2e9e98581807040db975f249d08e8fc532fbbf53
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-01-25 09:31:07 +0100

    Restauration

---
+++ module/Application/src/Entity/Db/Traits/ScenarioAwareTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Entity\Db\Traits;
+
+use Application\Entity\Db\Scenario;
+
+/**
+ * Description of ScenarioAwareTrait
+ *
+ * @author UnicaenCode
+ */
+trait ScenarioAwareTrait
+{
+    protected ?Scenario $scenario = null;
+
+
+
+    /**
+     * @param Scenario $scenario
+     *
+     * @return self
+     */
+    public function setScenario( ?Scenario $scenario )
+    {
+        $this->scenario = $scenario;
+
+        return $this;
+    }
+
+
+
+    public function getScenario(): ?Scenario
+    {
+        return $this->scenario;
+    }
+}

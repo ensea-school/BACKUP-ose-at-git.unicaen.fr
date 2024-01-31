92d6a632918b85819b3b560d9403fdc9c1c42b8a
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-01-31 10:10:55 +0100

    Erreur pour env

--- admin/src/OseEnv.php
+++ admin/src/OseEnv.php
@@ -22,7 +22,7 @@ class OseEnv
 
     public function getEnv()
     {
-        $forcedEnv = $this->oa->config()->get('dev', 'forcd-env', false);
+        $forcedEnv = $this->oa->config()->get('dev', 'forced-env', 'test');
 
         if (false !== $forcedEnv) return $forcedEnv;
 

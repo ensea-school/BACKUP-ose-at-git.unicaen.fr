41b19f95fe89ac6d404886b768922fd28c485dc2
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-02-05 09:56:07 +0100

    Modif v_formule_intervenant

--- data/ddl/view/V_FORMULE_INTERVENANT.sql
+++ data/ddl/view/V_FORMULE_INTERVENANT.sql
@@ -2,14 +2,14 @@ CREATE OR REPLACE FORCE VIEW V_FORMULE_INTERVENANT AS
 SELECT
   i.id                                                                 intervenant_id,
   i.annee_id                                                           annee_id,
-  ti.code                                                              type_intervenant_code,
+  ti.id                                                                type_intervenant_id,
   CASE WHEN ti.code = 'P' THEN s.code ELSE NULL END                    structure_code,
   si.service_statutaire                                                heures_service_statutaire,
+  COALESCE( SUM( msd.heures * mms.multiplicateur ), 0 )                heures_service_modifie,
   CASE WHEN
     si.depassement_service_du_sans_hc = 1
     OR COALESCE( SUM( msd.heures * mms.multiplicateur * mms.decharge ), 0 ) <> 0
-  THEN 1 ELSE 0 END                                                    depassement_service_du_sans_hc,
-  COALESCE( SUM( msd.heures * mms.multiplicateur ), 0 )                heures_service_modifie
+  THEN 1 ELSE 0 END                                                    depassement_service_du_sans_hc
 FROM
             intervenant                  i
   LEFT JOIN structure                    s ON s.id = i.structure_id
@@ -17,7 +17,5 @@ FROM
   LEFT JOIN motif_modification_service mms ON mms.id = msd.motif_id
        JOIN statut                      si ON si.id = i.statut_id
        JOIN type_intervenant            ti ON ti.id = si.type_intervenant_id
-WHERE
-  i.id = COALESCE( OSE_FORMULE.GET_INTERVENANT_ID, i.id )
 GROUP BY
-  i.id, i.annee_id, i.structure_id, ti.code, s.code, si.service_statutaire, si.depassement_service_du_sans_hc
+  i.id, i.annee_id, i.structure_id, ti.id, ti.code, s.code, si.service_statutaire, si.depassement_service_du_sans_hc

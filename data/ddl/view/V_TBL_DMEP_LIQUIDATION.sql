de8de1c2ddf4e9dd9828a61b92c760c6b0028e5e
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2024-02-20 17:11:32 +0100

    Adaptation de V_TBL_DMEP_LIQUIDATION.sql pour prendre en compte les missions.

--- data/ddl/view/V_TBL_DMEP_LIQUIDATION.sql
+++ data/ddl/view/V_TBL_DMEP_LIQUIDATION.sql
@@ -17,7 +17,7 @@ FROM
          JOIN centre_cout               cc ON cc.id = tp.centre_cout_id
          JOIN formule_resultat_service frs ON frs.id = tp.formule_res_service_id
          JOIN service                    s ON s.id = frs.service_id
-         JOIN intervenant                i ON i.id = s.intervenant_id
+         JOIN intervenant                i ON i.id = tp.intervenant_id
     LEFT JOIN element_pedagogique       ep ON ep.id = s.element_pedagogique_id
   WHERE
     1=1
@@ -29,14 +29,32 @@ FROM
   SELECT
     i.annee_id,
     cc.type_ressource_id,
-    sr.structure_id structure_id,
+    tp.structure_id structure_id,
     tp.heures_demandees_aa+tp.heures_demandees_ac   heures
   FROM
 		 tbl_paiement tp
          JOIN centre_cout                    cc ON cc.id = tp.centre_cout_id
          JOIN formule_resultat_service_ref frsr ON frsr.id = tp.formule_res_service_ref_id
          JOIN service_referentiel            sr ON sr.id = frsr.service_referentiel_id
-         JOIN intervenant                     i ON i.id = sr.intervenant_id
+         JOIN intervenant                     i ON i.id = tp.intervenant_id
+
+  WHERE
+	1=1
+	/*@INTERVENANT_ID=i.id*/
+    /*@ANNEE_ID=i.annee_id*/
+
+  UNION ALL
+
+    SELECT
+    i.annee_id,
+    cc.type_ressource_id,
+    tp.structure_id structure_id,
+    tp.heures_demandees_aa+tp.heures_demandees_ac   heures
+  FROM
+		 tbl_paiement tp
+         JOIN centre_cout                    cc ON cc.id = tp.centre_cout_id
+         JOIN mission                        m  ON m.id = tp.mission_id
+         JOIN intervenant                     i ON i.id = tp.intervenant_id
 
   WHERE
 	1=1

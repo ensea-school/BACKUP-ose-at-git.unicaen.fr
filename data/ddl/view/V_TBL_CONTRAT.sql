08f18f6ea64b59c4d3a1f5a920a95119e2968417
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2024-03-11 08:57:17 +0100

    rfontes de la vue V_TBl_CONTRAT

--- data/ddl/view/V_TBL_CONTRAT.sql
+++ data/ddl/view/V_TBL_CONTRAT.sql
@@ -5,12 +5,8 @@ WITH services AS (
             s.intervenant_id                                                            intervenant_id,
             c.id                                                                        contrat_id,
             str.id                                                                      structure_id,
-            str.libelle_court                                                           service_composante,
-            ep.code                                                                     service_code,
-            ep.libelle                                                                  service_libelle,
-            ep.id                                                                       element_pedagogique_id,
-            NULL                                                                        fonction_referentiel_id,
-            NULL                                                                        type_mission_id,
+            vh.service_id                                                               service_id,
+            NULL                                                                        service_referentiel_id,
             NULL                                                                        mission_id,
             SUM(CASE WHEN ti.code = 'CM' THEN vh.heures ELSE 0 END)                     heures_cm,
             SUM(CASE WHEN ti.code = 'TD' THEN vh.heures ELSE 0 END)                     heures_td,
@@ -36,33 +32,26 @@ WITH services AS (
             GROUP BY
             s.intervenant_id,
             c.id,
-            str.libelle_court,
-            ep.code,
-            ep.libelle,
             str.id,
-            ep.id
+            vh.service_id
 
 
     UNION ALL
 
 
         SELECT
-            sr.intervenant_id       intervenant_id,
-            c.id                    contrat_id,
-            str.id                  structure_id,
-            str.libelle_court       service_composante,
-            fr.code                 service_code,
-            fr.libelle_long         service_libelle,
-            NULL                    element_pedagogique_id,
-            fr.id                   fonction_referentiel_id,
-            NULL                    type_mission_id,
-            NULL                    mission_id,
-            0                       heures_cm,
-            0                       heures_td,
-            0                       heures_tp,
-            SUM(vhr.heures)         heures_autres,
-            SUM(vhr.heures)         heures_totales,
-            'REF'                   type_service_code
+            sr.intervenant_id                           intervenant_id,
+            c.id                                        contrat_id,
+            str.id                                      structure_id,
+            NULL                                        service_id,
+            vhr.service_referentiel_id                  service_referentiel_id,
+            NULL                                        mission_id,
+            0                                           heures_cm,
+            0                                           heures_td,
+            0                                           heures_tp,
+            SUM(vhr.heures)                             heures_autres,
+            SUM(vhr.heures)                             heures_totales,
+            'REF'                                       type_service_code
         FROM
             volume_horaire_ref vhr
             JOIN service_referentiel sr ON sr.id = vhr.service_referentiel_id
@@ -80,11 +69,8 @@ WITH services AS (
         GROUP BY
             sr.intervenant_id,
             c.id,
-            str.libelle_court,
-            fr.code,
-            fr.libelle_long,
             str.id,
-            fr.id
+            vhr.service_referentiel_id
 
 
 
@@ -95,12 +81,8 @@ WITH services AS (
             m.intervenant_id        intervenant_id,
             c.id                    contrat_id,
             str.id                  structure_id,
-            str.libelle_court       service_composante,
-            tm.code                 service_code,
-            tm.libelle              service_libelle,
-            NULL                    element_pedagogique_id,
-            NULL                    fonction_referentiel_id,
-            tm.id                   type_mission_id,
+            NULL                    service_id,
+            NULL                    service_referentiel_id,
             m.id                    mission_id,
             0                       heures_cm,
             0                       heures_td,
@@ -129,76 +111,45 @@ WITH services AS (
         GROUP BY
             m.intervenant_id,
             c.id,
-            str.libelle_court,
-            tm.code,
-            tm.libelle,
             str.id,
-            tm.id,
             m.id
-        )
+)
 
-        SELECT
-            rownum id,
-            res.intervenant_id,
-            res.contrat_id,
-            res.structure_id,
-            res.service_code,
-            res.service_libelle,
-            res.heures_cm,
-            res.heures_td,
-            res.heures_tp,
-            res.autres,
-            res.heures,
-            res.service_composante,
-            res.type_service_code,
-            COALESCE(trm.id, trs.id,tr.id)      taux_remu,
-            tmm.id                              taux_majore
-            FROM
-        (
-            SELECT
-                s.intervenant_id,
-                s.contrat_id,
-                ts.id                   type_service_id,
-                s.structure_id,
-                s.service_composante,
-                s.service_code,
-                s.service_libelle,
-                s.element_pedagogique_id,
-                s.fonction_referentiel_id,
-                s.type_mission_id,
-                s.mission_id,
-                s.heures_cm,
-                s.heures_td,
-                s.heures_tp,
-                s.heures_autres autres,
-                SUM(heures_totales) heures,
-                s.type_service_code
-            FROM
-                services s
-                JOIN TYPE_SERVICE ts ON ts.code = s.type_service_code
-            GROUP BY
-                s.intervenant_id,
-                s.contrat_id,
-                ts.id,
-                s.structure_id,
-                s.service_composante,
-                s.service_code,
-                s.service_libelle,
-                s.element_pedagogique_id,
-                s.fonction_referentiel_id,
-                s.type_mission_id,
-                s.mission_id,
-                s.heures_cm,
-                s.heures_td,
-                s.heures_tp,
-                s.heures_autres,
-                s.type_service_code
-        ) res
-        JOIN INTERVENANT i on i.id = res.intervenant_id
-        JOIN statut                     si ON si.id = i.statut_id
-        JOIN parametre                  p ON p.nom = 'taux-remu'
-        LEFT JOIN taux_remu             trs ON si.taux_remu_id = trs.id
-        LEFT JOIN mission               m ON res.mission_id = m.id
-        LEFT JOIN taux_remu             trm ON m.taux_remu_id = trm.id
-        LEFT JOIN taux_remu             tmm ON tmm.id = m.taux_remu_majore_id
-        LEFT JOIN taux_remu             tr ON tr.id = p.valeur
+SELECT
+    s.intervenant_id,
+    s.contrat_id,
+    ts.id                   type_service_id,
+    s.structure_id,
+    s.service_id,
+    s.service_referentiel_id,
+    s.mission_id,
+    s.heures_cm,
+    s.heures_td,
+    s.heures_tp,
+    s.heures_autres autres,
+    SUM(heures_totales) heures,
+    s.type_service_code
+FROM
+    services s
+    JOIN TYPE_SERVICE ts ON ts.code = s.type_service_code
+    JOIN INTERVENANT i on i.id = s.intervenant_id
+    JOIN statut                     si ON si.id = i.statut_id
+    JOIN parametre                  p ON p.nom = 'taux-remu'
+    LEFT JOIN taux_remu             trs ON si.taux_remu_id = trs.id
+    LEFT JOIN mission               m ON s.mission_id = m.id
+    LEFT JOIN taux_remu             trm ON m.taux_remu_id = trm.id
+    LEFT JOIN taux_remu             tmm ON tmm.id = m.taux_remu_majore_id
+    LEFT JOIN taux_remu             tr ON tr.id = p.valeur
+GROUP BY
+    s.intervenant_id,
+    s.contrat_id,
+    ts.id,
+    s.structure_id,
+    s.service_id,
+    s.service_referentiel_id,
+    s.mission_id,
+    s.heures_cm,
+    s.heures_td,
+    s.heures_tp,
+    s.heures_autres,
+    s.type_service_code

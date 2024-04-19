9b0e6d132cd34db2574929da90216e90a75a7d05
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2024-04-19 16:24:23 +0200

    Correction indicateur 391 pour tenir compte uniquement des intervenants qui ont un statut avec les indémnités de fin de contrat d'activées.

--- data/ddl/view/V_INDICATEUR_391.sql
+++ data/ddl/view/V_INDICATEUR_391.sql
@@ -6,8 +6,11 @@ FROM
 mission m
 JOIN contrat c ON c.mission_id = m.id AND c.histo_destruction IS NULL
 JOIN intervenant i ON i.id = m.intervenant_id
+JOIN statut s ON s.id = i.statut_id
 WHERE c.date_retour_signe IS NOT NULL
+AND s.mission_indemnitees = 1
 GROUP BY
 i.id,
 i.structure_id
 HAVING SUM(m.prime_id) IS NULL
+

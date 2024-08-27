d19af06b0e39d77bec32833402488b072795a9eb
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-08-27 11:09:17 +0200

    procédure non utilisée

--- data/ddl/package/OSE_PAIEMENT/body.sql
+++ data/ddl/package/OSE_PAIEMENT/body.sql
@@ -3,28 +3,6 @@ CREATE OR REPLACE PACKAGE BODY "OSE_PAIEMENT" AS
   mois_extraction_paie VARCHAR2(50) := '01';
   annee_extraction_paie VARCHAR2(50) := '22';
 
-  PROCEDURE check_bad_paiements(formule_res_service_id NUMERIC DEFAULT NULL, formule_res_service_ref_id NUMERIC DEFAULT NULL) IS
-    cc NUMERIC;
-  BEGIN
-    SELECT COUNT(*)
-    INTO cc
-    FROM mise_en_paiement mep
-    WHERE mep.histo_destruction IS NULL
-      AND mep.formule_res_service_id = COALESCE(check_bad_paiements.formule_res_service_id, mep.formule_res_service_id)
-      AND mep.formule_res_service_ref_id = COALESCE(check_bad_paiements.formule_res_service_ref_id, mep.formule_res_service_ref_id);
-
-    IF (cc > 0) THEN
-      RAISE_APPLICATION_ERROR(-20101, 'Il est impossible d''effectuer cette action : des demandes de mise en paiement ont été saisies et ne peuvent pas être modifiées');
-    ELSE
-      DELETE FROM
-        mise_en_paiement
-      WHERE
-        histo_destruction IS NOT NULL
-        AND formule_res_service_id = COALESCE(check_bad_paiements.formule_res_service_id, formule_res_service_id)
-        AND formule_res_service_ref_id = COALESCE(check_bad_paiements.formule_res_service_ref_id, formule_res_service_ref_id);
-    END IF;
-  END;
-
 
 
   PROCEDURE set_mois_extraction_paie(mois_extraction_paie VARCHAR2) IS

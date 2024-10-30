a1908f117b6453a571c44098480dedb36699ca6e
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-10-30 12:04:46 +0100

    Débug module Administration

---
+++ module/Administration/Module.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Administration;
+
+use Application\ConfigFactory;
+
+class Module
+{
+
+    public function getConfig()
+    {
+        return ConfigFactory::configFromSimplified(__DIR__, __NAMESPACE__);
+    }
+
+
+
+    public function getAutoloaderConfig()
+    {
+        return ConfigFactory::autoloaderConfig(__DIR__, __NAMESPACE__);
+    }
+
+}

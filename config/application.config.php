a2523fa0fa0ad7a63825179c6c13ea9e7e4234a4
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-10-30 12:01:45 +0100

    mise en conformité Laminas

--- config/application.config.php
+++ config/application.config.php
@@ -2,74 +2,13 @@
 
 $oa = OseAdmin::instance();
 
-$modules = [
-    'Laminas\Cache',
-    'Laminas\Filter',
-    'Laminas\Form',
-    'Laminas\Hydrator',
-    'Laminas\I18n',
-    'Laminas\InputFilter',
-    'Laminas\Log',
-    'Laminas\Mail',
-    'Unicaen\Console',
-    'Laminas\Mvc\I18n',
-    'Laminas\Mvc\Plugin\FlashMessenger',
-    'Laminas\Mvc\Plugin\Prg',
-    'Laminas\Navigation',
-    'Laminas\Paginator',
-    'Laminas\Router',
-    'Laminas\Session',
-    'Laminas\Validator',
-    'DoctrineModule',
-    'DoctrineORMModule',
-    'ZfcUser',
-    'UnicaenApp',
-    'UnicaenAuthentification',
-    'UnicaenMail',
-    'UnicaenUtilisateur',
-    'UnicaenPrivilege',
-    'UnicaenImport',
-    'UnicaenTbl',
-    'UnicaenSiham',
-    'UnicaenVue',
-    'Application',
-    'Agrement',
-    'Intervenant',
-    'Service',
-    'Enseignement',
-    'Referentiel',
-    'Mission',
-    'Paiement',
-    'OffreFormation',
-    'PieceJointe',
-    'Plafond',
-    'Indicateur',
-    'ExportRh',
-    'Dossier',
-    'Contrat',
-    'Lieu',
-    'Parametre',
-    'Formule',
-];
-
-if (!$oa->env()->inConsole()) {
-    array_unshift($modules, 'BjyAuthorize'); // ne charge BjyAuthorize QUE si on n'est pas en mode console
-}
-
-if ($oa->env()->inDev()) {
-    $modules[] = 'Laminas\DeveloperTools';
-}
-
-if ($oa->env()->inConsole() || $oa->env()->inDev()) {
-    $modules[] = 'UnicaenCode';
-}
-
 return [
     'translator'              => [
         'locale' => 'fr_FR',
     ],
-    'modules'                 => $modules,
+    'modules'                 => require __DIR__ . '/modules.config.php',
     'module_listener_options' => [
+        //'use_laminas_loader'       => false,
         'config_glob_paths'        => [
             'config/autoload/{,*.}{global,local' . ($oa->env()->inDev() ? ',dev' : '') . '}.php',
         ],
@@ -79,6 +18,8 @@ return [
         ],
         'cache_dir'                => 'cache/',
         'config_cache_enabled'     => ($oa->env()->inProd() && !$oa->env()->inConsole()),
+        'config_cache_key'         => 'application.config.cache',
         'module_map_cache_enabled' => ($oa->env()->inProd() && !$oa->env()->inConsole()),
+        'module_map_cache_key'     => 'application.module.cache',
     ],
 ];
---
+++ config/modules.config.php
@@ -0,0 +1,66 @@
+<?php
+
+$oa = OseAdmin::instance();
+
+$modules = [
+    'Laminas\Cache',
+    'Laminas\Filter',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\I18n',
+    'Laminas\InputFilter',
+    'Laminas\Log',
+    'Laminas\Mail',
+    'Unicaen\Console',
+    'Laminas\Mvc\I18n',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Navigation',
+    'Laminas\Paginator',
+    'Laminas\Router',
+    'Laminas\Session',
+    'Laminas\Validator',
+    'BjyAuthorize',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'ZfcUser',
+    'UnicaenApp',
+    'UnicaenAuthentification',
+    'UnicaenMail',
+    'UnicaenUtilisateur',
+    'UnicaenPrivilege',
+    'UnicaenImport',
+    'UnicaenTbl',
+    'UnicaenSiham',
+    'UnicaenVue',
+    'Unicaen\BddAdmin',
+    'Application',
+    'Administration',
+    'Agrement',
+    'Intervenant',
+    'Service',
+    'Enseignement',
+    'Referentiel',
+    'Mission',
+    'Paiement',
+    'OffreFormation',
+    'PieceJointe',
+    'Plafond',
+    'Indicateur',
+    'ExportRh',
+    'Dossier',
+    'Contrat',
+    'Lieu',
+    'Parametre',
+    'Formule',
+];
+
+if ($oa->env()->inDev()) {
+    $modules[] = 'Laminas\DeveloperTools';
+}
+
+if ($oa->env()->inConsole() || $oa->env()->inDev()) {
+    $modules[] = 'UnicaenCode';
+}
+
+return $modules;

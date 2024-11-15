5ef127bf7b689819c6106c75217849f73580fed9
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2024-11-15 14:37:49 +0100

    MAJ procédure d'install

--- module/Administration/src/Command/InstallCommand.php
+++ module/Administration/src/Command/InstallCommand.php
@@ -60,7 +60,7 @@ class InstallCommand extends Command
         $io->comment('Initialisation des répertoires de travail OK');
 
         if ($this->hasConfigBdd()) {
-            $this->runCommand($output, 'install-bdd', ['oseappli-pwd' => 'no']);
+            $this->runCommand($output, 'install-bdd', ['--oseappli-pwd' => 'no']);
         } else {
             $io->section("Il reste encore plusieurs étapes à réaliser pour que OSE soit pleinement fonctionnel :");
 

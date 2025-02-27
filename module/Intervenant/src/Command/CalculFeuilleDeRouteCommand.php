25657ce6bcfa4d1fbe87fadd3e59919e248c58c0
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-02-27 11:28:24 +0100

    Doc

--- module/Intervenant/src/Command/CalculFeuilleDeRouteCommand.php
+++ module/Intervenant/src/Command/CalculFeuilleDeRouteCommand.php
@@ -2,13 +2,11 @@
 
 namespace Intervenant\Command;
 
-use Application\Entity\Db\Traits\UtilisateurAwareTrait;
 use Application\Service\Traits\WorkflowServiceAwareTrait;
 use Intervenant\Entity\Db\Intervenant;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use UnicaenApp\Service\EntityManagerAwareTrait;

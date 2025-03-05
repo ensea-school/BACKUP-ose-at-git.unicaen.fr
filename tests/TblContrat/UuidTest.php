db5c0b27bfd04c8de01257861daafb924c1c599d
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-03-05 15:02:24 +0100

    Factorisation

--- tests/TblContrat/UuidTest.php
+++ tests/TblContrat/UuidTest.php
@@ -6,23 +6,8 @@ use Application\Entity\Db\Parametre;
 
 final class UuidTest extends TblContratTestCase
 {
-
-    public function testParametresCaen()
+    public function assertUuidDataset(array $dataset)
     {
-        $parametres = [
-            Parametre::AVENANT        => Parametre::AVENANT_AUTORISE,
-            Parametre::CONTRAT_MIS    => Parametre::CONTRAT_MIS_MISSION,
-            Parametre::CONTRAT_ENS    => Parametre::CONTRAT_ENS_COMPOSANTE,
-        ];
-        $this->useParametres($parametres);
-
-        $dataset = [
-            "contrat_id_1" => [
-                'contratId' => 1,    // int|null,
-            ],
-            // autres cas à ajouter ...
-        ];
-
         foreach ($dataset as $expected => $data) {
             extract($data);
 
@@ -39,33 +24,42 @@ final class UuidTest extends TblContratTestCase
 
 
 
-    public function testParametresContratUnique()
+    public function testParametresCaen()
     {
         $parametres = [
-            Parametre::AVENANT        => Parametre::AVENANT_DESACTIVE,
-            Parametre::CONTRAT_MIS    => Parametre::CONTRAT_MIS_GLOBALE,
-            Parametre::CONTRAT_ENS    => Parametre::CONTRAT_ENS_GLOBALE,
+            Parametre::AVENANT     => Parametre::AVENANT_AUTORISE,
+            Parametre::CONTRAT_MIS => Parametre::CONTRAT_MIS_MISSION,
+            Parametre::CONTRAT_ENS => Parametre::CONTRAT_ENS_COMPOSANTE,
         ];
         $this->useParametres($parametres);
 
         $dataset = [
-            "contrat_id_1" => [ // à adapter...
+            "contrat_id_1" => [
+                'contratId' => 1,    // int|null,
+            ],
+            // autres cas à ajouter ...
+        ];
+        $this->assertUuidDataset($dataset);
+    }
+
+
+
+    public function testParametresContratUnique()
+    {
+        $parametres = [
+            Parametre::AVENANT     => Parametre::AVENANT_DESACTIVE,
+            Parametre::CONTRAT_MIS => Parametre::CONTRAT_MIS_GLOBALE,
+            Parametre::CONTRAT_ENS => Parametre::CONTRAT_ENS_GLOBALE,
+        ];
+        $this->useParametres($parametres);
+
+        $dataset = [
+            "contrat_id_1" => [         // à adapter...
                 'contratId' => null,    // int|null,
             ],
             // autres cas à ajouter ...
         ];
 
-        foreach ($dataset as $expected => $data) {
-            extract($data);
-
-            $uuid = $this->process->generateUUID(
-                intervenant_id: $intervenantId ?? 1, // toujours 1 éventuellement
-                contratId     : $contratId ?? null,
-                structureId   : $structureId ?? null,
-                missionId     : $missionId ?? null
-            );
-
-            $this->assertEquals($expected, $uuid);
-        }
+        $this->assertUuidDataset($dataset);
     }
 }

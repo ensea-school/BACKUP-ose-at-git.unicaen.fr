267619f0b9e634a09790ec00185d97a647994354
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-03-05 14:53:44 +0100

    WIP

--- tests/TblContrat/UuidTest.php
+++ tests/TblContrat/UuidTest.php
@@ -2,18 +2,25 @@
 
 namespace tests\TblContrat;
 
+use Application\Entity\Db\Parametre;
+
 final class UuidTest extends TblContratTestCase
 {
 
-    public function testEssai1()
+    public function testParametresCaen()
     {
+        $parametres = [
+            Parametre::AVENANT        => Parametre::AVENANT_AUTORISE,
+            Parametre::CONTRAT_MIS    => Parametre::CONTRAT_MIS_MISSION,
+            Parametre::CONTRAT_ENS    => Parametre::CONTRAT_ENS_COMPOSANTE,
+        ];
+        $this->useParametres($parametres);
+
         $dataset = [
             "contrat_id_1" => [
-                'intervenantId' => 1,    // int
-                'contratId'     => 1,    // int|null,
-                'structureId'   => null, // int|null
-                //'missionId'     => null, // int|null
+                'contratId' => 1,    // int|null,
             ],
+            // autres cas à ajouter ...
         ];
 
         foreach ($dataset as $expected => $data) {
@@ -30,4 +37,35 @@ final class UuidTest extends TblContratTestCase
         }
     }
 
+
+
+    public function testParametresContratUnique()
+    {
+        $parametres = [
+            Parametre::AVENANT        => Parametre::AVENANT_DESACTIVE,
+            Parametre::CONTRAT_MIS    => Parametre::CONTRAT_MIS_GLOBALE,
+            Parametre::CONTRAT_ENS    => Parametre::CONTRAT_ENS_GLOBALE,
+        ];
+        $this->useParametres($parametres);
+
+        $dataset = [
+            "contrat_id_1" => [ // à adapter...
+                'contratId' => null,    // int|null,
+            ],
+            // autres cas à ajouter ...
+        ];
+
+        foreach ($dataset as $expected => $data) {
+            extract($data);
+
+            $uuid = $this->process->generateUUID(
+                intervenant_id: $intervenantId ?? 1, // toujours 1 éventuellement
+                contratId     : $contratId ?? null,
+                structureId   : $structureId ?? null,
+                missionId     : $missionId ?? null
+            );
+
+            $this->assertEquals($expected, $uuid);
+        }
+    }
 }

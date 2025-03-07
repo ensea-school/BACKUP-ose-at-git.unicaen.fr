04f1cd573a35c49338f1f201d11b7fd4d75babc3
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2025-03-07 12:16:49 +0100

    Utilisation d'entité pour les tests

--- tests/TblContrat/CalculParentsTest.php
+++ tests/TblContrat/CalculParentsTest.php
@@ -4,6 +4,7 @@ namespace tests\TblContrat;
 
 use Application\Entity\Db\Parametre;
 use Contrat\Tbl\Process\Model\Contrat;
+use Contrat\Tbl\Process\Model\VolumeHoraire;
 
 final class CalculParentsTest extends TblContratTestCase
 {
@@ -32,15 +33,15 @@ final class CalculParentsTest extends TblContratTestCase
         $this->useParametres($parametres);
 
         /* Jeu de données initial */
-        $contrat1 = new Contrat();
-        $contrat1->id = 1;
-        $contrat1->edite = true;
+        $contrat1            = new Contrat();
+        $contrat1->id        = 1;
+        $contrat1->edite     = true;
         $contrat1->isMission = false;
 
-        $contrat2 = new Contrat();
+        $contrat2            = new Contrat();
         $contrat2->isMission = false;
 
-        $this->process->calculParentsIds([$contrat1,$contrat2]);
+        $this->process->calculParentsIds([$contrat1, $contrat2]);
 
         $this->assertNotNull($contrat2->parent);
         $this->assertEquals(1, $contrat2->parent->id);
@@ -58,67 +59,76 @@ final class CalculParentsTest extends TblContratTestCase
         $this->useParametres($parametres);
 
         /* Jeu de données initial */
-        $contrat1 = [
-            'id'              => 1,
-            'isMission'       => true,
-            'structureId'     => null,
-            'edite'           => true,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                [
-                    ['missionId' => 1],
-                    ['missionId' => 2],
-                    ['missionId' => 3],
-                ],
-            ],
-        ];
+        $contrat1              = new Contrat();
+        $contrat1->id          = 1;
+        $contrat1->uuid        = 'contrat_principal';
+        $contrat1->isMission   = true;
+        $contrat1->structureId = null;
+        $contrat1->edite       = true;
+        $contrat1->avenants    = [];
+        // Création des objets VolumeHoraire
+        $volumeHoraire1            = new VolumeHoraire();
+        $volumeHoraire1->missionId = 1;
+        $volumeHoraire2            = new VolumeHoraire();
+        $volumeHoraire2->missionId = 1;
+        $volumeHoraire3            = new VolumeHoraire();
+        $volumeHoraire3->missionId = 1;
+        // Ajout des objets dans l'array volumesHoraires
 
-        $contrat2 = [
-            'id'              => 2,
-            'isMission'       => true,
-            'parents'         => &$contrat1,
-            'structureId'     => 1,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
-
-        $contrat1['avenants'][] = &$contrat2;
-
-        $contrat3 = [
-            'id'              => null,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 2,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        $contrat1->volumesHoraires = [$volumeHoraire1, $volumeHoraire2, $volumeHoraire3];
 
 
-        $contrats = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $contrat2              = new Contrat();
+        $contrat2->id          = 2;
+        $contrat2->uuid        = 'avenant_edite';
+        $contrat2->isMission   = true;
+        $contrat2->parent      = &$contrat1;
+        $contrat2->structureId = 1;
+        $contrat2->avenants    = [];
 
-        /* Calculs manuels */
+        // Création des objets VolumeHoraire
+        $volumeHoraire1            = new VolumeHoraire();
+        $volumeHoraire1->missionId = 1;
 
-        $contrat1['avenants'] = [&$contrat2, &$contrat3];
-        $contrat2['parent']   = &$contrat1;
-        $contrat3['parent']   = &$contrat1;
+        // Ajout des objets dans l'array volumesHoraires
+        $contrat2->volumesHoraires = [$volumeHoraire1];
 
 
-        /* Expected */
-        $expected = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $contrat1->avenants[] = &$contrat2;
 
-        $this->assertContrats($contrats, $expected);
+
+        $contrat3              = new Contrat();
+        $contrat3->id          = null;
+        $contrat1->uuid        = 'avenant_a_calculer';
+        $contrat3->isMission   = true;
+        $contrat3->parent      = null;
+        $contrat3->structureId = 2;
+        $contrat3->avenants    = [];
+
+        // Création des objets VolumeHoraire
+        $volumeHoraire1            = new VolumeHoraire();
+        $volumeHoraire1->missionId = 1;
+
+        // Ajout des objets dans l'array volumesHoraires
+        $contrat3->volumesHoraires = [$volumeHoraire1];
+
+        $this->process->calculParentsIds([$contrat1, $contrat2, $contrat3]);
+
+
+        /* Verification des calculs */
+
+        $this->assertCount(2, $contrat1->avenants);
+        $uuids = [];
+        foreach ($contrat1->avenants as $avenant) {
+            $uuids[] = $avenant->uuid;
+        }
+        $uuidsExpected = [$contrat2->uuid, $contrat3->uuid];
+        $this->assertArrayEquals($uuidsExpected, $uuids);
+        $this->assertNotNull($contrat2->parent);
+        $this->assertNotNull($contrat3->parent);
+        $this->assertEquals($contrat1->id, $contrat2->parent->id);
+        $this->assertEquals($contrat1->id, $contrat3->parent->id);
+
     }
 
 
@@ -133,64 +143,65 @@ final class CalculParentsTest extends TblContratTestCase
         $this->useParametres($parametres);
 
         /* Jeu de données initial */
-
         //deux contrats anciennement créer sous un parametrage contrat_mis_mission
-        $contrat1 = [
-            'id'              => 1,
-            'isMission'       => true,
-            'structureId'     => 3,
-            'edite'           => true,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 18],
-            ],
-        ];
 
-        $contrat2 = [
-            'id'              => 2,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'edite'           => true,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        // Création du contrat 1
+        $contrat1              = new Contrat();
+        $contrat1->id          = 1;
+        $contrat1->isMission   = true;
+        $contrat1->structureId = 3;
+        $contrat1->edite       = true;
+        $contrat1->avenants    = [];
 
-        //un nouveau contrat a créer apres changement de parametrage en contrat_mis_composante
-        $contrat3 = [
-            'id'              => null,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        // Création des objets VolumeHoraire pour contrat1
+        $volumeHoraire1            = new VolumeHoraire();
+        $volumeHoraire1->missionId = 18;
+        $contrat1->volumesHoraires = [$volumeHoraire1];
 
+        // Création du contrat 2
+        $contrat2              = new Contrat();
+        $contrat2->id          = 2;
+        $contrat2->isMission   = true;
+        $contrat2->parent      = null;
+        $contrat2->structureId = 3;
+        $contrat2->edite       = true;
+        $contrat2->avenants    = [];
 
-        $contrats = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        // Création des objets VolumeHoraire pour contrat2
+        $volumeHoraire2            = new VolumeHoraire();
+        $volumeHoraire2->missionId = 1;
+        $contrat2->volumesHoraires = [$volumeHoraire2];
 
-        /* Calculs manuels */
+        // Création du contrat 3 (nouveau contrat après changement de paramétrage)
+        $contrat3              = new Contrat();
+        $contrat3->id          = null;
+        $contrat3->isMission   = true;
+        $contrat3->parent      = null;
+        $contrat3->structureId = 3;
+        $contrat3->avenants    = [];
 
-        $contrat2['avenants'] = [&$contrat3];
-        $contrat3['parent']   = &$contrat2;
+        // Création des objets VolumeHoraire pour contrat3
+        $volumeHoraire3            = new VolumeHoraire();
+        $volumeHoraire3->missionId = 1;
+        $contrat3->volumesHoraires = [$volumeHoraire3];
 
 
-        /* Expected */
-        $expected = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $this->process->calculParentsIds([$contrat1, $contrat2, $contrat3]);
 
-        $this->assertContrats($contrats, $expected);
+        //Verification des données
+        $this->assertCount(0, $contrat1->avenants);
+        $this->assertCount(1, $contrat2->avenants);
+        $this->assertCount(0, $contrat3->avenants);
+        $uuids = [];
+        foreach ($contrat2->avenants as $avenant) {
+            $uuids[] = $avenant->uuid;
+        }
+        $uuidsExpected = [$contrat3->uuid];
+        $this->assertArrayEquals($uuidsExpected, $uuids);
+        $this->assertNull($contrat1->parent);
+        $this->assertNull($contrat2->parent);
+        $this->assertNotNull($contrat3->parent);
+        $this->assertEquals($contrat2->id, $contrat3->parent->id);
     }
 
 
@@ -207,63 +218,68 @@ final class CalculParentsTest extends TblContratTestCase
         /* Jeu de données initial */
 
         //deux contrats anciennement créer sous un parametrage contrat_mis_mission, l'un est un projet l'autre non
-        $contrat1 = [
-            'id'              => 1,
-            'isMission'       => true,
-            'structureId'     => 3,
-            'edite'           => true,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 18],
-            ],
-        ];
+        // Création du contrat 1
+        $contrat1              = new Contrat();
+        $contrat1->id          = 1;
+        $contrat1->isMission   = true;
+        $contrat1->structureId = 3;
+        $contrat1->edite       = true;
+        $contrat1->avenants    = [];
 
-        $contrat2 = [
-            'id'              => 2,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'edite'           => false,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        // Création des objets VolumeHoraire pour contrat1
+        $volumeHoraire1            = new VolumeHoraire();
+        $volumeHoraire1->missionId = 18;
+        $contrat1->volumesHoraires = [$volumeHoraire1];
 
-        //un nouveau contrat a créer apres changement de parametrage en contrat_mis_composante
-        $contrat3 = [
-            'id'              => null,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        // Création du contrat 2
+        $contrat2              = new Contrat();
+        $contrat2->id          = 2;
+        $contrat2->isMission   = true;
+        $contrat2->parent      = null;
+        $contrat2->structureId = 3;
+        $contrat2->edite       = false;
+        $contrat2->avenants    = [];
 
+        // Création des objets VolumeHoraire pour contrat2
+        $volumeHoraire2            = new VolumeHoraire();
+        $volumeHoraire2->missionId = 1;
+        $contrat2->volumesHoraires = [$volumeHoraire2];
 
-        $contrats = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        // Création du contrat 3 (nouveau contrat après changement de paramétrage)
+        $contrat3              = new Contrat();
+        $contrat3->id          = null;
+        $contrat3->isMission   = true;
+        $contrat3->parent      = null;
+        $contrat3->structureId = 3;
+        $contrat3->avenants    = [];
 
-        /* Calculs manuels */
-
-        $contrat1['avenants'] = [&$contrat3];
-        $contrat3['parent']   = &$contrat1;
+        // Création des objets VolumeHoraire pour contrat3
+        $volumeHoraire3            = new VolumeHoraire();
+        $volumeHoraire3->missionId = 1;
+        $contrat3->volumesHoraires = [$volumeHoraire3];
 
 
-        /* Expected */
-        $expected = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $this->process->calculParentsIds([$contrat1, $contrat2, $contrat3]);
 
-        $this->assertContrats($contrats, $expected);
+        //Verification des données
+        $this->assertCount(1, $contrat1->avenants);
+        $this->assertCount(0, $contrat2->avenants);
+        $this->assertCount(0, $contrat3->avenants);
+        $uuids = [];
+        foreach ($contrat1->avenants as $avenant) {
+            $uuids[] = $avenant->uuid;
+        }
+        $uuidsExpected = [$contrat3->uuid];
+        $this->assertArrayEquals($uuidsExpected, $uuids);
+        $this->assertNull($contrat1->parent);
+        $this->assertNotNull($contrat2->parent);
+        $this->assertNotNull($contrat3->parent);
+        $this->assertEquals($contrat1->id, $contrat2->parent->id);
+        $this->assertEquals($contrat1->id, $contrat3->parent->id);
+
     }
+
+
 
     public function testContratGlobalParamParMission(): void
     {
@@ -276,70 +292,76 @@ final class CalculParentsTest extends TblContratTestCase
 
         /* Jeu de données initial */
 
-        //deux contrats anciennement créer sous un parametrage contrat_mis_mission, l'un est un projet l'autre non
-        $contrat1 = [
-            'id'              => 1,
-            'isMission'       => true,
-            'structureId'     => null,
-            'edite'           => true,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                [
-                    'missionId' => 1,
-                ],
-                [
-                    'missionId' => 2,
-                ],
-                [
-                    'missionId' => 3,
-                ],
-            ],
-        ];
+        //Un contrat global créer precedemment, deux nouveau contrat créer en param_mis_mission
+        // Création du contrat 1
+        $contrat1              = new Contrat();
+        $contrat1->id          = 1;
+        $contrat1->isMission   = true;
+        $contrat1->structureId = null;
+        $contrat1->edite       = true;
+        $contrat1->avenants    = [];
 
-        $contrat2 = [
-            'id'              => null,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 4],
-            ],
-        ];
+        // Création des objets VolumeHoraire pour contrat1
+        $volumeHoraire1              = new VolumeHoraire();
+        $volumeHoraire1->missionId   = 1;
+        $volumeHoraire1->structureId = 4;
 
-        //un nouveau contrat a créer apres changement de parametrage en contrat_mis_composante
-        $contrat3 = [
-            'id'              => null,
-            'isMission'       => true,
-            'parents'         => null,
-            'structureId'     => 3,
-            'avenants'        => [],
-            'volumesHoraires' => [
-                ['missionId' => 1],
-            ],
-        ];
+        $volumeHoraire2              = new VolumeHoraire();
+        $volumeHoraire2->missionId   = 2;
+        $volumeHoraire2->structureId = 4;
 
+        $volumeHoraire3              = new VolumeHoraire();
+        $volumeHoraire3->missionId   = 3;
+        $volumeHoraire3->structureId = 4;
 
-        $contrats = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $contrat1->volumesHoraires = [$volumeHoraire1, $volumeHoraire2, $volumeHoraire3];
 
-        /* Calculs manuels */
+        // Création du contrat 2
+        $contrat2              = new Contrat();
+        $contrat2->id          = null;
+        $contrat2->isMission   = true;
+        $contrat2->parent      = null;
+        $contrat2->structureId = 3;
+        $contrat2->avenants    = [];
 
-        $contrat1['avenants'] = [&$contrat2,&$contrat3];
-        $contrat2['parent']   = &$contrat1;
-        $contrat3['parent']   = &$contrat1;
+        // Création de l'objet VolumeHoraire pour contrat2
+        $volumeHoraire4              = new VolumeHoraire();
+        $volumeHoraire4->missionId   = 5;
+        $volumeHoraire4->structureId = 6;
+        $contrat2->volumesHoraires   = [$volumeHoraire4];
+
+        // Création du contrat 3 (nouveau contrat après changement de paramétrage)
+        $contrat3              = new Contrat();
+        $contrat3->id          = null;
+        $contrat3->isMission   = true;
+        $contrat3->parent      = null;
+        $contrat3->structureId = 3;
+        $contrat3->avenants    = [];
+
+        // Création de l'objet VolumeHoraire pour contrat3
+        $volumeHoraire5              = new VolumeHoraire();
+        $volumeHoraire5->missionId   = 1;
+        $volumeHoraire5->structureId = 4;
+        $contrat3->volumesHoraires   = [$volumeHoraire5];
 
 
-        /* Expected */
-        $expected = [
-            $contrat1,
-            $contrat2,
-            $contrat3,
-        ];
+        $this->process->calculParentsIds([$contrat1, $contrat2, $contrat3]);
 
-        $this->assertContrats($contrats, $expected);
+        //Verification des données
+        //Verification des données
+        $this->assertCount(1, $contrat1->avenants);
+        $this->assertCount(0, $contrat2->avenants);
+        $this->assertCount(0, $contrat3->avenants);
+        $uuids = [];
+        foreach ($contrat1->avenants as $avenant) {
+            $uuids[] = $avenant->uuid;
+        }
+        $uuidsExpected = [$contrat3->uuid];
+        $this->assertArrayEquals($uuidsExpected, $uuids);
+        $this->assertNull($contrat1->parent);
+        $this->assertNull($contrat2->parent);
+        $this->assertNotNull($contrat3->parent);
+        $this->assertEquals($contrat1->id, $contrat3->parent->id);
+
     }
 }

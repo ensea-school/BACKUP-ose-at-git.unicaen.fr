73d8a43c5aa19267d9ebe1bfe41365fa137bf09b
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2025-03-20 10:17:38 +0100

    Mise en page indicateur 420

--- data/ddl/view/V_INDICATEUR_420.sql
+++ data/ddl/view/V_INDICATEUR_420.sql
@@ -3,13 +3,13 @@ SELECT DISTINCT
   w.intervenant_id,
   CASE
     WHEN w.structure_id IS NOT NULL
-      THEN w.structure_id
+    THEN w.structure_id
     ELSE i.structure_id
-    END structure_id
+  END structure_id
 FROM tbl_workflow w
-       JOIN intervenant  i ON w.intervenant_id = i.id
-       JOIN statut      si ON si.id = i.statut_id
-       LEFT JOIN tbl_contrat tblc ON tblc.intervenant_id = w.intervenant_id
+  JOIN intervenant  i ON w.intervenant_id = i.id
+  JOIN statut      si ON si.id = i.statut_id
+  LEFT JOIN tbl_contrat tblc ON tblc.intervenant_id = w.intervenant_id
 WHERE
   w.atteignable = 1
   AND w.etape_code = 'CONTRAT'

9f87eb1279309aad4342bdd4d478b9efa9fc12b1
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2025-03-20 10:16:45 +0100

    Correction indicateur 420

--- data/ddl/view/V_INDICATEUR_420.sql
+++ data/ddl/view/V_INDICATEUR_420.sql
@@ -3,13 +3,13 @@ SELECT DISTINCT
   w.intervenant_id,
   CASE
     WHEN w.structure_id IS NOT NULL
-    THEN w.structure_id
+      THEN w.structure_id
     ELSE i.structure_id
-  END structure_id
-  FROM tbl_workflow w
-  JOIN intervenant  i ON w.intervenant_id = i.id
-  JOIN statut      si ON si.id = i.statut_id
-  LEFT JOIN tbl_contrat tblc ON tblc.intervenant_id = w.intervenant_id
+    END structure_id
+FROM tbl_workflow w
+       JOIN intervenant  i ON w.intervenant_id = i.id
+       JOIN statut      si ON si.id = i.statut_id
+       LEFT JOIN tbl_contrat tblc ON tblc.intervenant_id = w.intervenant_id
 WHERE
   w.atteignable = 1
   AND w.etape_code = 'CONTRAT'
@@ -17,6 +17,6 @@ WHERE
   AND w.realisation = 0
   AND i.histo_destruction IS NULL
   AND si.histo_destruction IS NULL
-  AND tblc.id IS NULL
+  AND tblc.contrat_id IS NULL
   AND si.contrat = 1
   AND tblc.actif = 1

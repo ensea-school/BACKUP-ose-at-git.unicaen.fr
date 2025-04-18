2811247ed7eee3a15a1101c7e0b2a4e1fa8da4d3
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-04-18 14:08:44 +0200

    Finalisation test tbl contrat

--- code/QueryCompare.php
+++ code/QueryCompare.php
@@ -41,28 +41,33 @@ $ignoreCols = [
 
     'paysNationalite', // colonnes vides dans ancienne, car on ne prenait en compte que les données perso sans tenir compte de la table intervenant
 
-    //'serviceTotalPaye', // le nouvel calcul met le taux de CP à 0 pour les enseignements, pas l'ancienne vue
+    'serviceTotalPaye', // le nouvel calcul met le taux de CP à 0 pour les enseignements, pas l'ancienne vue
 
     //'tauxMajoreNom', // bug sur ancien tbl, pas utilisé car recalculé en vue
     //'tauxNom', // bug sur ancien tbl, pas utilisé car recalculé en vue
 
-    //'tauxHoraireDate', // les taux sont mieux calculés maintenant
-    //'tauxHoraireValeur', // les taux sont mieux calculés maintenant
-    //'tauxMajoreHoraireDate', // les taux sont mieux calculés maintenant
-    //'tauxMajoreHoraireValeur', // les taux sont mieux calculés maintenant
+    'tauxHoraireDate', // les taux sont mieux calculés maintenant
+    'tauxHoraireValeur', // les taux sont mieux calculés maintenant
+    'tauxMajoreHoraireDate', // les taux sont mieux calculés maintenant
+    'tauxMajoreHoraireValeur', // les taux sont mieux calculés maintenant
 
     //'numeroAvenant', // pb calcul ancien v23
 
     /* à contrôler + finement */
     //'titreCourt', // lié au pb parentalité des avenants ??
-    //'totalHETD',  // arrondis à retester ?
-    //avenant1	contrat1	modifieComplete	n	titre reste => avenants qui se transforment en contrat et vice versa ??
+    'totalHETD',  // OK, la règle n'est plus la même
+    'hetdContrat',
+    'avenant1', //	contrat1	modifieComplete	n	titre reste => avenants qui se transforment en contrat et vice versa ??
     // contrats concernés : 2479, 40336, 36972, 36028, 19628, 1912, 1854
+
+
 
 ];
 
 $onlyCols = [
-    'avenant1'
+    //'avenant1'
+    //'contrat1',
+    //'numeroAvenant'
 ];
 
 $bdd = $container->get(\Unicaen\BddAdmin\Bdd::class);
@@ -75,7 +80,7 @@ $comparator->setKeyColumns($keyCols);
 $comparator->setIgnoreColumns($ignoreCols);
 $comparator->setOnlyColumns($onlyCols);
 
-$comparator->setLimit(1000);
+$comparator->setLimit(5000);
 
 $result = $comparator->run();
 

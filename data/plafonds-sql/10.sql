6fef26d0a9977706e99b3d183568ae76e44bed22
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-05-12 09:43:53 +0200

    Adaptation des requêtes plafonds aux changements de la 24
    (cherry picked from commit 7c4de214d2eda3cfe02e564efefe3c154c294290)

--- data/plafonds-sql/10.sql
+++ data/plafonds-sql/10.sql
@@ -4,7 +4,7 @@ SELECT
   fr.intervenant_id                   intervenant_id,
   fr.heures_compl_fi + fr.heures_compl_fc + fr.heures_compl_fa + fr.heures_compl_referentiel heures
 FROM
-       intervenant                i
-  JOIN statut                    si ON si.id = i.statut_id
-  JOIN etat_volume_horaire      evh ON evh.code = 'saisi'
-  JOIN formule_resultat          fr ON fr.intervenant_id = i.id AND fr.etat_volume_horaire_id = evh.id
+       intervenant                   i
+  JOIN statut                       si ON si.id = i.statut_id
+  JOIN etat_volume_horaire         evh ON evh.code = 'saisi'
+  JOIN formule_resultat_intervenant fr ON fr.intervenant_id = i.id AND fr.etat_volume_horaire_id = evh.id

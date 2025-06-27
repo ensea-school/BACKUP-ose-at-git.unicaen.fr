97ff8db50df94772d871a60df34e398678bf79d8
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-06-27 17:10:05 +0200

    wip

--- code/test6.php
+++ code/test6.php
@@ -5,3 +5,14 @@
  * @var $container  \Psr\Container\ContainerInterface
  * @var $io         \Symfony\Component\Console\Style\SymfonyStyle
  */
+
+$sw = $container->get(\Workflow\Service\WorkflowService::class);
+
+// GARDIE Melanie 2024/2025
+$intervenant = $container->get(\Intervenant\Service\IntervenantService::class)->get(1063992);
+
+$fdr = $sw->getFeuilleDeRoute($intervenant);
+
+$etapes = $fdr->getEtapes();
+
+dd($etapes);
--- code/test7.php
+++ code/test7.php
@@ -5,3 +5,8 @@
  * @var $container  \Psr\Container\ContainerInterface
  * @var $io         \Symfony\Component\Console\Style\SymfonyStyle
  */
+
+$wp = $container->get(\Workflow\Tbl\Process\WorkflowProcess::class);
+
+$wp->test();
+

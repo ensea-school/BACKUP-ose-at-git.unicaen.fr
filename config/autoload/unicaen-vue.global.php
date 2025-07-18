21cdcf7e444406005df7e7614a0ae6e751fde19d
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-07-18 16:01:05 +0200

    Gestion proxy

--- config/autoload/unicaen-vue.global.php
+++ config/autoload/unicaen-vue.global.php
@@ -1,6 +1,6 @@
 <?php
 
-$hostUrl = ($_ENV['REQUEST_SCHEME'] ?? 'http').'://'.($_ENV['APP_HOST'] ?? 'localhost').':'.($_ENV['APP_VITE_PORT'] ?? 5133);
+$hostUrl = ($_ENV['HTTP_X_FORWARDED_PROTO'] ?? $_ENV['REQUEST_SCHEME'] ?? 'http').'://'.($_ENV['APP_HOST'] ?? 'localhost').':'.($_ENV['APP_VITE_PORT'] ?? 5133);
 
 return [
     'unicaen-vue' => [

c3f52d29842d9166ef2c9f763bd8cf47b660b3dd
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-07-22 09:46:40 +0200

    - Modifications du connecteur côté Apogée   - Suppression des tables tampon ose_groupe_type_formation et ose_type_formation   - modification requête alimentation de ose_etape - Suppression de la vue source src_groupe_type_formation : les groupes de type de formation sont maintenant gérés directement dans OSE - Modification de src_type_formation : la vue va puiser ses données dans la table Apogée `typ_diplome` - Modification de src_etape : la vue puise dans la table OSE des groupes de type de formation pour détecter si le niveau est pertinent ou non

--- doc/Connecteurs-Import/Apogée/SRC_ETAPE.sql
+++ doc/Connecteurs-Import/Apogée/SRC_ETAPE.sql
@@ -4,7 +4,7 @@ SELECT
   e.libelle                           libelle,
   to_number(e.annee_id)               annee_id,
   tf.id                               type_formation_id,
-  to_number(e.niveau)                 niveau,
+  CASE WHEN gtf.pertinence_niveau = 0 THEN null ELSE to_number(e.niveau) END niveau,
   e.specifique_echanges               specifique_echanges,
   s.id                                structure_id,
   src.id                              source_id,
@@ -14,6 +14,7 @@ FROM
             ose_etape@apoprod            e
        JOIN source                     src ON src.code       = 'Apogee'
   LEFT JOIN type_formation              tf ON tf.source_code = e.z_type_formation_id
+  LEFT JOIN groupe_type_formation      gtf ON gtf.id = tf.groupe_id
   LEFT JOIN SRC_HARPEGE_STRUCTURE_CODES sc ON sc.c_structure = e.z_structure_id
   LEFT JOIN structure                    s ON s.source_code  = sc.c_structure_n2
   LEFT JOIN domaine_fonctionnel         df ON df.source_code = e.domaine_fonctionnel

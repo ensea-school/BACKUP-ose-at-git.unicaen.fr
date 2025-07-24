413ecb647aadb8c840fd0ed258d6c25e592c9f54
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2025-07-24 14:38:26 +0200

    Config vite unicaen vue

--- config/autoload/unicaen-vue.global.php
+++ config/autoload/unicaen-vue.global.php
@@ -3,11 +3,11 @@
 $hostUrl = $_ENV['DEV_VITE_URL'] ?? null;
 
 if (!$hostUrl) {
-    $sheme = $_ENV['HTTP_X_FORWARDED_PROTO'] ?? $_ENV['REQUEST_SCHEME'] ?? 'http';
+    $scheme = $_ENV['HTTP_X_FORWARDED_PROTO'] ?? $_ENV['REQUEST_SCHEME'] ?? 'http';
     $host = $_ENV['APP_HOST'] ?: 'localhost';
     $port = $_ENV['DEV_VITE_PORT'] ?: 5133;
 
-    $hostUrl = $sheme.'://'.$host.':'.$port;
+    $hostUrl = $scheme . '://' . $host . ':' . $port;
 }
 
 

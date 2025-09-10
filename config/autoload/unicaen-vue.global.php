d6d363b17629ba405a53af2a11f4d969a9a125e5
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2025-09-10 11:59:24 +0200

    Erreur de synthaxe qui générait un warning (#63193)

--- config/autoload/unicaen-vue.global.php
+++ config/autoload/unicaen-vue.global.php
@@ -4,10 +4,11 @@ $hostUrl = $_ENV['DEV_VITE_URL'] ?? null;
 
 if (!$hostUrl) {
     $scheme = $_ENV['HTTP_X_FORWARDED_PROTO'] ?? $_ENV['REQUEST_SCHEME'] ?? 'http';
-    $host   = $_ENV['APP_HOST'] ?: 'localhost';
-    $port   = $_ENV['DEV_VITE_PORT'] ?: 5133;
+    $host = $_ENV['APP_HOST'] ?? 'localhost';
+    $port = $_ENV['DEV_VITE_PORT'] ?? 5133;
 
     $hostUrl = $scheme . '://' . $host . ':' . $port;
+
 }
 
 return [

566e2b830f6d7f893b5651cac3a499e86f2116c0
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-10-16 13:52:05 +0200

    correction : l'admin n'a pas tous les privileges

--- module/Utilisateur/src/Provider/UserProvider.php
+++ module/Utilisateur/src/Provider/UserProvider.php
@@ -152,8 +152,7 @@ class UserProvider implements UserAdapterInterface
         $query->enableResultCache(true);
         $query->setResultCacheId('intervenant_profiles_' . $user->getId() . '_' . $annee->getId());
 
-        $query->setParameters(['utilisateurCode' => $utilisateurCode,
-                               'annee'           => $annee->getId()]);
+        $query->setParameters(['utilisateurCode' => $utilisateurCode, 'annee' => $annee->getId()]);
 
         /** @var Intervenant[] $intervenants */
         $intervenants = $query->getResult();
@@ -246,30 +245,21 @@ class UserProvider implements UserAdapterInterface
 
     public function getAvailablePrivileges(): array
     {
-        $privileges = [];
-
-        $sql    = "
-        SELECT 
-          cp.code || '-' || p.code priv_code
-        FROM 
-          privilege p 
-          JOIN categorie_privilege cp ON cp.id = p.categorie_id
-          JOIN role_privilege rp ON rp.privilege_id = p.id
-        ";
-        $result = $this->entityManager->getConnection()->executeQuery($sql);
-        while ($privilege = $result->fetchOne()) {
-            $privileges[] = $privilege;
-        }
-
-        return $privileges;
+        $rc         = new \ReflectionClass(Privileges::class);
+        return array_values($rc->getConstants());
     }
 
 
 
     protected function getAdministrateurPrivileges(): array
     {
-        $rc = new \ReflectionClass(Privileges::class);
-        return array_values($rc->getConstants());
+        $privileges = $this->getAvailablePrivileges();
+        foreach ($privileges as $index => $privilege) {
+            if (in_array($privilege, $this->noAdminPrivileges)) {
+                unset($privileges[$index]);
+            }
+        }
+        return $privileges;
     }
 
 

199f0f3cfbc475b067e97ca37c13449854ccc9a5
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-10-17 09:18:03 +0200

    ménage & contrôle modifs receipes

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,7 +10,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];

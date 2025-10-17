bc717a35f94d4d38cbc582de34c57ac5f07aade9
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-10-17 16:54:43 +0200

    script de migration pour v25

--- module/Administration/src/Migration/v25Workflow.php
+++ module/Administration/src/Migration/v25Workflow.php
@@ -3,6 +3,8 @@
 namespace Administration\Migration;
 
 use Unicaen\BddAdmin\Migration\MigrationAction;
+use Unicaen\Framework\Application\Application;
+use Workflow\Service\WorkflowService;
 
 class v25Workflow extends MigrationAction
 {
@@ -29,4 +31,24 @@ class v25Workflow extends MigrationAction
         $this->getBdd()->exec('DROP TABLE TBL_VALIDATION_REFERENTIEL CASCADE CONSTRAINTS');
     }
 
+
+
+    public function after()
+    {
+        $bdd = $this->getBdd();
+
+        try {
+            $bdd->data()->addAction('workflow-reset', 'Réinitialisation du workflow');
+
+            $config = $bdd->data()->getOption('config');
+            // ignore plus ces colonnes en cas d'update afin de tout remettre à l'état initial
+            unset($config['WORKFLOW_ETAPE']['options']['update-ignore-cols']);
+            $bdd->data()->setOption('config', $config);
+            $bdd->data()->run('workflow-reset');
+            $this->logSuccess('Le nouveau workflow a été initialisé avec des paramétrages par défaut, il vous faudra le configurer par vous-même');
+        } catch (\Exception $e) {
+            $this->logError($e->getMessage());
+            return Command::FAILURE;
+        }
+    }
 }

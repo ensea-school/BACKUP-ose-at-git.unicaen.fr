ad398497de4e3099c11a773c6182ce1a651221f0
Laurent Lécluse <laurent.lecluse@unicaen.fr>
2025-11-06 10:47:26 +0100

    réparation popConfirm

--- front/autoload.js
+++ front/autoload.js
@@ -2,7 +2,6 @@ export default {
     /* UI components */
     UHeures: 'Application/UI/UHeures',
     UInputFloat: 'Application/UI/UInputFloat',
-    UConfirmDialog: 'Application/UI/UConfirmDialog',
 
     /* App common components */
     Utilisateur: 'Application/Utilisateur',

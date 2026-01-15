98f7f0459a71f68fbd46a9632357431e4aa5101c
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2026-01-15 15:15:27 +0100

    correction probleme calcul % completion validation mission

--- data/ddl/view/V_TBL_WORKFLOW_MISSION.sql
+++ data/ddl/view/V_TBL_WORKFLOW_MISSION.sql
@@ -18,12 +18,12 @@ GROUP BY
 UNION ALL
 
 SELECT
-  'mission_validation'                                                                                   etape_code,
-  m.intervenant_id                                                                                       intervenant_id,
-  m.structure_id                                                                                         structure_id,
-  SUM(1)                                                                                                 objectif,
-  SUM(CASE WHEN heures_realisees_validees = m.heures_realisees_saisies AND heures_realisees_validees > 0  THEN 1 ELSE 0 END)                partiel,
-  SUM(CASE WHEN m.valide = 1 AND m.heures_prevues_validees = m.heures_prevues_saisies AND heures_realisees_validees > 0  THEN 1 ELSE 0 END) realisation
+  'mission_validation'                                                                                                                  etape_code,
+  m.intervenant_id                                                                                                                      intervenant_id,
+  m.structure_id                                                                                                                        structure_id,
+  SUM(1)                                                                                                                                objectif,
+  SUM(CASE WHEN heures_prevues_validees > 0  THEN 1 ELSE 0 END)                                                                         partiel,
+  SUM(CASE WHEN m.valide = 1 AND m.heures_prevues_validees = m.heures_prevues_saisies AND heures_prevues_validees > 0  THEN 1 ELSE 0 END) realisation
 FROM
   tbl_mission m
 WHERE

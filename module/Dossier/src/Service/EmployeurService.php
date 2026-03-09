4d1ab6cd0f78b716e50d9852213cf8ae3c577c72
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2026-03-09 14:55:31 +0100

    Meilleure lisibilité du libellé employeur

--- module/Dossier/src/Service/EmployeurService.php
+++ module/Dossier/src/Service/EmployeurService.php
@@ -103,6 +103,10 @@ class EmployeurService extends AbstractEntityService
                 'source'      => $r['SOURCE_ID'],
                 'source_code' => $r['CODE'],
             ];
+
+            if (!empty($r['NOM_COMMERCIAL'])) {
+                $employeurs[$r['ID']]['label'] .= ' - ' . $r['NOM_COMMERCIAL'];
+            }
         }
 
 
@@ -160,9 +164,15 @@ class EmployeurService extends AbstractEntityService
             * $data[8] = Date de dernier traitement de l'unité légale
             * $data[9] = Unité pouvant employer des personnes
             * $data[10] = Identifiant association
-            * $data[12] = Siret
+            * $data[11] = Siret
             *
             */
+
+            //On ignore la premiere ligne du fichier qui est un header
+            if ($data[0] == 'siren') {
+                continue;
+            }
+
 
             $nomCommercial = (!empty($data[5])) ? $data[5] : '';
             $nomCommercial .= (!empty($data[6])) ? ' ' . $data[6] : '';
@@ -173,7 +183,7 @@ class EmployeurService extends AbstractEntityService
             //SIREN
             $siren = $data[0];
             //SIRET
-            $siret = (isset($data[12])) ? $data[0] . $data[12] : '';
+            $siret = (isset($data[11])) ? $data[0] . $data[11] : '';
 
             //IDENTIFIANT ASSOCIATION
             $identifiantAssociation = $data[10];

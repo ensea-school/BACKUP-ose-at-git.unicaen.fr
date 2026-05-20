6180c0050211507dc142374783454a48079df9fb
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2026-05-20 15:40:20 +0200

    modification de la commande d'update de structure

--- module/Administration/src/Command/UpdateStructuresCommand.php
+++ module/Administration/src/Command/UpdateStructuresCommand.php
@@ -9,9 +9,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
- * Description of UpdateCodeCommand
+ * Description of UpdateStructuresCommand
  *
- * @author Laurent Lécluse <laurent.lecluse at unicaen.fr>
  */
 class UpdateStructuresCommand extends Command
 {

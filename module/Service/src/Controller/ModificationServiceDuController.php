be516154805b5591d2cb1b6f82639610bd9f862e
Antony Le Courtes <antony.lecourtes@unicaen.fr>
2026-08-26 12:31:55 +0200

    Correction pour filtrer les motifs de modification de service dû lorsqu'elles sont historisées sans impacter les années passés (#67398)

--- module/Service/src/Controller/ModificationServiceDuController.php
+++ module/Service/src/Controller/ModificationServiceDuController.php
@@ -29,10 +29,9 @@ class ModificationServiceDuController extends AbstractController
 
     public function saisirAction()
     {
-        $this->em()->getFilters()->enable('historique')->init([
-            ModificationServiceDu::class,
-            MotifModificationServiceDu::class,
-        ]);
+        $historiqueFilter = $this->em()->getFilters()->enable('historique');
+        $historiqueFilter->enableForEntity(ModificationServiceDu::class);
+        $historiqueFilter->disableForEntity(MotifModificationServiceDu::class);
 
 
         /** @var Intervenant $intervenant */
@@ -65,8 +64,12 @@ class ModificationServiceDuController extends AbstractController
         /** @var MotifModificationServiceDu[] $mds */
         $mds = $this->getServiceMotifModificationServiceDu()->getList();
         $multiplicateurs = [];
+        $motifsObsoletes = [];
         foreach( $mds as $md ){
             $multiplicateurs[$md->getId()] = $md->getMultiplicateur();
+            if ($md->getHistoDestruction()) {
+                $motifsObsoletes[] = $md->getId();
+            }
         }
 
         $variables = [
@@ -74,6 +77,7 @@ class ModificationServiceDuController extends AbstractController
             'intervenant'     => $intervenant,
             'title'           => "Modifications de service dû",
             'multiplicateurs' => $multiplicateurs,
+            'motifsObsoletes' => $motifsObsoletes,
             'canEdit'         => $canEdit,
         ];
 

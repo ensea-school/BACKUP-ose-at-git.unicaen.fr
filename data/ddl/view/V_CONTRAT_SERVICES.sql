1b173c722012ce5eb36d0702a9ddc12504254b95
florian.joriot@unicaen.fr <florian.joriot@unicaen.fr>
2026-09-15 15:10:50 +0200

    Correction vue V_CONTRAT_SERVICE pour ignorer les elements et service historisé

--- data/ddl/view/V_CONTRAT_SERVICES.sql
+++ data/ddl/view/V_CONTRAT_SERVICES.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE FORCE VIEW V_CONTRAT_SERVICES AS
+
 SELECT
   t.intervenant_id,
   t.contrat_id,
@@ -25,51 +26,54 @@ SELECT
   CASE WHEN SUM(t.autres) = 0 THEN '0' ELSE REPLACE(ltrim(to_char(SUM(t.autres), '999999.00')),'.',',') END "autres"
 FROM
   (
-  SELECT
-    tblc.intervenant_id                                  intervenant_id,
-    tblc.contrat_id                                      contrat_id,
-    tblc.type_service_id                                 type_service_id,
-    str.id                                               structure_id,
-    ep.id                                                element_pedagogique_id,
-    fr.id                                                fonction_referentiel_id,
-    m.id                                                 mission_id,
-    tm.id                                                type_mission_id,
+    SELECT
+      tblc.intervenant_id                                  intervenant_id,
+      tblc.contrat_id                                      contrat_id,
+      tblc.type_service_id                                 type_service_id,
+      str.id                                               structure_id,
+      ep.id                                                element_pedagogique_id,
+      fr.id                                                fonction_referentiel_id,
+      m.id                                                 mission_id,
+      tm.id                                                type_mission_id,
 
-    ts.code                                              type_service,
-    str.libelle_court                                    service_composante,
-    p.libelle_long                                       periode,
-    COALESCE(tm.code, fr.code, ep.code)                  service_code,
-    COALESCE(tm.libelle, fr.libelle_long, ep.libelle)    service_libelle,
+      ts.code                                              type_service,
+      str.libelle_court                                    service_composante,
+      p.libelle_long                                       periode,
+      COALESCE(tm.code, fr.code, ep.code)                  service_code,
+      COALESCE(tm.libelle, fr.libelle_long, ep.libelle)    service_libelle,
 
-    -- somme par contrat
-    SUM(tblc.heures) OVER (PARTITION BY tblc.intervenant_id, tblc.contrat_id, tblc.type_service_id, COALESCE(ep.id,tm.id,fr.id) ,p.libelle_long) service_heures,
+      -- somme par contrat
+      SUM(tblc.heures) OVER (PARTITION BY tblc.intervenant_id, tblc.contrat_id, tblc.type_service_id, COALESCE(ep.id,tm.id,fr.id) ,p.libelle_long) service_heures,
 
     -- heures atomiques
-    tblc.heures            heures,
-    tblc.hetd              hetd,
-    tblc.cm                cm,
-    tblc.td                td,
-    tblc.tp                tp,
-    tblc.autres            autres
+      tblc.heures            heures,
+      tblc.hetd              hetd,
+      tblc.cm                cm,
+      tblc.td                td,
+      tblc.tp                tp,
+      tblc.autres            autres
 
-  FROM
-              tbl_contrat        tblc
-         JOIN type_service         ts ON ts.id = tblc.type_service_id
-    LEFT JOIN service               s ON s.id = tblc.service_id
-    LEFT JOIN element_pedagogique  ep ON ep.id = s.element_pedagogique_id
-    LEFT JOIN volume_horaire       vh ON vh.id = tblc.volume_horaire_id
-    LEFT JOIN periode               p ON p.id = vh.periode_id
+    FROM
+      tbl_contrat        tblc
+        JOIN type_service         ts ON ts.id = tblc.type_service_id
+        LEFT JOIN service               s ON s.id = tblc.service_id
+        LEFT JOIN element_pedagogique  ep ON ep.id = s.element_pedagogique_id
+        LEFT JOIN volume_horaire       vh ON vh.id = tblc.volume_horaire_id
+        LEFT JOIN periode               p ON p.id = vh.periode_id
 
-    LEFT JOIN service_referentiel  sr ON sr.id = tblc.service_referentiel_id
-    LEFT JOIN fonction_referentiel fr ON fr.id = sr.fonction_id
+        LEFT JOIN service_referentiel  sr ON sr.id = tblc.service_referentiel_id
+        LEFT JOIN fonction_referentiel fr ON fr.id = sr.fonction_id
 
-    LEFT JOIN mission               m ON m.id = tblc.mission_id
-    LEFT JOIN type_mission         tm ON tm.id = m.type_mission_id
+        LEFT JOIN mission               m ON m.id = tblc.mission_id
+        LEFT JOIN type_mission         tm ON tm.id = m.type_mission_id
 
-    LEFT JOIN structure           str ON str.id = COALESCE(m.structure_id, sr.structure_id, ep.structure_id)
-  WHERE
-    tblc.actif = 1
-) t
+        LEFT JOIN structure           str ON str.id = COALESCE(m.structure_id, sr.structure_id, ep.structure_id)
+    WHERE
+      tblc.actif = 1
+      AND ep.histo_destruction is null
+      AND s.histo_destruction is null
+      AND vh.histo_destruction is null
+  ) t
 GROUP BY
   t.intervenant_id,
   t.contrat_id,
